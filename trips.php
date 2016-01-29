e085782d1c6b67b037dbbf684cf5cf193b7b378d
Steven Smith <Steven.Smith1989@Hotmail.co.uk>
2016-01-29 11:31:32 +0000

    Update to trips page

--- trips.php
+++ trips.php
@@ -5,12 +5,12 @@
         <title>Park Community School | Trips</title>
         <?php include("includes/head.html") ?>
     </head>
-<body class="page-parents">
+<body class="page-parent">
 <!-- NAVIGATION -->
 <?php include("includes/topnav.html") ?>
 
 <!-- HEADER SECTION -->
-<header class="header-parents">
+<header class="header-parent">
     <div class="row">
         <div class="medium-8 large-8 columns">
             <h1>Trips</h1>
@@ -25,112 +25,133 @@
         <br>
         <div class="medium-12 columns">
 
-            <h2>Apple Pi Filming, York</h2>
-            <p>Monday 28th June – Saturday 2nd July 2016</p>
-            <p>Lead Teacher – Mr Eacott</p>
-            <p>Cost - £30.00</p>
-            <p>Payment is due before trip date</p>
+        <ul class="medium-block-grid-1 large-block-grid-2">
+            <li>
+                <h3>Apple Pi Filming, York</h3>
+                <p>Monday 28th June – Saturday 2nd July 2016</p>
+                <p>Lead Teacher – Mr Eacott</p>
+                <p>Cost - £30.00</p>
+                <p>Payment is due before trip date</p>
+                <hr>
+            </li>
+            
 
+            <li>
+                <h3>Year 7 Mary Poppins</h3>
+                <p>Thursday 9th June 2016</p>
+                <p>Lead Teacher – Miss Chuter</p>
+                <p>Cost £20.00</p>
+                <table>
+                  <tr>
+                    <th>Mary Poppins</th>
+                    <th>Payment by</th>
+                    <th>Amount</th>
+                    <th>Total</th>
+                  </tr>
+                  <tr>
+                    <td>Non-refundable Deposit</td>
+                    <td>01/11/15</td>
+                    <td>£10.00</td>
+                    <td>£10.00</td>
+                  </tr>
+                  <tr>
+                    <td>Final payment</td>
+                    <td>19/05/16</td>
+                    <td>£10.00</td>
+                    <td>£20.00</td>
+                  </tr>
+                </table>
+                <hr>
+            </li>
+            
 
-            <h2>Year 7 Mary Poppins</h2>
-            <p>Thursday 9th June 2016</p>
-            <p>Lead Teacher – Miss Chuter</p>
-            <p>Cost £20.00</p>
-            <table>
-              <tr>
-                <th>Mary Poppins</th>
-                <th>Payment by</th>
-                <th>Amount</th>
-                <th>Total</th>
-              </tr>
-              <tr>
-                <td>Non-refundable Deposit</td>
-                <td>01/11/15</td>
-                <td>£10.00</td>
-                <td>£10.00</td>
-              </tr>
-              <tr>
-                <td>Final payment</td>
-                <td>19/05/16</td>
-                <td>£10.00</td>
-                <td>£20.00</td>
-              </tr>
-            </table>
+            <li>
+               <h3>Marwell Activity Centre</h3>
+                <p>Thursday 28th and Friday 29th April</p>
+                <p>Lead Teacher – Miss French</p>
+                <p>Cost - £105.00</p>
+                <table>
+                  <tr>
+                    <th>Marwell</th>
+                    <th>Payment by</th>
+                    <th>Amount</th>
+                    <th>Total</th>
+                  </tr>
+                  <tr>
+                    <td>Non-refundable Deposit</td>
+                    <td>15/01/16</td>
+                    <td>£50.00</td>
+                    <td>£50.00</td>
+                  </tr>
+                  <tr>
+                    <td>First Instalment</td>
+                    <td>12/02/16</td>
+                    <td>£30.00</td>
+                    <td>£80.00</td>
+                  </tr>
+                  <tr>
+                    <td>Final payment</td>
+                    <td>18/03/16</td>
+                    <td>£25.00</td>
+                    <td>£105.00</td>
+                  </tr>
+                </table> 
+                <hr>
+            </li>
+            
 
+            <li>
+                <h3>Rock Challenge</h3>
+                <p>Monday 14th March 2016</p>
+                <p>Lead Teacher – Miss Chuter</p>
+                <p>Cost - £20.00</p>
+                <p>Payment is due before trip date</p> 
+                <hr>
+            </li>
+            
 
-            <h2>Marwell Activity Centre</h2>
-            <p>Thursday 28th and Friday 29th April</p>
-            <p>Lead Teacher – Miss French</p>
-            <p>Cost - £105.00</p>
-            <table>
-              <tr>
-                <th>Marwell</th>
-                <th>Payment by</th>
-                <th>Amount</th>
-                <th>Total</th>
-              </tr>
-              <tr>
-                <td>Non-refundable Deposit</td>
-                <td>15/01/16</td>
-                <td>£50.00</td>
-                <td>£50.00</td>
-              </tr>
-              <tr>
-                <td>First Instalment</td>
-                <td>12/02/16</td>
-                <td>£30.00</td>
-                <td>£80.00</td>
-              </tr>
-              <tr>
-                <td>Final payment</td>
-                <td>18/03/16</td>
-                <td>£25.00</td>
-                <td>£105.00</td>
-              </tr>
-            </table> 
+            <li>
+               <h3>London Theatre Trip</h3>
+                <p>Wednesday 10th February 2016</p>
+                <p>Lead Teacher – Miss Chuter</p>
+                <p>Cost £60.00</p>
+                <table>
+                  <tr>
+                    <th>London Theatre Trip</th>
+                    <th>Payment by</th>
+                    <th>Amount</th>
+                    <th>Total</th>
+                  </tr>
+                  <tr>
+                    <td>Non-refundable Deposit</td>
+                    <td>18/12/15</td>
+                    <td>£30.00</td>
+                    <td>£30.00</td>
+                  </tr>
+                  <tr>
+                    <td>Final payment</td>
+                    <td>02/02/16</td>
+                    <td>£30.00</td>
+                    <td>£60.00</td>
+                  </tr>
+                </table> 
+                <hr>
+            </li>
+            
 
+            <li>
+                <h3>Portrait Gallery Trip</h3>
+                <p>Monday 1st February 2016</p>
+                <p>Lead Teacher – Miss Loake</p>
+                <p>Cost - £7.50</p>
+                <p>Payment is due before trip date</p>
+                <hr>
+            </li>
+            
 
-            <h2>Rock Challenge</h2>
-            <p>Monday 14th March 2016</p>
-            <p>Lead Teacher – Miss Chuter</p>
-            <p>Cost - £20.00</p>
-            <p>Payment is due before trip date</p>
+        </ul>
 
-
-            <h2>London Theatre Trip</h2>
-            <p>Wednesday 10th February 2016</p>
-            <p>Lead Teacher – Miss Chuter</p>
-            <p>Cost £60.00</p>
-            <table>
-              <tr>
-                <th>London Theatre Trip</th>
-                <th>Payment by</th>
-                <th>Amount</th>
-                <th>Total</th>
-              </tr>
-              <tr>
-                <td>Non-refundable Deposit</td>
-                <td>18/12/15</td>
-                <td>£30.00</td>
-                <td>£30.00</td>
-              </tr>
-              <tr>
-                <td>Final payment</td>
-                <td>02/02/16</td>
-                <td>£30.00</td>
-                <td>£60.00</td>
-              </tr>
-            </table>
-
-
-            <h2>Portrait Gallery Trip</h2>
-            <p>Monday 1st February 2016</p>
-            <p>Lead Teacher – Miss Loake</p>
-            <p>Cost - £7.50</p>
-            <p>Payment is due before trip date</p>
-
-
-            <a class="button radius success" href="//www.scopay.com/pcs" target="_blank">Pay Online</a>
+        <a class="button radius success" href="//www.scopay.com/pcs" target="_blank">Pay Online</a>
 
         </div>
         
