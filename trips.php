2414bbf739cffac3b041e0bbd85920ffd18c460a
Steven Smith <Steven.Smith1989@Hotmail.co.uk>
2016-01-29 10:50:03 +0000

    Add trips page and update top navigation

---
+++ trips.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+        
+        <title>Park Community School | Trips</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-parents">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-parents">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Trips</h1>
+            <p class="text-capitalize">Current list of available trips</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-12 columns">
+
+            <h2>Apple Pi Filming, York</h2>
+            <p>Monday 28th June – Saturday 2nd July 2016</p>
+            <p>Lead Teacher – Mr Eacott</p>
+            <p>Cost - £30.00</p>
+            <p>Payment is due before trip date</p>
+
+
+            <h2>Year 7 Mary Poppins</h2>
+            <p>Thursday 9th June 2016</p>
+            <p>Lead Teacher – Miss Chuter</p>
+            <p>Cost £20.00</p>
+            <table>
+              <tr>
+                <th>Mary Poppins</th>
+                <th>Payment by</th>
+                <th>Amount</th>
+                <th>Total</th>
+              </tr>
+              <tr>
+                <td>Non-refundable Deposit</td>
+                <td>01/11/15</td>
+                <td>£10.00</td>
+                <td>£10.00</td>
+              </tr>
+              <tr>
+                <td>Final payment</td>
+                <td>19/05/16</td>
+                <td>£10.00</td>
+                <td>£20.00</td>
+              </tr>
+            </table>
+
+
+            <h2>Marwell Activity Centre</h2>
+            <p>Thursday 28th and Friday 29th April</p>
+            <p>Lead Teacher – Miss French</p>
+            <p>Cost - £105.00</p>
+            <table>
+              <tr>
+                <th>Marwell</th>
+                <th>Payment by</th>
+                <th>Amount</th>
+                <th>Total</th>
+              </tr>
+              <tr>
+                <td>Non-refundable Deposit</td>
+                <td>15/01/16</td>
+                <td>£50.00</td>
+                <td>£50.00</td>
+              </tr>
+              <tr>
+                <td>First Instalment</td>
+                <td>12/02/16</td>
+                <td>£30.00</td>
+                <td>£80.00</td>
+              </tr>
+              <tr>
+                <td>Final payment</td>
+                <td>18/03/16</td>
+                <td>£25.00</td>
+                <td>£105.00</td>
+              </tr>
+            </table> 
+
+
+            <h2>Rock Challenge</h2>
+            <p>Monday 14th March 2016</p>
+            <p>Lead Teacher – Miss Chuter</p>
+            <p>Cost - £20.00</p>
+            <p>Payment is due before trip date</p>
+
+
+            <h2>London Theatre Trip</h2>
+            <p>Wednesday 10th February 2016</p>
+            <p>Lead Teacher – Miss Chuter</p>
+            <p>Cost £60.00</p>
+            <table>
+              <tr>
+                <th>London Theatre Trip</th>
+                <th>Payment by</th>
+                <th>Amount</th>
+                <th>Total</th>
+              </tr>
+              <tr>
+                <td>Non-refundable Deposit</td>
+                <td>18/12/15</td>
+                <td>£30.00</td>
+                <td>£30.00</td>
+              </tr>
+              <tr>
+                <td>Final payment</td>
+                <td>02/02/16</td>
+                <td>£30.00</td>
+                <td>£60.00</td>
+              </tr>
+            </table>
+
+
+            <h2>Portrait Gallery Trip</h2>
+            <p>Monday 1st February 2016</p>
+            <p>Lead Teacher – Miss Loake</p>
+            <p>Cost - £7.50</p>
+            <p>Payment is due before trip date</p>
+
+
+            <a class="button radius success" href="//www.scopay.com/pcs" target="_blank">Pay Online</a>
+
+        </div>
+        
+    </div><br>
+</section>
+    
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
