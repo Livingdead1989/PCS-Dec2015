2a3007b5f46f57f494a0a8f908bd6980efc7a2ca
s.smith2 <s.smith2@it-services-ssi.park.internal>
2016-03-08 15:11:32 +0000

    Draft gym page created and images added

---
+++ gym.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Gym - Fitness Suite</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-misc">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-misc">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>The Fitness Suite</h1>
+            <p class="text-capitalize">Feel the burn</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-8 columns">
+
+        </div>
+        <div class="small-offset-2 small-pull-2 small-8 medium-pull-0 medium-offset-0 medium-4 columns">
+          <img src="images/gym/gym-01.jpg" alt="The Gym - Brand new matrix machines">
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-12 columns">
+          <img src="images/gym/gym-02.jpg" alt="The Gym - Weight training">
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-12 columns">
+          <h4>Fitness Club</h4>
+
+        </div>
+    </div><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
