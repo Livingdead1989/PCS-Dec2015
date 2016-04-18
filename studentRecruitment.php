ef4e573e31c7db5dbf5b55b2fb8332f242de4c58
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-04-18 11:43:27 +0100

    Added Student Recruitment page and topnav item

---
+++ studentRecruitment.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Student Recruitment</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-school">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-school">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Student Recruitment</h1>
+            <p class="text-capitalize">Valuable Work Experience</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <div class="large-12 columns">
+            <p>At Park we recognise how difficult it is for young people to gain valuable work experience so we are opening our own Recruitment Agency for positions that can be filled by our students.</p>
+
+            <p>Students will go through a recruitment process including completing an application form in which they will need to obtain references, group task and interview. Vacancies will be advertised outside KD3.</p>
+
+            <p>The vacancies that will occur show the progression that can be made, through the different roles. More roles will be added as they become available.</p>
+
+            <p>The law states that children must be 13 years of age to be employed therefore Year 7/8s can only volunteer, they will get a gift voucher and it will be recognised in their application for positions in Year 9.</p>
+
+        </div>
+    </div>
+</section>
+<!-- Jobs -->
+<section>
+    <div class="row" data-equalizer data-equalizer-mq="large-up">
+        <br>
+        <div class="medium-push-1 medium-10 large-8 large-push-0 columns">
+            <div class="panel" data-equalizer-watch>
+                <h4>Available Positions</h4>
+                <ul class="no-bullet">
+                  <li>Printshop Helper<br><span class="subheader"><a href="assets/studentRecruitment/PrintShopStudentHelper.docx">Position Details</a></span></li>
+                  <li>Staunton Country Park<br><span class="subheader"><a href="assets/studentRecruitment/StauntonCountryPark.docx">Position Details</a></span></li>
+                  <li>Animal Carer - Term Time<br><span class="subheader"><a href="assets/studentRecruitment/StudentAnimalCarer-TermTime.docx">Position Details</a></span></li>
+                  <li>Community Assistant<br><span class="subheader"><a href="assets/studentRecruitment/StudentCommunityAssistant.docx">Position Details</a></span></li>
+                  <li>Litter Collector<br><span class="subheader"><a href="assets/studentRecruitment/StudentLitterCollector.docx">Position Details</a></span></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="medium-push-1 medium-10 end large-4 large-pull-8 columns">
+            <div class="panel" data-equalizer-watch>
+                <h4>Documentation</h4>
+                    <ul class="no-bullet">
+                        <li class="subheader">Information</li>
+                        <li><a href="assets/studentRecruitment/documentation/positionsandPay.docx" target="_blank">Positions &amp; Pay</a></li>
+                    </ul>
+                    <ul class="no-bullet">
+                        <li class="subheader">Forms<br><small>Downloadable Word Documents</small></li>
+                        <li><a href="assets/studentRecruitment/documentation/application-form.docx" target="_blank">Application Form</a></li>
+                    </ul>
+            </div>
+        </div>
+
+    </div>
+    <br>
+</section>
+<!-- Terms -->
+<section>
+    <div class="row">
+        <div class="large-12 columns">
+            <p>Once a Student is successful they will receive the necessary training and equipment. In order to be paid they will require a bank account.</p>
+
+            <p>Please fill in the application form from the list above and return completed applications <a href="javascript:location='mailto:\u0053\u0074\u0075\u0064\u0065\u006e\u0074\u0052\u0065\u0063\u0072\u0075\u0069\u0074\u006d\u0065\u006e\u0074';void 0"><script type="text/javascript">document.write('\u0053\u0074\u0075\u0064\u0065\u006e\u0074\u0052\u0065\u0063\u0072\u0075\u0069\u0074\u006d\u0065\u006e\u0074\u0020\u0040\u0020\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b')</script></a>.</p>
+            <br>
+        </div>
+    </div>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
