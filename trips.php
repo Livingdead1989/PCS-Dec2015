89dbdbb5024c06bbc495dd9e9b09870d00f00165
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-04-21 14:54:44 +0100

    Updated Trips

--- trips.php
+++ trips.php
@@ -26,6 +26,66 @@
         <div class="medium-12 columns">
 
         <ul class="medium-block-grid-1 large-block-grid-2">
+
+            <li>
+                <h3>Disneyland Paris 2017 - INVITE ONLY</h3>
+                <p>Tuesday 7th February to Friday 10th February 2017</p>
+                <p>Lead Teacher – Mrs Butler</p>
+                <p>Cost - £280.00</p>
+                <hr>
+
+                <table>
+                  <tr>
+                    <th>Disneyland Paris</th>
+                    <th>Payment<br>by</th>
+                    <th>Amount</th>
+                    <th>Total</th>
+                  </tr>
+                  <tr>
+                    <td>Non-refundable<br>Deposit</td>
+                    <td>29/04/16</td>
+                    <td>£70.00</td>
+                    <td>£210.00</td>
+                  </tr>
+                  <tr>
+                    <td>First<br>payment</td>
+                    <td>31/05/16</td>
+                    <td>£35.00</td>
+                    <td>£175.00</td>
+                  </tr>
+                  <tr>
+                    <td>Second payment</td>
+                    <td>30/06/16</td>
+                    <td>£35.00</td>
+                    <td>£140.00</td>
+                  </tr>
+                  <tr>
+                    <td>Third payment</td>
+                    <td>20/07/16</td>
+                    <td>£35.00</td>
+                    <td>£105.00</td>
+                  </tr>
+                  <tr>
+                    <td>Fourth payment</td>
+                    <td>07/09/16</td>
+                    <td>£35.00</td>
+                    <td>£70.00</td>
+                  </tr>
+                  <tr>
+                    <td>Fifth payment</td>
+                    <td>03/10/16</td>
+                    <td>£35.00</td>
+                    <td>£35.00</td>
+                  </tr>
+                  <tr>
+                    <td>Final payment</td>
+                    <td>01/11/16</td>
+                    <td>£35.00</td>
+                    <td>-</td>
+                  </tr>
+                </table>
+            </li>
+
             <li>
                 <h3>Apple Pi Filming, York</h3>
                 <p>Monday 28th June – Saturday 2nd July 2016</p>
@@ -55,7 +115,7 @@
             </li>
 
         </ul>
-        
+
         <p>Payments can now be made Online via <a href="//www.scopay.com/pcs" target="_blank">SCO Online Payments</a></p>
         <a class="button radius success" href="//www.scopay.com/pcs" target="_blank">Pay Online</a>
 
