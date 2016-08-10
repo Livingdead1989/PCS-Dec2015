a2792b4e25abe31db24c0b1d2a2024a75e81b820
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-08-10 10:48:17 +0100

    Apex page copy changes and addition photo

--- apex-centre.php
+++ apex-centre.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en" >
     <head>
-        
+
         <title>Park Community School | Apex Centre</title>
         <?php include("includes/head.html") ?>
     </head>
@@ -26,6 +26,7 @@
         <div class="medium-8 columns">
             <p>We are keen to introduce practical, hands-on courses for students who wish to work in the trades industries, whilst ensuring that curriculum requirements are met.</p>
             <p>The recent government changes to school examinations and accreditations dictate that from 2016 non-academic courses have no ‘league table value’ to schools. A consequence of this is that schools now need to ensure any time spent on vocational learning is highly relevant to the student’s needs.</p>
+            <p>Park Community Enterprises Ltd have leased facilities named The Apex Centre, to accommodate this. This Centre is situated to the north east of the central shopping district of Leigh Park and is within fifteen minutes walking distance from Park Community School. It is within the school’s catchment area. The Apex Centre is just two minutes walk from the industrial unit we already lease.</p>
         </div>
         <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-4 columns"><img src="images/apex/apex-01.jpg" alt="Apex Centre - Trade Skills for Life"></div>
     </div>
@@ -38,7 +39,19 @@
     </div>
     <div class="row">
         <div class="medium-push-4 medium-8 columns">
-            <p>Park Community Enterprises Ltd have leased facilities named The Apex Centre, to accommodate this. This Centre is situated to the north east of the central shopping district of Leigh Park and is within fifteen minutes walking distance from Park Community School. It is within the school’s catchment area. The Apex Centre is just two minutes walk from the industrial unit we already lease.</p>
+            <p>The student cab undertake a construction skills course leading to a Laser Level 1 in Construction Skills. The qualification is a Certification for Learning, Employability and Progression (LEAP). The course will take 60 sessions over a 2-year period to complete depending on ability.</p>
+            <p>The units of study will include:</p>
+            <ol>
+              <li>Brickwork/Blockwork</li>
+              <li>Painting</li>
+              <li>Carpentry</li>
+              <li>Plastering</li>
+              <li>Construction</li>
+              <li>Health and Safety</li>
+              <li>Measurements of distance and length</li>
+            </ol>
+            <p>We believe we are best placed to address these needs of our students and other providers who are not yet aware of the implications of the forthcoming changes.</p>
+
         </div>
         <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-8 medium-offset-0 medium-4 columns"><img src="images/apex/apex-02.jpg" alt="Park Community School Apex Centre - Bricklaying, Plastering, Tiling, Painting and Car Mechanics"></div>
     </div>
@@ -54,21 +67,16 @@
             <h4>Address</h4>
             <hr>
             <ul class="no-bullet">
-                    <li>The Apex Centre</li>
-                    <li>Bishopstoke Road</li>
-                    <li>Havant</li>
-                    <li>PO9 5BN</li>
-                </ul>
-            <h4>Refurbishment</h4>
-            <hr>
-            <p>After refurbishing the unit, our intention is to be in the position to offer learning opportunities such as bricklaying, plastering, tiling, painting and car mechanics.</p>
-            
-            
+              <li>The Apex Centre</li>
+              <li>Bishopstoke Road</li>
+              <li>Havant</li>
+              <li>PO9 5BN</li>
+            </ul>
         </div>
         <div class="hide-for-small-only medium-pull-0 medium-offset-0 medium-4 columns"><img src="images/apex/apex-03.jpg" alt="Apex Centre - Continuing Education"></div>
     </div><br>
 </section>
-    
+
 <!-- FOOTER -->
 <?php include("includes/footer.html") ?>
 
