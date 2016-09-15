7c38eb65c0068e5a6f3e78e1fd4c8116c7af91e5
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-09-15 11:46:20 +0100

    Updates to RE curriculum

---
+++ religious-education.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Religious Education</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-learning">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-learning">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Religious Education</h1>
+            <p class="text-capitalize">Beliefs &amp; Culture</p>
+        </div>
+    </div>
+</header>
+<!-- Breadcrumb -->
+<div class="row">
+    <ul class="breadcrumbs" role="menubar" aria-label="breadcrumbs">
+        <li role="menuitem"><a href="/curriculum.php">Curriculum</a></li>
+        <li role="menuitem" class="current"><a href="#">Religious Education</a></li>
+    </ul>
+</div>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <div class="large-12 columns">
+        <br>
+            <h3>Purpose</h3>
+            <p>The purpose of religious education (R.E) in Hampshire, Portsmouth and Southampton is to support students in developing their own coherent patterns of values and principles, and to support their spiritual, moral, social, and cultural development. In Park Community School the name ‘Beliefs and Cultures’ is used to signify our focus on the development of personal patterns of values in the light of contemporary ethical and social issues facing our society and culture, alongside an exploration of the culture and values of others.</p>
+
+            <p>The agreed syllabus which provides the legal framework for R.E states that “we have to understand how and why people live differently from each other and respect their right to do so. Living Difference identifies that respecting difference requires us to engage with difference confidently. We need to identify how and why we have different beliefs, attitudes and practices from other people. By doing this, we can gain respect from others. Accepting difference does not imply agreement; nor does it imply that all difference is acceptable. It is important that students make reasoned and informed judgements about difference and that these are based on beliefs and values that they sincerely hold, and that they can express and defend in reasoned debate. Teachers have a responsibility for helping students to develop their beliefs and values, in relation to those values that society prizes. As a result, the agreed syllabus places particular importance upon the spiritual, moral, social and cultural development of students."</p>
+            <cite>Agreed Syllabus, ‘Living Difference’, for Hampshire, Portsmouth and Southampton</cite>
+            <p>The purpose of Beliefs and Cultures at Park is therefore to promote pupils spiritual, moral, social and cultural development through an awareness and appreciation of diversity and difference of beliefs and values, cultures and societies within the UK and the wider world. As part of this pupils are offered the chance to respond and in so doing develop their own sense of self and place in the local, national and global community.</p>
+
+            <h3>Aims</h3>
+            <ul class="circle">
+                <li>Encourage each student to interpret and respond to a variety of concepts, beliefs and practices within religions and to their own and others’ cultural and life experiences.</li>
+                <li>Students will develop the capacities to interpret, evaluate and respond to differing values and beliefs.</li>
+                <li>This can be achieved through extending their thinking and analytical skills and their creative, imaginative and emotional development.</li>
+                <li>B.C aims to foster mutual understanding between students of differing religious and cultural backgrounds.</li>
+                <li>B.C aims to promote an awareness of differing understandings of human experience and ways in which religions view the purpose of life.</li>
+                <li>Pupils and students are encouraged to develop their own concepts to interpret their own experiences, and explain the experiences of others in the wider world.</li>
+                <li></li>
+            </ul>
+
+            <h3>Areas of Study</h3>
+            <h5>Year 7</h5>
+            <ul class="circle">
+                <li><strong>The Island</strong> - Pupils explore concepts such as community and tradition common to many religious traditions and human experiences. They are offered the opportunity to explore what these concepts mean to them as individuals and the society in which they live.</li>
+                <li><strong>Judaism</strong> - Pupils will examine how this world religion impacts on the life of Jewish communities in the U.K.</li>
+                <li><strong>Pilgrimage</strong> - Pupils will explore the value and significance of pilgrimage to a range of faiths.  They will go on to enquire into secular pilgrimage and the role it can play in the lives of those with no religious faith.</li>
+            </ul>
+
+            <h5>Year 8</h5>
+            <ul class="circle">
+              <li><strong>Being Human</strong> - Pupils will explore some of the big questions facing human beings such as, why are we here? Is there a purpose to existence? They will begin to examine arguments for and against the existence of God. Pupils will be encouraged to explore and form their own responses to these questions.</li>
+              <li><strong>Christianity</strong> – Pupils will examine the significance of the concepts of ‘sacrifice’ and ‘salvation’ within Christianity through the story of ‘The Lion, the Witch and the Wardrobe’.</li>
+              <li><strong>Islam</strong> - Pupils will take an in-depth look at Islam followed by an investigation into a selection of ethical issues facing society today and how these might be interpreted within, and beyond, Islam.</li>
+            </ul>
+
+            <h5>Year 9</h5>
+            <ul class="circle">
+              <li><strong>Religion and Relationships</strong> - Students explore a range of topics within this theme including gender equality, marriage and same-sex relationships.</li>
+              <li><strong>Hinduism: The Nature of God</strong> - Students will explore what ‘God’ means within this faith tradition.  They will look at how this impacts on believers’ daily life and on beliefs about life after death.</li>
+              <li><strong>Christianity: The Nature of God</strong> - Students go on to explore how beliefs within Christianity contrast with those of Hinduism.</li>
+            </ul>
+
+            <h5>Year 10</h5>
+            <ul class="circle">
+              <li><strong>Christianity: Life and Death</strong> - Students will explore Christian beliefs about the afterlife before applying this learning to debates about euthanasia and abortion.</li>
+              <li><strong>Hinduism: Life and Death</strong> - This unit looks at Hindu beliefs about life and death and how these beliefs impact on how Hindu’s choose to interact with the local and global community.</li>
+              <li><strong>Religion in the U.K</strong> – Pupils will examine how religious faith impacts the lives of individuals and communities within the U.K.  This learning will then be applied to a range of contemporary issues facing society.</li>
+            </ul>
+
+
+            <h5>Year 11</h5>
+            <ul class="circle">
+              <li><strong>Animal Testing</strong> - Animal testing is widely used in the UK for both medical and cosmetic purposes. Students will consider different religious responses to this question before having the opportunity to develop their own personal response.</li>
+              <li><strong>Capital Punishment</strong> - This unit focuses on the debate as to whether the death penalty should be brought back to the UK.</li>
+              <li><strong>Religion and the media</strong> – Students explore issues surrounding the use of the media by religions, and the portrayal of religious ideas by the media.  They will also consider what, if any, restrictions and controls government should have over the freedom of the media.</li>
+            </ul>
+            <br>
+        </div>
+
+    </div>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
