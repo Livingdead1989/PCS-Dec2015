b7adbf7a0a2835b77819c0330e1b802915332707
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-09-16 15:19:56 +0100

    New homework ]page added with assets and update to topnav

---
+++ homework.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Homework</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-learning">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-learning">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Homework</h1>
+            <p class="text-capitalize">Learning is for life</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-7 columns">
+            <p>Homework is a significant way through which we increase students’ <strong>Understanding</strong> and <strong>Mastery</strong> of content and skills, and raise standards.</p>
+
+            <p>The expectations of new exam syllabuses and a move to terminal examinations in all subjects over the next three years, means that students must be supported to consolidate, review and revise learning outside of lessons in order to fully master the knowledge and skills they require. Homework is a vital part of this.</p>
+
+            <div class="text-center">
+              <a class="small button radius success" href="assets/homework/homework-timetable.pdf" target="_blank">Homework Timetable</a>
+            </div>
+        </div>
+        <div class="small-offset-2 small-pull-2 small-8 medium-pull-0 medium-offset-0 medium-5 columns"><img src="images/general/general-23.jpg" alt="Park Community School - Understanding and Mastery of Subjects"></div>
+    </div>
+    <br><br><br>
+</section>
+
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-12 columns">
+            <h3>Purpose of Homework</h3>
+            <ul class="circle">
+              <li>Set a clear expectation that students must work independently to truly master a given body of knowledge or skills set</li>
+              <li>Consolidate learning from the classroom through further independent application, practise or revision</li>
+              <li>Prepare students for new learning through independent research and inquiry</li>
+              <li>Allow students to develop their learning in different or creative ways</li>
+              <li>Encourage students to explore an area in greater depth and develop a thirst for learning</li>
+            </ul>
+
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <br>
+        <div class="medium-8 columns">
+          <h3>Tips &amp; Tricks | <small>Student &amp; Parent</small></h3>
+          <p>Students are more successful in school when parents take an active interest in their homework — it shows students that what they do is important.</p>
+          <p>How can you support your child:</p>
+          <ul class="circle">
+            <li>Encourage students to value their homework and work hard</li>
+            <li>Ensure students have a quiet space where they can work</li>
+            <li>Set a regular slot for homework after school</li>
+            <li>Help your child manage their time</li>
+            <li>Keep distractions to a minimum</li>
+            <li>Praise their work and efforts</li>
+            <li>Daily reading is essential</li>
+            <li>Access MILK to view homework assignments</li>
+          </ul>
+          <h4 class="text-capitalize">The Internet has lots of helpful resources</h4>
+          <p>If your child has forgotten some facts or how to solve a maths problem, there will always be a <a href="//www.youtube.com/channel/UC3yA8nDwraeOfnYfBWun83g" target="_blank">YouTube</a> video on it that you can watch together.</p>
+
+        </div>
+        <div class="medium-pull-0 medium-offset-0 medium-4 columns"><br class="show-for-medium-up"><img class="hide-for-small" src="images/general/general-12.jpg" alt="Park Community School - Encourage students to value their work"></div>
+    </div>
+    <br><br><br>
+</section>
+
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <br>
+        <div class="medium-12 columns">
+          <h3>Homework Documents</h3>
+          <p><strong>Venture Learning</strong> - In Years 7 and 8, students also receive three-week Venture Learning projects designed to offer choice, promote independence and the chance to explore ideas creatively. These projects are timetabled across the year to ensure students have the chance to complete them for all subjects.</p>
+          <ul class="circle">
+            <li><a href="assets/homework/VL-Year7Timetable.pdf" target="_blank">Venture Learning Year 7 Timetable</a></li>
+            <li><a href="assets/homework/VL-Year8Timetable.pdf" target="_blank">Venture Learning Year 8 Timetable</a></li>
+          </ul>
+
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <br>
+        <div class="medium-8 columns">
+          <h3>Clubs and Support</h3>
+          <p>Students are welcome to stay in school on any day to use facilities to complete homework.</p>
+          <p>Study clubs and homework support clubs are available in all subjects</p>
+          <p>Year 7-8 homework club is run by Communitas EVERY day for one hour after school</p>
+          <br>
+
+          <h4 class="text-capitalize">Forgotten your homework?</h4>
+          <p><a href="//my.milkapp.io/" target="_blank">MILK</a> provides an electronic version of homework which can be accessed from home</p>
+        </div>
+
+        <div class="medium-pull-0 medium-offset-0 medium-4 columns">
+          <h3>External Resources</h3>
+          <ul class="circle">
+            <li><a href="//www.understood.org/en/school-learning/learning-at-home/homework-study-skills/7-tips-for-improving-your-childs-homework-and-study-skills" target="_blank">Top 10 Homework Tips</a></li>
+            <li><a href="//www.helpguide.org/articles/learning-disabilities/helping-children-with-learning-disabilities.htmhttp:/www.helpguide.org/articles/learning-disabilities/helping-children-with-learning-disabilities.htm" target="_blank">7 Tips for Improving Your Child’s Homework and Study Skills</a></li>
+            <li><a href="//www.bdadyslexia.org.uk/parent/homework-tips" target="_blank">Helping Children with Learning Disabilities</a></li>
+            <li><a href="//www.bdadyslexia.org.uk/parent/homework-tips" target="_blank">British Dyslexia Association</a></li>
+            <li><a href="//www.bbc.co.uk/education/levels/z4kw2hv" target="_blank">Keystage 3 Bitesize</a></li>
+            <li><a href="//www.bbc.co.uk/schools/gcsebitesize/" target="_blank">GCSE Bitesize</a></li>
+          </ul>
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
