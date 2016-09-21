59bc1d1934626f079bbeb44e06c4959550770039
Phil Warren <P.Warren@pcs.hants.sch.uk>
2016-09-21 13:22:07 +0100

    Update to Admissions Page

--- admissions.php
+++ admissions.php
@@ -58,10 +58,8 @@
     </div>
     <div class="row">
         <div class="medium-push-4 medium-8 columns">
-            <h4>Year 7 September 2015</h4>
-            <p>Year 7 placements for 2015 have now closed.</p>
-            <br><br>
-            <p>You can find out more about this process or what to do if you are unhappy with your allocated place for September 2015 at: <a href="//www3.hants.gov.uk/education/admissions/ad-guidance/main-admissions-round/ed-ad-mainround-process.htm" target="_blank">www.Hants.gov</a></p>
+            <h4>Admissions Process</h4>
+            <p>You can find out more about this process or what to do if you are unhappy with your allocated place, at: <a href="//www3.hants.gov.uk/education/admissions/ad-guidance/main-admissions-round/ed-ad-mainround-process.htm" target="_blank">www.Hants.gov</a></p>
             <p>If you have any questions or would like any help with any of the above, please feel free to contact our admissions team at: <a href="javascript:location='mailto:\u0061\u0064\u006d\u0069\u0073\u0073\u0069\u006f\u006e\u0073\u0040\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b';void 0"><script type="text/javascript">document.write('\u0061\u0064\u006d\u0069\u0073\u0073\u0069\u006f\u006e\u0073\u0040\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b')</script></a></p>
         </div>
         <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-8 medium-offset-0 medium-4 columns"><img src="images/general/general-13.jpg" alt="Park Community School student selecting her library books"></div>
