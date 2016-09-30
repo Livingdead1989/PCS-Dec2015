e4fd31b6562643199f9c85326565946745b57060
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-09-30 14:41:35 +0100

    Added Kitchen Porter Job Advert

--- employment.php
+++ employment.php
@@ -40,7 +40,8 @@
             <div class="panel" data-equalizer-watch>
                 <h4>Support Roles</h4>
                 <ul class="no-bullet">
-                  <li>No positions currently available <br><span class="subheader">please check back later.</span></li>
+                  <!-- <li>No positions currently available <br><span class="subheader">please check back later.</span></li> -->
+                  <li>Kitchen Porter<br><span class="subheader"><a href="assets/recruitment/KitchenPorter.pdf" target="_blank">Position Details</a></span></li>
                   <!-- <li>Position<br><span class="subheader"><a href="assets/recruitment/file.pdf" target="_blank">Position Details</a></span></li> -->
                   </ul>
             </div>
