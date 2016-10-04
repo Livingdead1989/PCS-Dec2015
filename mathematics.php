3f3065f7acca8742c0c031ab6197756567f5b9bc
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-10-04 10:52:04 +0100

    Changes to maths curriculum and added roadmap pdf

--- mathematics.php
+++ mathematics.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en" >
     <head>
-        
+
         <title>Park Community School | Mathematics</title>
         <?php include("includes/head.html") ?>
     </head>
@@ -32,80 +32,57 @@
         <div class="large-12 columns">
         <br>
             <h3>Purpose</h3>
-            <p>Mathematics is a creative and highly inter-connected problem solving discipline developed over centuries. Through history, mathematics has been essential for solving some of the most intriguing problems and today it is just as necessary for everyday life. Although we now have technological advances to solve many difficult calculations, these are a direct result of mathematical knowledge and the need for mathematical thinking in science, technology and engineering.</p>
-
-            <p>The mathematics curriculum provides a foundation for understanding, develops reasoning ability, and encourages enjoyment and curiosity.</p>
+            <p>Mathematics is a creative and highly inter-connected problem solving discipline developed over centuries. Through history, mathematics has been essential for solving some of the most intriguing problems and today it is just as necessary for everyday life. Although we now have technological advances to solve many difficult calculations, these are a direct result of mathematical knowledge; the need for mathematical thinking in science, technology and engineering, continues to demonstrate its power and importance. The mathematics curriculum provides a foundation for understanding and develops reasoning ability, whilst also developing enjoyment and curiosity for the subject.</p>
 
             <h3>Aims</h3>
-            <p>We aim to develop pupils’ understanding of key mathematical concepts and processes in order to enable pupils to solve problems using a variety of techniques and skills. This requires pupils to be fluent in the fundamentals of mathematics and to reason mathematically. This happens through repeated practise to increase fluency and mastery, application of skills to familiar and unfamiliar contexts, learning of formulae, application to calculations, analysis and interpretation of mathematical information, as well as contextualising and applying appropriate elements to situations within which pupils are familiar.</p>
-            
+            <p>We aim to develop students’ understanding of key mathematical concepts and processes to enable students to solve problems using a variety of techniques and skills. This requires students to be fluent in the fundamentals of mathematics and to reason mathematically. This happens through repeated practise to increase fluency and mastery, application of skills to familiar and unfamiliar contexts, learning of formulae, application to calculations, analysis and interpretation of mathematical information, as well as contextualising and applying appropriate elements to situations within which students are familiar.</p>
+
             <h3>Areas of Study</h3>
             <ul class="circle">
                 <li>Number</li>
                 <li>Algebra</li>
                 <li>Ratio, Proportion and Rates of Change</li>
-                <li>Geometry and measures: learn and apply formulae in order to calculate problems.</li>
+                <li>Geometry and measures: learn and apply formulae solve problems.</li>
                 <li>Probability</li>
                 <li>Statistics</li>
             </ul>
-            <p><strong>Year 7</strong> – All year 7 pupils start secondary mathematics with strong links to STEM (Science Technology Engineering and Mathematics).  This enables pupils to develop a depth of understanding for core operations + - × ÷ with strong links to physical application to support their understanding of core concepts.</p>
-            <p><strong>Key Stage 3</strong> - Throughout Key Stage 3, all pupils study mathematics covering the full spectrum of Number, Calculating, Algebra, Geometry and Statistics. These sit alongside Mathematical Processes and Applications and Functional mathematics in order to develop independent mathematicians with skills transferable to other subject areas.</p>
-            <p><strong>Key Stage 4</strong> - All pupils are studying a Linear GCSE mathematics course set by Edexcel. This is a two year course with two examination papers in the summer of Year 11, one of which is a calculator paper and the other a non-calculator paper. These will test the core skills of Number, Algebra, Geometry, Statistics, Probability and Ratio.</p>
+            <p><strong>Years 7 - 11</strong> – from years 7 to 11 we follow a single programme of study leading to a GCSE in mathematics.</p>
+            <p><strong>Years 7 - 8</strong> - our curriculum is designed to equip students with the mathematical knowledge and skill to successfully embark on their GCSE course in Year 9. It covers the full mathematical spectrum of Number, Ratio, Algebra, Geometry, Statistics and Probability required for their GCSE. We help the students achieve mastery in the use of this knowledge by emphasising the importance of using mathematical reasoning when solving problems in many different contexts.</p>
+            <p><strong>Years 9 - 11</strong> - our curriculum continues to build towards GCSE mathematics.  The syllabus we follow is Edexcel with students either following the foundation or higher specification.</p>
+
+            <p>View our <a href="assets/curriculum/Curriculum-Roadmap.pdf" target="_blank">Curriculum Roadmap</a></p>
 
             <ul class="accordion" data-accordion role="tablist">
 
                 <li class="accordion-navigation">
-                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">Keystage 3 - National Curriculum - Collins Mathematics</a>
+                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">The foundation course, grade 1-5</a>
                     <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Number</li>
-                            <li>Algebra</li>
-                            <li>Shape</li>
-                            <li>Data</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Our two year KS3 course is designed to ensure our pupils have the full tool kit they need to tackle GCSE Mathematics.</li>
-                            <li>It builds on their numeracy and starts to develop their ability to use and manipulate algebra.</li>
-                            <li>It also continues to develop their skills in data handling and shape, space and measure.</li>
-                            <li>Functional Mathematics is built into the course and is also the focus of a dedicated lesson every half term.</li>
-                        </ul>
-
-                        <small>For more information please visit the <a href="http://www.collins.co.uk/category/Secondary/Maths#KS3" target="_blank">Collins Education website</a></small>
+                        <p>The foundation course covers a range of topics in Number, Algebra, Shape and Data handling. It emphasises students’ ability to reason with and use these skills in unfamiliar contexts in order for them to be able to apply them to real life problems.</p>
                     </div>
                 </li>
 
                 <li class="accordion-navigation">
-                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Keystage 4 - GCSE</a>
+                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">The higher course, grades 4-9</a>
                     <div id="panel2d" class="content" role="tabpanel" aria-labelledby="panel2d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Number</li>
-                            <li>Algebra</li>
-                            <li>Shape</li>
-                            <li>Data</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>We teach the Edexcel linear course with pupils either following the foundation or higher specification. </li>
-                            <li>The foundation course covers a range of topics in Number, Algebra, Shape and Data handling up to C grade. It emphasises pupils ability to use these skills and also be able to apply them to real life problems. </li>
-                            <li>The higher course takes these topics further so pupils can tackle more complex problems, as well as giving them the basis they need to use Mathematics beyond GCSE.</li>
-                        </ul>
-                        
-                        <small>For more information please visit the <a href="http://qualifications.pearson.com/en/qualifications/edexcel-gcses/mathematics-a-2010.html" target="_blank">Pearson website</a></small>
-
+                        <p>The higher course takes these topics further so students can tackle more complex and mathematically abstract problems, as well as giving them the basis they need to use Mathematics beyond GCSE.</p>
                     </div>
                 </li>
             </ul>
+            <br>
+            <p>The final qualification consists of three written examination papers:</p>
+            <ul class="circle">
+              <li>Paper 1 is a non-calculator assessment. A calculator is allowed for Paper 2 and Paper 3.</li>
+              <li>Each paper is 1 hour and 30 minutes long.</li>
+            </ul>
+
+            <p><strong>Note:</strong> The new GCSE syllabus is far more demanding than in previous years. To help you understand some of the changes to mathematics please see the <a href="//qualifications.pearson.com/en/home.html" target="_blank">Edexcel website</a> and ‘<a href="//qualifications.pearson.com/content/dam/pdf/GCSE/mathematics/2015/misc/T772%20GCSE%20Maths%20Guide%20(web%20-%20final).pdf" target="_blank">The guide to Edexcel GCSE Mathematics (9-1)</a>’
+
             <br>
         </div>
 
     </div>
 </section>
-    
+
 <!-- FOOTER -->
 <?php include("includes/footer.html") ?>
 
