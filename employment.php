365170ce0c8b0da99589bfacbc020c91bfa3624b
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2016-10-10 09:40:50 +0100

    Combined KS4 Results PDF and newly updated Employment/HR page

--- employment.php
+++ employment.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en" >
     <head>
 
-        <title>Park Community School | Work for us</title>
+        <title>Park Community School | You could make a difference</title>
         <?php include("includes/head.html") ?>
     </head>
 <body class="page-school">
@@ -14,75 +14,97 @@
     <div class="row">
         <div class="medium-8 large-8 columns">
             <h1>Employment</h1>
-            <p class="text-capitalize">Job Opportunities, come join the family!</p>
+            <p class="text-capitalize">You could make a difference...</p>
         </div>
     </div>
 </header>
 
 <!-- CONTENT -->
-<!-- Jobs -->
 <section>
-    <div class="row" data-equalizer data-equalizer-mq="large-up">
-        <br>
-        <div class="medium-push-1 medium-10 large-4 large-push-0 columns">
-            <div class="panel" data-equalizer-watch>
-                <h4>Teaching Roles</h4>
-                <ul class="no-bullet">
-                  <!-- <li>Position<br><span class="subheader"><a href="assets/recruitment/file.pdf" target="_blank">Position Details</a></span></li> -->
-                  <li>No positions currently available <br><span class="subheader">please check back later.</span></li>
-                </ul>
-                <hr>
-                <h5><small>Information about School Direct visit <br><a href="//www.thesolentscitt.co.uk/" target="_blank">www.theSolentSCITT.co.uk</a></small></h5>
-            </div>
-        </div>
-
-        <div class="medium-push-1 medium-10 large-4 large-push-4 columns">
-            <div class="panel" data-equalizer-watch>
-                <h4>Support Roles</h4>
-                <ul class="no-bullet">
-                  <!-- <li>No positions currently available <br><span class="subheader">please check back later.</span></li> -->
-                  <li>Kitchen Porter<br><span class="subheader"><a href="assets/recruitment/KitchenPorter.pdf" target="_blank">Position Details</a></span></li>
-                  <!-- <li>Position<br><span class="subheader"><a href="assets/recruitment/file.pdf" target="_blank">Position Details</a></span></li> -->
-                  </ul>
-            </div>
-        </div>
-
-        <div class="medium-push-1 medium-10 end large-4 large-pull-4 columns">
-            <div class="panel" data-equalizer-watch>
-                <h4>Documentation</h4>
-                    <ul class="no-bullet">
-                        <li class="subheader">Information</li>
-                        <li><a href="assets/recruitment/documentation/applicant-information.pdf" target="_blank">Applicant Information</a></li>
-                        <li><a href="assets/recruitment/documentation/staff-dress-code.pdf" target="_blank">Staff Dress Code</a></li>
-                        <li><a href="assets/recruitment/documentation/privacy-statement.pdf" target="_blank">Privacy Statement</a></li>
-                    </ul>
-                    <ul class="no-bullet">
-                        <li class="subheader">Forms<br><small>Downloadable Word Documents</small></li>
-                        <li><a href="assets/recruitment/documentation/equalities-form.docx" target="_blank">Equalities Form</a></li>
-                        <li><a href="assets/recruitment/documentation/teaching-application-form.docx" target="_blank">Teaching Application Form</a></li>
-                        <li><a href="assets/recruitment/documentation/support-application-form.docx" target="_blank">Support Application Form</a></li>
-                    </ul>
-                    <hr>
-                    <h5><small>Note: we cannot accept CVs as a form of application</small></h5>
-            </div>
-        </div>
-
-    </div>
+  <div class="row">
     <br>
+    <div class="small-12 medium-6 columns">
+      <h4 class="text-capitilise">Vacancies</h4>
+      <ul class="no-bullet">
+        <!-- <li>Position<br><span class="subheader"><a href="assets/recruitment/file.pdf" target="_blank">Position Details</a></span></li> -->
+        <li>Kitchen Porter - <span class="subheader"><a href="assets/recruitment/KitchenPorter.pdf" target="_blank">Position Details</a></span></li>
+        <!-- <li>No positions currently available <br><span class="subheader">please check back later.</span></li> -->
+      </ul>
+      <hr>
+      <h5><small>Information about School Direct visit <br><a href="//www.thesolentscitt.co.uk/" target="_blank">www.theSolentSCITT.co.uk</a></small></h5>
+      <div class="show-for-small-only"><br></div>
+    </div>
+    <div class="small-10 small-centered medium-uncentered medium-6 columns">
+      <img src="/images/general/general-24.jpg" alt="Park Community School - A happy place to be">
+    </div>
+
+  </div>
+  <br><br><br>
+</section>
+<!-- Jobs -->
+<section class="grey-bg">
+  <div class="row">
+      <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+  </div>
+  <div class="row">
+    <div class="large-12 columns">
+      <h4>Documentation</h4>
+      <ul class="medium-block-grid-2">
+        <li>
+          <ul>Information
+            <li><a href="assets/recruitment/documentation/applicant-information.pdf" target="_blank">Applicant Information</a></li>
+            <li><a href="assets/recruitment/documentation/staff-dress-code.pdf" target="_blank">Staff Dress Code</a></li>
+            <li><a href="assets/recruitment/documentation/privacy-statement.pdf" target="_blank">Privacy Statement</a></li>
+          </ul>
+        </li>
+        <li>
+          <ul>Forms | <small>Downloadable Word Documents</small>
+            <li><a href="assets/recruitment/documentation/equalities-form.docx" target="_blank">Equalities Form</a></li>
+            <li><a href="assets/recruitment/documentation/teaching-application-form.docx" target="_blank">Teaching Application Form</a></li>
+            <li><a href="assets/recruitment/documentation/support-application-form.docx" target="_blank">Support Application Form</a></li>
+          </ul>
+        </li>
+      </ul>
+      <hr>
+      <br>
+      <p>Note: for legal reasons, we cannot accept CVs as a form of application, and therefore ask all applicants to complete our application form and Equal Opportunities Monitoring Form. Completed forms can be sent to <a href="javascript:location='mailto:\u0072\u0065\u0063\u0072\u0075\u0069\u0074\u006d\u0065\u006e\u0074\u0040\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b';void%200">Recruitment @ pcs.hants.sch.uk</a>.</p>
+      <p>Park Community School and Hampshire County Council are committed to safeguarding and promoting the welfare of children and young people and expect all staff and volunteers to share this commitment. We will ensure that all our recruitment and selection practices reflect this commitment.</p>
+      <p>All successful candidates will be subject to satisfactory references and other relevant pre-employment checks including Disclosure and Barring Services (DBS) and medical clearance.</p>
+      <p>View our Child Protection Policy on our <a href="/policies.php">policies page</a>.</p>
+    </div>
+  </div>
+  <br><br><br>
 </section>
 <!-- Terms -->
 <section>
+  <div class="row">
+      <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+  </div>
     <div class="row">
-        <div class="large-12 columns">
-            <p>Please fill in the appropriate form from the list above and return completed applications to the HR Manager at the school address, or via email to <a href="javascript:location='mailto:\u0072\u0065\u0063\u0072\u0075\u0069\u0074\u006d\u0065\u006e\u0074\u0040\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b';void 0"><script type="text/javascript">document.write('\u0052\u0065\u0063\u0072\u0075\u0069\u0074\u006d\u0065\u006e\u0074\u0020\u0040\u0020\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b')</script></a>.</p>
+        <div class="small-12 medium-7 large-8 columns">
+            <h4>Human Resources Department</h4>
+            <p>The HR department is responsible for making sure Park Community School’s workforce are safe, healthy and happy (and in turn, able to help the students achieve the same).</p>
+            <p>From recruiting and onboarding new staff members to handling payroll and contractual queries, no two days are ever the same, and project work includes promoting staff wellbeing and spreading awareness of benefits and support schemes available to the workforce.</p>
+            <p>Donna Patterson, HR Manager, is a fully qualified Chartered Member of the CIPD, who stipulate a <a href="//www.cipd.co.uk/binaries/code-of-professional-conduct_july-2015.pdf?utm_medium=email&utm_source=cipd&utm_campaign=mem_renew&utm_term=164452&utm_content=Retention email 1%2C resources%2C mcipd%2C fcipd%2C assoc-7297-17819-20160930110922-Please_download_and_read_the_Code_of_Professional_Conduct" target="_blank">Professional Code of Conduct</a>.</p>
 
-            <p>Park Community School and Hampshire County Council are committed to safeguarding and promoting the welfare of children and young people and expect all staff and volunteers to share this commitment. We will ensure that all our recruitment and selection practices reflect this commitment.</p>
-
-            <p>All successful candidates will be subject to satisfactory references and other relevant pre-employment checks including Disclosure and Barring Services (DBS) and medical clearance.</p>
-            <p class="text-capitalize"><a href="/policies.php">View our Child Protection Policy on our policies page</a></p>
-            <br>
+        </div>
+        <div class="small-centered small-6 medium-uncentered medium-5 large-4 columns">
+          <img src="images/general/general-25.jpg" alt="Park Community School, much more than just a school" />
         </div>
     </div>
+    <div class="row">
+      <br>
+      <div class="large-12 columns">
+        <h5>Helpful Resources</h5>
+        <ul class="medium-block-grid-2 large-block-grid-3">
+          <li><a href="//www3.hants.gov.uk/hampshirecountycouncil/portal-help/hr/salaryexpensesbenefits.htm#step-2" target="_blank">HCC – Salary and Employee Benefits</a></li>
+          <li><a href="//www3.hants.gov.uk/education/educationjobshampshire.htm" target="_blank">Hampshire Jobs in Education</a></li>
+          <li><a href="//www.thesolentscitt.co.uk/" target="_blank">The Solent SCITT</a></li>
+        </ul>
+
+      </div>
+    </div>
+    <br><br><br>
 </section>
 
 <!-- FOOTER -->
