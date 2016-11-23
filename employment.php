613fb5a8b1504d4b803e97dd025f3293308f6c79
Phil Warren <P.Warren@pcs.hants.sch.uk>
2016-11-23 10:35:28 +0000

    Recruitment Adver Update

--- employment.php
+++ employment.php
@@ -28,7 +28,7 @@
       <ul class="no-bullet">
         <!-- <li>Position<br><span class="subheader"><a href="assets/recruitment/file.pdf" target="_blank">Position Details</a></span></li> -->
         <li>Science Teacher<br><span class="subheader"><a href="assets/recruitment/ScienceTeacher.pdf" target="_blank">Position Details</a></span></li>
-        <li>Behaviour Support and Guidance Manager <br><span class="subheader"><a href="assets/recruitment/BehaviourSupportandGuidanceManager.pdf" target="_blank">HCC Details</a></span><br><span class="subheader"><a href="assets/recruitment/HCCPupilSupportandGuidanceManager.pdf" target="_blank">HCC Details</a></span></li>
+        <li>Behaviour Support and Guidance Manager <br><span class="subheader"><a href="assets/recruitment/BehaviourSupportandGuidanceManager.pdf" target="_blank">Position Details</a></span><br><span class="subheader"><a href="assets/recruitment/HCCPupilSupportandGuidanceManager.pdf" target="_blank">HCC Role Profile</a></span></li>
 
         <!-- <li>No positions currently available <br><span class="subheader">please check back later.</span></li> -->
       </ul>
