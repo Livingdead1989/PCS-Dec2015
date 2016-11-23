0a3aec906762c9620f1895164d686c1636ae9c1a
Phil Warren <P.Warren@pcs.hants.sch.uk>
2016-11-23 09:45:05 +0000

    SMSC Update

---
+++ smsc.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+        
+        <title>Park Community School | SMSC</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-learning">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-learning">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>SMSC</h1>
+            <p class="text-capitalize"></p>
+        </div>
+    </div>
+</header>
+<!-- Breadcrumb -->
+<div class="row">
+    <ul class="breadcrumbs" role="menubar" aria-label="breadcrumbs">
+        <li role="menuitem"><a href="/curriculum.php">Curriculum</a></li>
+    </ul>
+</div>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <div class="large-12 columns">
+        <br>
+            <h3>Purpose</h3>
+            <p>To prepare students positively for life in modern Britain and promotes the fundamental British values of democracy, the rule of law, individual liberty and mutual respect for and tolerance of those with different faiths and beliefs and for those without faith. This means fully developing the whole child by helping students to grow and develop as people thereby preparing them for the adult world and allowing students to make sense of the world.</p>
+
+            <h3>Aims</h3>
+            <ul class="circle">
+            <li>Our curriculum offers daily opportunities for our pupils’ spiritual, moral, social and cultural development. Across all of the different subject areas, pupils are given time to explore their beliefs and reflect upon their reasoning.  At the heart of subject are core values centred on developing our students’ beliefs, allowing them to build their own character and helping them to become young adults who have well-considered values and ideas. </li>
+            </ul>
+
+            <h3>Areas of Study</h3>
+            <p><strong>Assemblies</strong> – At Park Community school we have regular assemblies where students gather together for a common purpose. The areas covered include a wide variety of which are closely matched in content to different areas of the curriculum.  There are common termly themes throughout the year which are underpinned by specific local, British or worldwide celebration and awareness days. This means that there is a coherence to the delivery of SMSC across the school as a community.</p>
+            <p><strong>Butterfly Broadcasts</strong> – This is approximately an 8 minute broadcast shown during the week in an allotted mentoring slot. It covers current issues and trending features as well as events and ideas that are developing across the school.  It is an important feature of the week in that it allows students to access current issues that they may not necessarily have any knowledge of.</p>
+            <p><strong>Mentoring</strong> – Mentors have a specific slot during the Tuesday amphitheatre time where they mentor students on a one to one basis.  These sessions will focus on academic achievement and progress as well as community and extra-curricular involvement. It is an essential part of life at Park as we believe that good solid relationships should be forged between at least one adult and student in their time at school.  This means that they always feel safe and supported in the school environment.</p>
+            <p><strong>Challenge days</strong> – Challenge days are whole days off curriculum where students learn and take part in activities that are over and above their normal working day. The topics covered include Britishness, tolerance, Sex and relationships education, citizenship, making informed choices with regards alcohol and drugs as well as career choices.  They get to listen to outside speakers with real life experiences in the fields that they are exploring which broadens their horizons in so that they are fully equipped to face the world when they leave school.</p>
+            <p><strong>A Rights Respecting School</strong> – In a Rights Respecting School the pupils have an opportunity to learn about the United Nations Convention on the Rights of the Child through family assemblies and pupil led groups. As a rights respecting school we do not only teach our pupils about children’s rights but we also model rights and respect in all our relationships: between staff, between staff and pupils, and between pupils.</p>
+
+            <ul class="accordion" data-accordion role="tablist">
+
+                <li class="accordion-navigation">
+                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
+                        <p class="text-capitalize">Main skills students develop:</p>
+                        <ul class="circle">
+                            <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
+                            <li>Understanding of their own rights and responsibilities.</li>
+                            <li>Participating actively in promoting the UNCRC articles and RRR school events.</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+             <p><strong>Families</strong> – The overall aim of the Family system is to connect all year groups within the school, providing them with the opportunity to further develop skills and experiences in order to become confident and well-rounded individuals. Families aim to get students to work to help each other build confidence through collaboration; persevering through a task; taking risks as a group to achieve a common goal and learning to be enquiring and creative in a safe atmosphere. Families aim to do this by providing Family members with enjoyable and competitive Family events as well as learning about the UNCRC and raising awareness of those in need.</p>
+            
+            <ul class="accordion" data-accordion role="tablist">
+
+                <li class="accordion-navigation">
+                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
+                        <p class="text-capitalize">Main skills students develop:</p>
+                        <ul class="circle">
+                            <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
+                            <li>Participating actively in promoting the UNCRC articles and RRR school events.</li>
+                            <li>Raising money for charitable causes – chosen by the Family </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+             <p><strong>Information, Advice and Guidance</strong> – Assisting pupils in their ‘next steps’ be that college or apprenticeships, future career ideas or their option choices. Pupils learn about the world of work and the range of opportunities open to them after they complete their time at Park. At Park we work very closely with local employers through our career related sessions with EBP. We work on raising pupils' aspirations to achieve their full potential and support them in doing so. All pupils can have a 1:1 personal careers appointment with our trained independent impartial careers advisor.</p>
+            
+            <ul class="accordion" data-accordion role="tablist">
+
+                <li class="accordion-navigation">
+                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
+                        <p class="text-capitalize">Main skills students develop:</p>
+                        <ul class="circle">
+                            <li>Understanding the world of work.</li>
+                            <li>Understanding the career choices available.</li>
+                            <li>Learning about the choices process through college visits, mock interviews and visiting speakers.</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+             <p><strong>Beliefs and Culture</strong> – The purpose of Beliefs and Culture lessons (B.C) is to support students in developing their own coherent patterns of values and principles, and to support their spiritual, moral, social, and cultural development. Our focus is on the development of personal patterns of values in the light of contemporary ethical and social issues facing society.</p>
+             <p>The agreed syllabus which provides the legal framework for R.E states that “we have to understand how and why people live differently from each other and respect their right to do so. Living Difference identifies that respecting difference requires us to engage with difference confidently. We need to identify how and why we have different beliefs, attitudes and practices from other people. By doing this, we can gain respect from others. Accepting difference does not imply agreement; nor does it imply that all difference is acceptable. It is important that students make reasoned and informed judgements about difference and that these are based on beliefs and values that they sincerely hold, and that they can express and defend in reasoned debate. Teachers have a responsibility for helping students to develop their beliefs and values, in relation to those values that society prizes. As a result, the agreed syllabus places particular importance upon the spiritual, moral, social and cultural development of students."</p>
+             <p>Agreed Syllabus, ‘Living Difference’, for Hampshire, Portsmouth and Southampton</p>
+             <p>The purpose of B.C at Park is therefore to promote pupils’ spiritual, moral, social and cultural development through an awareness and appreciation of diversity and difference of beliefs and values, cultures and societies within the UK and the wider world. As part of this pupils are offered the chance to respond and in so doing develop their own sense of self and place in the local, national and global community.</p>
+
+            <ul class="accordion" data-accordion role="tablist">
+
+                <li class="accordion-navigation">
+                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
+                        <p class="text-capitalize">Main skills students develop:</p>
+                        <ul class="circle">
+                            <li>Understanding range of diversity in the world and a respect for individual beliefs.</li>
+                            <li>Appreciate the necessity for tolerance and respect of others.</li>
+                            <li>A sense of self and place as a British citizen.</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+             <p><strong>Character building lessons</strong> – These lessons involve promoting good behaviour characteristics which enhance the integrity, reputation and moral fibre of the students taking part. They explore areas of character that are easily forgotten in the normal daily activities that encompass school life and learn how to make informed choices about how to behave as a decent and considerate citizen.</p>
+
+            <ul class="accordion" data-accordion role="tablist">
+
+                <li class="accordion-navigation">
+                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
+                        <p class="text-capitalize">Main skills students develop:</p>
+                        <ul class="circle">
+                            <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
+                            <li>Understanding of their own rights and responsibilities.</li>
+                            <li>Developing positive character traits while understanding why some characteristics are undesirable.</li>
+                            <li>Develop strategies to develop positive traits and identify when alternative ways of behaving are preferred.</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+
+            <h4>SMSC Overview</h4>
+            <p>Please click <a href="/assets/curriculum/yr8-options.pdf" target="_blank">here</a> for an overview of SMSC.</p>
+        </div>
+
+    </div>
+</section>
+    
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
