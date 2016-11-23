da358a4d6de31303baa2b5bc1d90425f35c41d92
Phil Warren <P.Warren@pcs.hants.sch.uk>
2016-11-23 10:07:43 +0000

    SMSC Update

--- smsc.php
+++ smsc.php
@@ -50,72 +50,49 @@
                             <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
                             <li>Understanding of their own rights and responsibilities.</li>
                             <li>Participating actively in promoting the UNCRC articles and RRR school events.</li>
-                    </ul>
+                        </ul>
 
              <p><strong>Families</strong> – The overall aim of the Family system is to connect all year groups within the school, providing them with the opportunity to further develop skills and experiences in order to become confident and well-rounded individuals. Families aim to get students to work to help each other build confidence through collaboration; persevering through a task; taking risks as a group to achieve a common goal and learning to be enquiring and creative in a safe atmosphere. Families aim to do this by providing Family members with enjoyable and competitive Family events as well as learning about the UNCRC and raising awareness of those in need.</p>
             
-            <ul class="accordion" data-accordion role="tablist">
 
-                <li class="accordion-navigation">
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main skills students develop:</p>
+                    <p><strong>Main skills students develop:</strong></p>
                         <ul class="circle">
                             <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
                             <li>Participating actively in promoting the UNCRC articles and RRR school events.</li>
                             <li>Raising money for charitable causes – chosen by the Family </li>
                         </ul>
-                    </div>
-                </li>
-            </ul>
              <p><strong>Information, Advice and Guidance</strong> – Assisting pupils in their ‘next steps’ be that college or apprenticeships, future career ideas or their option choices. Pupils learn about the world of work and the range of opportunities open to them after they complete their time at Park. At Park we work very closely with local employers through our career related sessions with EBP. We work on raising pupils' aspirations to achieve their full potential and support them in doing so. All pupils can have a 1:1 personal careers appointment with our trained independent impartial careers advisor.</p>
             
-            <ul class="accordion" data-accordion role="tablist">
 
-                <li class="accordion-navigation">
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main skills students develop:</p>
+                    <p><strong>Main skills students develop:</strong></p>
                         <ul class="circle">
                             <li>Understanding the world of work.</li>
                             <li>Understanding the career choices available.</li>
                             <li>Learning about the choices process through college visits, mock interviews and visiting speakers.</li>
                         </ul>
-                    </div>
-                </li>
-            </ul>
+
              <p><strong>Beliefs and Culture</strong> – The purpose of Beliefs and Culture lessons (B.C) is to support students in developing their own coherent patterns of values and principles, and to support their spiritual, moral, social, and cultural development. Our focus is on the development of personal patterns of values in the light of contemporary ethical and social issues facing society.</p>
              <p>The agreed syllabus which provides the legal framework for R.E states that “we have to understand how and why people live differently from each other and respect their right to do so. Living Difference identifies that respecting difference requires us to engage with difference confidently. We need to identify how and why we have different beliefs, attitudes and practices from other people. By doing this, we can gain respect from others. Accepting difference does not imply agreement; nor does it imply that all difference is acceptable. It is important that students make reasoned and informed judgements about difference and that these are based on beliefs and values that they sincerely hold, and that they can express and defend in reasoned debate. Teachers have a responsibility for helping students to develop their beliefs and values, in relation to those values that society prizes. As a result, the agreed syllabus places particular importance upon the spiritual, moral, social and cultural development of students."</p>
              <p>Agreed Syllabus, ‘Living Difference’, for Hampshire, Portsmouth and Southampton</p>
              <p>The purpose of B.C at Park is therefore to promote pupils’ spiritual, moral, social and cultural development through an awareness and appreciation of diversity and difference of beliefs and values, cultures and societies within the UK and the wider world. As part of this pupils are offered the chance to respond and in so doing develop their own sense of self and place in the local, national and global community.</p>
 
-            <ul class="accordion" data-accordion role="tablist">
 
-                <li class="accordion-navigation">
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main skills students develop:</p>
+                    <p><strong>Main skills students develop:</strong></p>
                         <ul class="circle">
                             <li>Understanding range of diversity in the world and a respect for individual beliefs.</li>
                             <li>Appreciate the necessity for tolerance and respect of others.</li>
                             <li>A sense of self and place as a British citizen.</li>
                         </ul>
-                    </div>
-                </li>
-            </ul>
              <p><strong>Character building lessons</strong> – These lessons involve promoting good behaviour characteristics which enhance the integrity, reputation and moral fibre of the students taking part. They explore areas of character that are easily forgotten in the normal daily activities that encompass school life and learn how to make informed choices about how to behave as a decent and considerate citizen.</p>
 
-            <ul class="accordion" data-accordion role="tablist">
 
-                <li class="accordion-navigation">
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main skills students develop:</p>
+                    <p><strong>Main skills students develop:</strong></p>
                         <ul class="circle">
                             <li>Developing the pupils' social, moral, cultural and spiritual development.</li>
                             <li>Understanding of their own rights and responsibilities.</li>
                             <li>Developing positive character traits while understanding why some characteristics are undesirable.</li>
                             <li>Develop strategies to develop positive traits and identify when alternative ways of behaving are preferred.</li>
                         </ul>
-                    </div>
-                </li>
-            </ul>
 
             <h4>SMSC Overview</h4>
             <p>Please click <a href="/assets/curriculum/smsc.pdf" target="_blank">here</a> for an overview of SMSC.</p>
