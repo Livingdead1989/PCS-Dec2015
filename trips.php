f869aa1f5f3c68f47957e3217b10ded92976976f
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-01-19 10:47:29 +0000

    updated trip information

--- trips.php
+++ trips.php
@@ -26,111 +26,43 @@
         <div class="medium-12 columns">
 
         <ul class="medium-block-grid-1 large-block-grid-1">
+          <!-- <li>
+            <h3>Title</h3>
+            <p>Date – Date</p>
+            <p>Lead Teacher – Mr Teacher</p>
+            <p>Cost - £00.00</p>
+          </li>   -->
 
-            <li>
-                <h3>Disneyland Paris 2017 - INVITE ONLY</h3>
-                <p>Tuesday 7th February to Friday 10th February 2017</p>
-                <p>Lead Teacher – Mrs Butler</p>
-                <p>Cost - £280.00</p>
+          <li>
+            <h3>Chichester College ‘Intermezzo’</h3>
+            <p>Limited Spaces - first come first serve</p>
+            <p>Friday 3rd February 2017</p>
+            <p>Lead Teacher – Miss Chuter</p>
+            <p>Cost - £1.00</p>
+            <p><strong>DEADLINE FOR TRIP HAS NOW PASSED (09/01/17)</strong></p>
+          </li>
 
-                <table style="width:100%;">
-                  <tr>
-                    <th>Disneyland Paris</th>
-                    <th>Payment Due</th>
-                    <th>Amount</th>
-                    <th>Total</th>
-                  </tr>
-                  <tr>
-                    <td>Non-refundable Deposit</td>
-                    <td>29/04/16</td>
-                    <td>£70.00</td>
-                    <td>£210.00</td>
-                  </tr>
-                  <tr>
-                    <td>First Payment</td>
-                    <td>31/05/16</td>
-                    <td>£35.00</td>
-                    <td>£175.00</td>
-                  </tr>
-                  <tr>
-                    <td>Second Payment</td>
-                    <td>30/06/16</td>
-                    <td>£35.00</td>
-                    <td>£140.00</td>
-                  </tr>
-                  <tr>
-                    <td>Third Payment</td>
-                    <td>20/07/16</td>
-                    <td>£35.00</td>
-                    <td>£105.00</td>
-                  </tr>
-                  <tr>
-                    <td>Fourth Payment</td>
-                    <td>07/09/16</td>
-                    <td>£35.00</td>
-                    <td>£70.00</td>
-                  </tr>
-                  <tr>
-                    <td>Fifth Payment</td>
-                    <td>03/10/16</td>
-                    <td>£35.00</td>
-                    <td>£35.00</td>
-                  </tr>
-                  <tr>
-                    <td>Final Payment</td>
-                    <td>01/11/16</td>
-                    <td>£35.00</td>
-                    <td>-</td>
-                  </tr>
-                </table>
-                <p><strong>Deadline for Final Payment has now passed.</strong></p>
-                <hr>
-            </li>
+          <li>
+            <h3>BFI Exam Course – London TV Studios</h3>
+            <p>Tuesday 7th February 2017</p>
+            <p>Lead Teacher – Miss Loake</p>
+            <p>Cost - £11.00</p>
+          </li>
 
+          <li>
+            <h3>Houses of Parliament</h3>
+            <p>Wednesday 15th February 2017</p>
+            <p>Lead Teacher – Mrs Coles</p>
+            <p>Cost - £10.00</p>
+          </li>
 
-
-			      <li>
-              <h3>New York</h3>
-              <p>Friday 17 February – Tuesday 21 February 2017</p>
-              <p>Lead Teacher – Mr Roberson</p>
-              <p>Cost - £661.00</p>
-
-              <table style="width:100%;">
-                <tr>
-                  <th>Disneyland Paris</th>
-                  <th>Payment Due</th>
-                  <th>Amount</th>
-                  <th>Total</th>
-                </tr>
-                <tr>
-                  <td colspan="2"><strong>Deadline for non-refundable deposit has now passed</strong></td>
-                  <td>£150.00</td>
-                  <td>£511.00</td>
-                </tr>
-                <tr>
-                  <td>First Payment</td>
-                  <td>09/09/16</td>
-                  <td>£171.00</td>
-                  <td>£340.00</td>
-                </tr>
-                <tr>
-                  <td>Second Payment</td>
-                  <td>07/10/16</td>
-                  <td>£170.00</td>
-                  <td>£170.00</td>
-                </tr>
-                <tr>
-                  <td>Final Payment</td>
-                  <td>04/11/16</td>
-                  <td>£170.00</td>
-                  <td style="text-align:center;">-</td>
-                </tr>
-              </table>
-              <p><strong>Deadline for Final Payment has now passed.</strong></p>
-
-              <hr>
-            </li>
-
+          <li>
+            <h3>Curious Incident of Dog</h3>
+            <p>Thursday 30th March 2017</p>
+            <p>Lead Teacher – Miss Spies</p>
+            <p>Cost - £23.00</p>
+            <p><strong>4 SPACES CURRENTLY AVAILABLE (19/01/17)</strong></p>
+          </li>
         </ul>
 
         <p>Payments can now be made Online via <a href="//www.scopay.com/pcs" target="_blank">SCO Online Payments</a>.</p>
