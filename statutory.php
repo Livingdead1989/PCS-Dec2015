4777d5a9259eca5cd3a5abba4866d2345ee14570
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-01-24 10:18:01 +0000

    Added leavers PDF to stats page

--- statutory.php
+++ statutory.php
@@ -40,6 +40,7 @@
                 <li><a href="/assets/policies/charging-and-remissions.pdf" target="_blank">Charging and Remissions</a></li>
                 <li><a href="/assets/statutory/ethos-values.pdf" target="_blank">Ethos and Values</a></li>
                 <li><a href="/assets/statutory/Complaints_Procedure.pdf" target="_blank">Complaints Procedure</a></li>
+                <li><a href="/assets/statutory/Leaver-Destinations.pdf" target="_blank">Leaver Destinations</a></li>
             </ul>
             <br>
             <h5 class="subheader text-capitalize text-center"><small>Policies are available in downloadable PDF format <br>our <a href="/policies.php">Policies page</a></small></h5>
