45d54cdfe85ae13bc00b65acd6464d6e78fc50e0
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-01-25 10:39:58 +0000

    Layout and Apperance Changes for newly added PP content

--- pupil-premium.php
+++ pupil-premium.php
@@ -28,21 +28,29 @@
           <p>In 2015/2016 the school received £446,093 for Pupil Premium (PP) plus £12,700 for Children in Care.</p>
           <p>The school spent £500,275 providing additional support for PP students. The pupil premium is grant funding and is in addition to the school’s Delegated Budget. The school chooses to invest additional funding from its delegated budget to support pupil premium funding.</p>
           <p>The Pupil Premium is allocated by calculating the number of students who are on Free School Meals (FSM) or have had FSM in the last six years. Students who have been in care continuously for more than six months or have a parent in the Armed Services also lead to some additional funding for the school.</p>
-          <p>Main barriers to educational achievement faced by eligible students could include:</p>
-                         <ul class="circle">
-                            <li>Low aspiration</li>
-                            <li>Gaps in learning</li>
-                            <li>Parents who have poor experiences of school, or have not completed further education or in some cases do not value education</li>
-                            <li>Large number of siblings</li>
-                            <li>Often in single parent families</li>
-                            <li>May have experienced significant trauma in earlier years eg alcohol abuse, domestic violence</li>
-                            <li>Lack of trust in, or significant dependence on, outside agencies</li>
-                            <li>Inconsistent attendance at school</li>
-                            <li>Personal health related and well-being issues</li>
-                            <li>Low self-esteem</li>
-                        </ul>
-            <p>This is not an exhaustive list and by no means applies to all students who may qualify for Pupil Premium. </p>
-            
+          <ul class="accordion" data-accordion>
+            <li class="accordion-navigation">
+              <a href="#panel1a">Main barriers to educational achievement faced by eligible students could include:</a>
+              <div id="panel1a" class="content">
+                <ul class="circle">
+                   <li>Low aspiration</li>
+                   <li>Gaps in learning</li>
+                   <li>Parents who have poor experiences of school, or have not completed further education or in some cases do not value education</li>
+                   <li>Large number of siblings</li>
+                   <li>Often in single parent families</li>
+                   <li>May have experienced significant trauma in earlier years eg alcohol abuse, domestic violence</li>
+                   <li>Lack of trust in, or significant dependence on, outside agencies</li>
+                   <li>Inconsistent attendance at school</li>
+                   <li>Personal health related and well-being issues</li>
+                   <li>Low self-esteem</li>
+               </ul>
+               <br>
+               <p>This is not an exhaustive list and by no means applies to all students who may qualify for Pupil Premium. </p>
+              </div>
+            </li>
+          </ul>
+          <br class="show-for-small-only">
+
         </div>
 
         <div class="small-10 small-centered medium-uncentered medium-5 columns">
@@ -58,27 +66,33 @@
     </div>
     <div class="row">
         <div class="medium-8 medium-push-4 columns">
-            <p><h3>How Pupil Premium is spent to address these barriers</h3></p>
+          <h3>How Pupil Premium is spent to address these barriers</h3>
           <p>The Pupil Premium spent is used to raise attainment, autonomy and resilience, promote social skills, independent learning and positive behaviour, in order to increase student progress.</p>
           <p>54% of students in Year 11 in 2015/2016 qualified for PP. In 2016 the gap in achievement between not PP and PP students in their GCSE results was 0.02 for Progress 8 (PP -0.12, Not PP -0.14). The gap between Not PP and PP students achieving English and Maths A*-C was 6%. 42% of PP were entered for English Baccalaureate.</p>
-
-          <p>For 2016/2017 our allocated budget is:</p>
-          <table>
-            <tr>
-              <td>Pupil premium</td>
-              <td>£440,385</td>
-            </tr>
-            <tr>
-              <td>Service Children</td>
-              <td>£1,500</td>
-            </tr>
-            <tr>
-              <td>Children in Care</td>
-              <td>£3,500 per term</td>
-            </tr>
-          </table>
+          <ul class="block-grid-large-2">
+            <li>
+              <p>For 2016/2017 our allocated budget is:</p>
+            </li>
+            <li>
+              <table>
+                <tr>
+                  <td>Pupil premium</td>
+                  <td>£440,385</td>
+                </tr>
+                <tr>
+                  <td>Service Children</td>
+                  <td>£1,500</td>
+                </tr>
+                <tr>
+                  <td>Children in Care</td>
+                  <td>£3,500 per term</td>
+                </tr>
+              </table>
+            </li>
+          </ul>
         </div>
         <div class="hide-for-small-only medium-uncentered medium-4 medium-pull-8 columns">
+          <br>
           <img src="../images/general/general-13.jpg" alt="Park Community School creativity blooms." />
         </div>
     </div>
@@ -91,82 +105,110 @@
     </div>
     <div class="row">
         <div class="large-12 columns">
-            <h3>Pupil Premium interventions</h3>
-            <br>
+          <h3>Pupil Premium interventions</h3>
+          <br>
 
-            <ul class="large-block-grid-2 small-block-grid-1" data-equalizer data-equalizer-mq="large-up">
-                <li>
-                        <h5>Teaching &amp; Learning – addressing student low aspiration and gaps in learning</h5>
-                        <hr>
-                        <ul class="circle">
-                            <li>Emphasis on effective feedback and marking</li>
-                            <li>Summer School</li>
-                            <li>Residential visits</li>
-                            <li>Reduced contact time for teaching staff to allow for quality lesson planning</li>
-                            <li>Smaller class sizes in key subjects</li>
-                            <li>Writing portfolios</li>
-                            <li>Staff focus on success for all PP students</li>
-                            <li>1 : 1 Mentoring</li>
-                        </ul>
-                </li>
-                <li>
-                        <h5>Curriculum – addressing student low aspiration and gaps in learning</h5>
-                        <hr>
-                        <ul class="circle">
-                            <li>Numeracy programmes</li>
-                            <li>Study Clubs</li>
-                            <li>Literacy programmes</li>
-                            <li>Interventions to support learning</li>
-                            <li>Enrichment activities</li>
-                            <li>International visits/trips</li>
-                            <li>Social &amp; cultural visits/trips</li>
-                        </ul>
-                </li>
-                <li>
-                        <h5>Inclusion (could be used) - addressing vulnerability arising from mental health and trauma within the families together with a lack of trust in, or significant dependence on, outside agencies</h5>
-                        <hr>
-                        <ul class="circle">
-                            <li>Home Liaison team; fortnightly referral meeting to follow up targeted PP students</li>
-                            <li>Referral meeting attended by Key Stage leaders, Lead WSG behavior, Lead WSG pastoral, SENCO, Deputy Headteacher pastoral, Medical officer. Focus:</li>
-                            <li>Nurture programmes</li>
-                            <li>Solace: counselling and support service</li>
-                            <li>Individual programmes</li>
-                            <li>Communitas: Youth Workers available during school day</li>
-                            <li>Medical &amp; Emotional support</li>
-                        </ul>
-                </li>
-                <li>
-                        <h5>Community &amp; Partnership– addressing education/low aspiration </h5>
-                        <br>
-                        <br>
-                        <hr>
-                        <ul class="circle">
-                            <li>Interventions to support families (those families identified on the Supporting Families programme; Attendance where there is family history of poor attendance)</li>
-                            <li>IAG (Information, Advice and Guidance) support &amp; intervention</li>
-                        </ul>
-                </li>
-                <li>
-                        <h5>Attendance &amp; Behaviour – addressing inconsistent attendance at school, personal health related and well-being issues and low self-esteem</h5>
-                        <hr>
-                        <ul class="circle">
-                            <li>Attendance groups</li>
-                            <li>1:1 Support</li>
-                        </ul>
-                </li>
-            </ul>
-            <h3>How the impact of Pupil Premium is measured</h3>
+          <ul class="large-block-grid-2 small-block-grid-1" data-equalizer data-equalizer-mq="large-up">
+              <li>
+                <h4>Teaching &amp; Learning <br><small>addressing student low aspiration and gaps in learning</small></h4>
+                <hr>
+                <ul class="circle">
+                    <li>Emphasis on effective feedback and marking</li>
+                    <li>Summer School</li>
+                    <li>Residential visits</li>
+                    <li>Reduced contact time for teaching staff to allow for quality lesson planning</li>
+                    <li>Smaller class sizes in key subjects</li>
+                    <li>Writing portfolios</li>
+                    <li>Staff focus on success for all PP students</li>
+                    <li>1 : 1 Mentoring</li>
+                </ul>
+              </li>
+              <li>
+                <h4>Curriculum <br><small>addressing student low aspiration and gaps in learning</small></h4>
+                <hr>
+                <ul class="circle">
+                    <li>Numeracy programmes</li>
+                    <li>Study Clubs</li>
+                    <li>Literacy programmes</li>
+                    <li>Interventions to support learning</li>
+                    <li>Enrichment activities</li>
+                    <li>International visits/trips</li>
+                    <li>Social &amp; cultural visits/trips</li>
+                </ul>
+              </li>
+              <li>
+                <h4>Inclusion (could be used) <br><small>addressing vulnerability arising from mental health and trauma within the families together with a lack of trust in, or significant dependence on, outside agencies</small></h4>
+                <hr>
+                <ul class="circle">
+                    <li>Home Liaison team; fortnightly referral meeting to follow up targeted PP students</li>
+                    <li>Referral meeting attended by Key Stage leaders, Lead WSG behavior, Lead WSG pastoral, SENCO, Deputy Headteacher pastoral, Medical officer. Focus:</li>
+                    <li>Nurture programmes</li>
+                    <li>Solace: counselling and support service</li>
+                    <li>Individual programmes</li>
+                    <li>Communitas: Youth Workers available during school day</li>
+                    <li>Medical &amp; Emotional support</li>
+                </ul>
+              </li>
+              <li>
+                <h4>Community &amp; Partnership <br><small>addressing education/low aspiration</small></h4>
+                <hr>
+                <ul class="circle">
+                    <li>Interventions to support families (those families identified on the Supporting Families programme; Attendance where there is family history of poor attendance)</li>
+                    <li>IAG (Information, Advice and Guidance) support &amp; intervention</li>
+                </ul>
+              </li>
+              <li>
+                <h4>Attendance &amp; Behaviour <br><small>addressing inconsistent attendance at school, personal health related and well-being issues and low self-esteem</small></h4>
+                <hr>
+                <ul class="circle">
+                    <li>Attendance groups</li>
+                    <li>1:1 Support</li>
+                </ul>
+              </li>
+          </ul>
+        </div>
+      </div>
+    <br><br><br>
+</section>
 
-            <ul class="circle">
-                            <li>Improvement in overall attendance - monitored daily by Attendance Officer</li>
-                            <li>Reduction in the number of persistent absentees and/or improved attendance - monitored weekly by Attendance Officer</li>
-                            <li>Reduction of truancy from lessons – monitored by ‘alerts’ recorded lesson by lesson. Follow up by WSG/on call</li>
-                            <li>Academic progress and attainment data – monitored by HODs, HOYs, mentors and Year 11 intervention team</li>
-                            <li>Reduction in number of behaviour points – analysed and followed up by HOYs weekly</li>
-                        </ul>
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+      <div class="medium-7 large-8 columns">
+        <h3>How the impact of Pupil Premium is measured</h3>
 
-                        <p><h3>Outcomes: Key Assessment point 2 (KA2) December 2016 shows:</h3></p>
+        <ul class="circle">
+          <br>
+          <li><strong>Improvement in overall attendance</strong> - <em>monitored daily by Attendance Officer</em></li>
+          <br>
+          <li><strong>Reduction in the number of persistent absentees and/or improved attendance</strong> - <em>monitored weekly by Attendance Officer</em></li>
+          <br>
+          <li><strong>Reduction of truancy from lessons</strong> – <em>monitored by ‘alerts’ recorded lesson by lesson. Follow up by WSG/on call</em></li>
+          <br>
+          <li><strong>Academic progress and attainment data</strong> – <em>monitored by HODs, HOYs, mentors and Year 11 intervention team</em></li>
+          <br>
+          <li><strong>Reduction in number of behaviour points</strong> – <em>analysed and followed up by HOYs weekly</em></li>
+        </ul>
+      </div>
+      <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-5 large-4 columns">
+        <img src="images/general/general-12.jpg" alt="Teaching at Park Community School is an enjoyable experience.">
+      </div>
+    </div>
+    <br><br><br>
+</section>
 
-                        <p>Overall Attitude To Learning (OATL) is good in all years i.e. above average score of 2:</p>
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+      <div class="large-12 columns">
+
+          <h3>Outcomes: Key Assessment point 2 (KA2) December 2016 shows:</h3>
+
+          <p><strong>Overall Attitude To Learning (OATL) is Good in All Years</strong> <em>i.e. above average score of 2:</em></p>
           <table>
             <tr>
               <td>Year 7</td>
@@ -180,8 +222,8 @@
               <td>7.35</td>
               <td>6.84</td>
             </tr>
-            </table>
-                        <p>Homework is good in all years ie above average score of 2: </p>
+          </table>
+          <p><strong>Homework is Good in All Years</strong> <em>i.e. above average score of 2:</em></p>
           <table>
             <tr>
               <td>Year 7</td>
@@ -195,10 +237,10 @@
               <td>3.72</td>
               <td>3.6</td>
             </tr>
-            </table>
-            <p>Homework clubs run by Communitas (youth workers) have been introduced to support this by providing additional support and a quiet space to work with computers</p>
+          </table>
+          <p><em>Homework clubs run by Communitas (youth workers) have been introduced to support this by providing additional support and a quiet space to work with computers</em></p>
 
-            <p>P8 PP/non PP gap </p>
+          <p><strong>P8 PP/non PP Gap</strong> <em>i.e. gaps are small</em></p>
           <table>
             <tr>
               <td>Year 7</td>
@@ -212,11 +254,8 @@
               <td>-0.07</td>
               <td>+0.15</td>
             </tr>
-            </table>
-            <p>i.e. gaps are small</p>
-
-
-            <p>Year 11 P8 PP/non PP gap </p>
+          </table>
+          <p><strong>Year 11 P8 PP/non PP Gap</strong></p>
           <table>
             <tr>
               <td>English</td>
@@ -230,11 +269,10 @@
               <td>-0.43</td>
               <td>-0.38</td>
             </tr>
-            </table>
-            <p>Attendance: 128 PA who are PP and 46 who are non PP. Of the 112 PAs who have improved their attendance 80 were PP and 6 of those now have attendance of above 90%.</p>
+          </table>
+          <p><strong>Attendance:</strong> 128 PA who are PP and 46 who are non PP. Of the 112 PAs who have improved their attendance 80 were PP and 6 of those now have attendance of above 90%.</p>
         </div>
-
-        <br>
+      </div>
     </div>
     <br><br>
 </section>
