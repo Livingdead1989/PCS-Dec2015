924e0914ed57173dfb0d2397c91a554db1dafd25
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-05-31 08:47:19 +0100

    Added Events Page, link added to Top Navigation

---
+++ events.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Events</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-school">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-school">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Events</h1>
+            <p class="text-capitalize">Highlighting Events at Park</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+	<div class="row">
+        <br>
+        <div class="medium-12 columns text-center">
+			<p>For the latest in News and Event Information please follow us on <a href="//twitter.com/ParkSchSocial">Twitter</a> and <a href="//facebook.com/ParkCommunitySchoolNews">Facebook</a>.</p>
+			<hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <br>
+        <div class="medium-12 columns">
+	        <ul class="medium-block-grid-1 large-block-grid-1">
+	          <!-- <li>
+	            <h3>Title</h3>
+	            <p>Date – Date</p>
+	            <p>Info</p>
+	          </li>   -->
+
+	          <li>
+	            <h3><a href="assets/munch.pdf" target="_blank">Munch</a></h3>
+	            <p>30th May 2017 – 2nd June 2017</p>
+	            <p>Free two course meals over the holiday, Munch is free to those in receipt of free school meals, for others we suggest an optional donation of £2.<br> For more information please view the <a href="assets/munch.pdf" target="_blank">PDF</a>.</p>
+	          </li>
+	          <hr>
+
+	        </ul>
+        </div>
+    </div><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
