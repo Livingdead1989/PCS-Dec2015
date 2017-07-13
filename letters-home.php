1b1577e8739dcab15514bfde62578dc725844bff
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-07-13 12:09:58 +0100

    Added new letter, removed old and blending naming conv

--- letters-home.php
+++ letters-home.php
@@ -26,7 +26,12 @@
         <div class="medium-12 columns">
         <p>We feel that everyone should be kept informed. Below you will find all the letters we send home so you get the information needed, regardless of the animals eating it or it getting lost!</p>
             <ul class="small-block-grid-1 medium-block-grid-2">
-            	<li>           	
+            <li>           	
+                  <ul class="circle">July 2017<hr>
+                  	<li><a href="/assets/letters/2017-07/crazy_hair_week-end_start_of_term-blazers.pdf" target="_blank">Crazy Hair Week, End &amp; Start of Term and Blazers</a></li>
+                  </ul>
+              </li>
+	        	<li>           	
                   <ul class="circle">June 2017<hr>
                   	<li><a href="/assets/letters/2017-06/key_events_to_end_of_academic_year.pdf" target="_blank">Key Events to end of Academic Year</a></li>
                     <li><a href="/assets/letters/2017-06/Discount_Week_Information.pdf" target="_blank">Skoolkit Discount Week Information</a></li>
@@ -41,26 +46,16 @@
               </li>
               <li>
                   <ul class="circle">April 2017<hr>
-                      <li><a href="/assets/letters/april-2017/RaceatOurPlace-Y11Y10exams-uniform-absenceNotification.pdf" target="_blank">Race at Our Place, Yr11 Yr10 exams, uniform and absence notification</a></li>
-                      <li><a href="/assets/letters/april-2017/KA4Reports-InsetDay10July2017.pdf" target="_blank">KA4 Reports and Inset Day 10th July 2017</a></li>
+                      <li><a href="/assets/letters/2017-04/RaceatOurPlace-Y11Y10exams-uniform-absenceNotification.pdf" target="_blank">Race at Our Place, Yr11 Yr10 exams, uniform and absence notification</a></li>
+                      <li><a href="/assets/letters/2017-04/KA4Reports-InsetDay10July2017.pdf" target="_blank">KA4 Reports and Inset Day 10th July 2017</a></li>
                   </ul>
               </li>
               <li>
                   <ul class="circle">February 2017<hr>
-                      <li><a href="/assets/letters/february-2017/ESafety.pdf" target="_blank">ESafety</a></li>
-                      <li><a href="/assets/letters/february-2017/Statement-from-the-Governing-Bodies-and-Headteachers-of-Havant-Federation-of-Secondary-Schools.pdf" target="_blank">Statement from the Governing Bodies and Headteachers of Havant Federation of Secondary Schools</a></li>
-                      <li><a href="/assets/letters/february-2017/Ofsted-School-Meal-price-increase.pdf" target="_blank">Ofsted, School Meal Price Increase</a></li>
-                      <li><a href="/assets/letters/february-2017/Y11-mocks-provisional-exam-timetable-and-the-Prom.pdf" target="_blank">Y11 mocks, provisional exam timetable and the Prom</a></li>
-                  </ul>
-              </li>
-              <li>
-                  <ul class="circle">December 2016<hr>
-                      <li><a href="/assets/letters/december-2016/EndStartofTerm-SchoolDayChange-Uniform-KeyDates.pdf" target="_blank">End and Start of Term, School Day Changes, Uniform and Key Dates</a></li>
-                  </ul>
-              </li>
-              <li>
-                  <ul class="circle">November 2016<hr>
-                      <li><a href="/assets/letters/november-2016/november-2016-behaviour-system.pdf" target="_blank">Behaviour System</a></li>
+                      <li><a href="/assets/letters/2017-02/ESafety.pdf" target="_blank">ESafety</a></li>
+                      <li><a href="/assets/letters/2017-02/Statement-from-the-Governing-Bodies-and-Headteachers-of-Havant-Federation-of-Secondary-Schools.pdf" target="_blank">Statement from the Governing Bodies and Headteachers of Havant Federation of Secondary Schools</a></li>
+                      <li><a href="/assets/letters/2017-02/Ofsted-School-Meal-price-increase.pdf" target="_blank">Ofsted, School Meal Price Increase</a></li>
+                      <li><a href="/assets/letters/2017-02/Y11-mocks-provisional-exam-timetable-and-the-Prom.pdf" target="_blank">Y11 mocks, provisional exam timetable and the Prom</a></li>
                   </ul>
               </li>
 
