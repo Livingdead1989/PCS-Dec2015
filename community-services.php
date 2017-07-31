3d955217cccef04ac2769bb2594d26f8da4e6a58
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2017-07-31 08:55:11 +0100

    Draft Revamp of Community Page, Pending Copy review and images

--- community-services.php
+++ community-services.php
@@ -20,54 +20,191 @@
 </header>
 
 <!-- CONTENT -->
+
+<!-- Introduction -->
 <section>
     <div class="row">
         <br>
         <div class="medium-6 columns">
             <p>The role of the Community Department is to provide a whole range of activities for our students and the wider community.</p>
             <p>This ensures that the school can be open in the evenings up until 10pm, weekends and holidays.</p>
-
             <br>
             <p>Click here for more information about our parties.</p>
-
             <div class="small-only-text-center">
                 <a href="assets/community/PartyLeaflet.pdf" target="_blank" class="button radius">Party Flyer</a>
-                <a href="assets/community/AdvertisingFlyer.pdf" target="_blank" class="button radius">Awards Presentations</a>
             </div>
-
-
-            </div>
-        <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-6 columns"><img src="images/community/community-01.jpg" alt="Park Community - Laser Tag perfect for Parties"></div>
+        </div>
+        <div class="small-8 small-offset-2 small-pull-2 medium-pull-0 medium-offset-0 medium-6 columns">
+        	<img src="images/community/community-01.jpg" alt="Park Community - Laser Tag perfect for Parties">
+        </div>
     </div>
     <br><br><br>
 </section>
 
+
+<!-- Facilities -->
 <section class="grey-bg">
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
     </div>
+	<div class="row">
+		<div class="column text-center">
+        	<h2>Our Fantastic Facilities</h2>
+		</div>
+	</div>
+	<br>
     <div class="row">
         <div class="medium-push-4 medium-8 columns">
-            <ul class="circle">
-                <p>At Park we have fantastic new facilities including:</p>
-                <li>Theatre seating 245</li>
-                <li>Lecture Theatre seating 180</li>
-                <li>3G FIFA accredited synthetic turf pitch</li>
-                <li>4 badminton court sized Sports hall</li>
+            <ul class="circle" style="line-height: 2.3;">
+                <li>Theatre - seating 245</li>
+                <li>Lecture Theatre - seating 180</li>
+                <li>3G FIFA accredited Synthetic Turf Pitch (STP)</li>
+                <li>4 Badminton court sized Sports Hall</li>
                 <li>Media Suite</li>
                 <li>Dickinson Centre, a perfect conference and training venue</li>
                 <li>Nursery and pre-school on site</li>
                 <li>Wide variety of room sizes and outdoor spaces available to hire.</li>
             </ul>
-            <p>We offer an extensive range of children's birthday parties for all ages and are able to provide a variety of inflatable equipment. We provide catering for events big and small, by our team of highly qualified chefs.</p>
-            <p>During school holidays, there is a varied programme of events to keep children entertained.</p>
         </div>
-        <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-8 medium-offset-0 medium-4 columns"><img src="images/community/community-03.jpg" alt="Park Community - Offering a range of managed services"></div>
+        <div class="small-8 small-offset-2 small-pull-2 medium-pull-8 medium-offset-0 medium-4 columns"><img src="images/community/community-03.jpg" alt="Park Community - Offering a range of managed services"></div>
+    </div>
+    <br><br><br>
+</section>
+
+
+<!-- Party Packages -->
+<section class="white-bg">
+	<div class="row">
+		<div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+	</div>
+    <div class="row">
+        <div class="column text-center">
+        	<h2>Party Packages</h2>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+    	<div class="large-12 columns">
+            <ul class="text-center large-block-grid-4 medium-block-grid-4 small-block-grid-2">
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-LaserTag">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Laser Tag</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-BouncyCastle">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Bouncy Castle</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-SoftPlay">
+                            <img src="http://placehold.it/400" alt=" ">
+                            <p>Bouncy Castle &amp; Soft Play</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-Zorb">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Inflatable Zorb &amp; Bouncy Castle</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-AssaultCourse">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Assault Course &amp; Bouncy Castle</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-TreasureHunt">
+                            <img src="http://placehold.it/400" alt=" ">
+                            <p>Teasure Hunt Play 'n' Slide &amp; Bouncy Castle</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-PartySpace">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Party Venue Space</p>
+                        </a></div>
+                </li>
+                <li>
+                    <div class="menu-box"><a href="#" data-reveal-id="pty-Extras">
+                            <img src="http://placehold.it/400" alt="">
+                            <p>Optional Extras</p>
+                        </a></div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+            <div class="column text-center">
+                <a href="assets/community/PartyLeaflet.pdf" target="_blank" class="button success radius">Party Packages PDF</a>
+            </div>
     </div>
     <br><br>
 </section>
 
-<section>
+<!-- Sport Awards -->
+<section class="grey-bg">
+	<div class="row">
+		<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+	</div>
+    <div class="row">
+        <div class="column text-center">
+        	<h2>Sports Awards Presentations</h2>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+    	<div class="column medium-6">
+    		<h4>Theatre, Lights &amp; PA System</h4>
+    		<p>Discount for clubs using our facilities weekly this season! <small>(2016/2017)</small></p>
+			<hr>
+
+    		<h4>Afternoon or Evening</h4>
+    		<p>Whatever suits you best!</p>
+    		<hr>
+			
+    		<h4>Licensed Bar Available</h4>
+    		<p>or just soft drinks if you'd prefer!</p>
+    	</div>
+    	<div class="small-8 small-offset-2 medium-pull-0 medium-offset-0 medium-6 columns">
+    		<img src="images/community/community-04.jpg" alt="">
+    	</div>
+    </div>
+    <br>
+
+    <div class="row">
+    	<div class="column medium-6 medium-push-6">
+    		<h4>Hot &amp; Cold Food Available</h4>
+    		<p>as well as pick and mix sweets!</p>
+    		<hr>
+			
+    		<h4>Printing Services</h4>
+    		<p>Certificates, Programmes, Banners and More!</p>
+    		<hr>
+			
+    		<h4>Anything else?</h4>
+            <p>Get in Touch!</p>
+    	</div>
+    	<div class="small-8 small-offset-2 medium-pull-6 medium-offset-0 medium-6 columns">
+    		<img src="images/community/community-05.jpg" alt="">
+    	</div>
+    </div>
+    <br><br>
+    <div class="row">
+    	<div class="column text-center">
+            <a href="assets/community/AdvertisingFlyer.pdf" target="_blank" class="button success radius">Awards Presentations PDF</a>
+    	</div>
+	</div>
+    <br><br>
+</section>
+
+
+<!-- Contact -->
+<section id="contact">
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
@@ -97,14 +234,214 @@
             <hr>
             <ul class="no-bullet">
                 <li>Telephone: 02392 489 811</li>
-                <br>
                 <li>Email: <a href="javascript:location='mailto:\u0062\u006f\u006f\u006b\u0069\u006e\u0067\u0073\u0040\u0070\u0063\u0073\u002e\u0068\u0061\u006e\u0074\u0073\u002e\u0073\u0063\u0068\u002e\u0075\u006b';void 0">Bookings</a></li>
             </ul>
-
+        <br>
         </div>
         <div class="hide-for-small-only medium-pull-0 medium-offset-0 medium-4 columns"><img src="images/community/community-02.jpg" alt="Park Community - Removing the stresses and hassles so you can enjoy the party"></div>
     </div><br>
 </section>
+
+<!-- MODALS -->
+<!-- LaserTag -->
+<div id="pty-LaserTag" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Laser Tag Party Package</h2>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        <li>1 hour play</li>
+        <li>30 minutes after to eat in our restaurant</li>
+        <li>Staff members to assist with the party</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+        <li>Minimum age of 7 years</li>
+        <li>Maximum attending of 16 children</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p>Saturdays, Sundays and School Holidays</p>
+    <p>10:30 - 12:30 &amp; 13:00 - 15:00</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £175</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- BouncyCastle -->
+<div id="pty-BouncyCastle" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Bouncy Castle Party Package</h2>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+        
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p>Saturdays, Sundays and School Holidays</p>
+    <p>10:30 - 12:30 &amp; 13:00 - 15:00</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £100</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- SoftPlay -->
+<div id="pty-SoftPlay" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Bouncy Castle &amp; Soft Play Party Package</h2>
+    <p class="text-center">Ideal for younger children.</p>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+        
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p>Saturdays, Sundays and School Holidays</p>
+    <p>10:00 - 12:00 &amp; 13:00 - 15:00</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £120</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- Zorb -->
+<div id="pty-Zorb" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Inflatable Zorb &amp; Bouncy Castle Party Package</h2>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        <li>Staff members to assist with the party</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+        <li>Minimum age of 6 years</li>
+        <li>Maximum attending of 20 children</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p><em>Saturday Early Bird, <strong>10% Discount</strong> - 9:30 - 11:30</em></p>
+    <p>Saturdays, 12:00 - 14:00 &amp; 14:30 - 16:30</p>
+    <p>Sundays, 11:00 - 13:00 &amp; 13:00 - 15:30</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £175</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- AssaultCourse -->
+<div id="pty-AssaultCourse" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Assault Course &amp; Bouncy Castle Party Package</h2>
+    <p class="text-center">Assault Course is 24 Metres Long!</p>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        <li>Staff members to assist with the party</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+        <li>Minimum age of 6 years</li>
+        <li>Maximum attending of 20 children</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p><em>Saturday Early Bird, <strong>10% Discount</strong> - 9:30 - 11:30</em></p>
+    <p>Saturdays, 12:00 - 14:00 &amp; 14:30 - 16:30</p>
+    <p>Sundays, 11:00 - 13:00 &amp; 13:00 - 15:30</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £150</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- TreasureHunt -->
+<div id="pty-TreasureHunt" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Treasure Hunt Play 'n' Slide &amp; Bouncy Castle Party Package</h2>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <p>Saturdays, Sundays and School Holidays</p>
+    <p>10:00 - 12:00 &amp; 13:00 - 15:00</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £150</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- PartySpace -->
+<div id="pty-PartySpace" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 class="text-center" id="modalTitle">Party Venue Space</h2>
+    <hr>
+
+    <p class="lead">Includes</p>
+    <ul class="circle">
+        <li>3 Hour Space Hire</li>
+    </ul>
+    <hr>
+
+    <p class="lead">Limitations</p>
+    <ul class="circle">
+    </ul>
+    <hr>
+
+    <p class="lead">Available Days &amp; Times</p>
+    <hr>
+
+    <p class="lead text-right">Cost: £54 - £90</p>
+
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<!-- Extras -->
+<div id="pty-Extras" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <h2 id="modalTitle"></h2>
+    <p class="lead"></p>
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
 
 <!-- FOOTER -->
 <?php include("includes/footer.html") ?>
