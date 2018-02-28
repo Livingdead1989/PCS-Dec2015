28bb901ae63af99116758379b9e3434b14dc483b
Phil Warren <P.Warren@pcs.hants.sch.uk>
2018-02-28 12:13:28 +0000

    Severe Weather

---
+++ sev-weather.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Severe Weather</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-school">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-school">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Park News</h1>
+            <p class="text-capitalize">Severe Weather Information</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div id="park-post" class="medium-12 columns">
+            <h3>Information</h3>
+            <p>Dear Parent/ Carer</p>
+            <p>We have received weather warnings for Thursday and Friday, today’s forecast suggests there is a 25%-50% chance of snow. We currently plan to keep the school open as usual, but we will review the situation throughout the rest of the week. Any decision to close will depend largely on ensuring the health and safety of our students and staff. Many staff travel some distance and maybe unable to do so if snow settles in the wider area. If we are unable to be certain of sufficient staffing levels a decision to close may need to be taken. This decision will be made as promptly as possible and ideally before students set off for school in the morning. However, snow conditions may mean the decision to close is taken during the school day. Please make sure we have up to date records in order that we can contact you during the school day. You will then be asked to collect your child at the earliest possible opportunity.</p>
+            <p>At present the parents’ evening for Year 8 is still taking place on Thursday, a decision to delay this evening will be taken if the weather prevents its smooth and safe running. If delayed, it will most likely take place a week later, on Thursday, 8 March, at the same time. Confirmation of any new date will be advised separately.</p>
+            <p>We will keep you informed if plans to open change. Before setting off on your journey please find the most up-to-date information via: school email, on the school website and by using school social media. We will also send a text message to all phone contacts held in this way. If you do not currently receive texts from the school, please provide a mobile or home/work phone number to school reception. If you have not yet done so, please sign up for our school information via MyEd app.</p>
+             
+        </div>
+    </div><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
