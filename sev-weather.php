5b71841b6398485a209875310042aa09349400a5
Phil Warren <P.Warren@pcs.hants.sch.uk>
2018-03-01 17:58:15 +0000

    Weather Update

--- sev-weather.php
+++ sev-weather.php
@@ -26,7 +26,15 @@
         <div id="park-post" class="medium-12 columns">
             <h3>Information</h3>
             <p>Dear Parent/ Carer</p>
-            <p>As severe weather is forecast later today a decision will be made around 6pm about whether the school will open tomorrow. We will update Facebook, and the website when that decision has been made, or if there are any other changes.</p>
+            <p>Park Community School will be closed on Friday, 2nd March 2018.
+            <p>We have taken this decision reluctantly but in the light of the best information currently available to us from the met office. The weather and snowfall position remains unpredictable. The met office places Havant in the yellow warning zone and very near the more serious amber warning zone. Snow is currently expected until 5am Friday, though this may change. With further snow from mid morning onwards on Friday.</p>
+            <p>The consequence is that travel to the school tomorrow will be problematic and ultimately depend on snowfall overnight with the high risk of ice on roads. We think it better, given the likelihood of snowfall and continuing freezing conditions, to make the decision now rather than early tomorrow morning. We believe most students, parents and staff would prefer to know where they stand as soon as possible rather than a delayed decision based on the small chance things will improve sufficiently to have enough staff at school to open safely.</p>
+            <p>Year 10 and 11 have exams at various points next week and should take the time for further revision.</p>
+            <p>The Year 8 Options Parents' Evening due to take place this evening will now take place at the same time next Thursday, 8 March 2018.</p>
+            <p>We anticipate the school will be open on Monday as usual. In the meantime, stay warm! 
+            <br>
+            <p>Best wishes,</p>
+            <p>Mr Anders</p>
                          
         </div>
     </div><br>
