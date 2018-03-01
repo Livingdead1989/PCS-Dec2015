09aef3f3dcb0f9fe71b0841cbcfe62a8bceef2f0
Phil Warren <P.Warren@pcs.hants.sch.uk>
2018-03-01 08:11:06 +0000

    Weather Update

--- sev-weather.php
+++ sev-weather.php
@@ -26,10 +26,8 @@
         <div id="park-post" class="medium-12 columns">
             <h3>Information</h3>
             <p>Dear Parent/ Carer</p>
-            <p>We have received weather warnings for Thursday and Friday, today’s forecast suggests there is a 25%-50% chance of snow. We currently plan to keep the school open as usual, but we will review the situation throughout the rest of the week. Any decision to close will depend largely on ensuring the health and safety of our students and staff. Many staff travel some distance and maybe unable to do so if snow settles in the wider area. If we are unable to be certain of sufficient staffing levels a decision to close may need to be taken. This decision will be made as promptly as possible and ideally before students set off for school in the morning. However, snow conditions may mean the decision to close is taken during the school day. Please make sure we have up to date records in order that we can contact you during the school day. You will then be asked to collect your child at the earliest possible opportunity.</p>
-            <p>At present the parents’ evening for Year 8 is still taking place on Thursday, a decision to delay this evening will be taken if the weather prevents its smooth and safe running. If delayed, it will most likely take place a week later, on Thursday, 8 March, at the same time. Confirmation of any new date will be advised separately.</p>
-            <p>We will keep you informed if plans to open change. Before setting off on your journey please find the most up-to-date information via: school email, on the school website and by using school social media. We will also send a text message to all phone contacts held in this way. If you do not currently receive texts from the school, please provide a mobile or home/work phone number to school reception. If you have not yet done so, please sign up for our school information via MyEd app.</p>
-             
+            <p>Park will be open this morning. However forecasts and police advice is that problems are likely with travel this afternoon. So school will close after lunch time at 1.25pm. No parents evening today.</p>
+                         
         </div>
     </div><br>
 </section>
