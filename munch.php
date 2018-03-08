ff15919eb207d2e95c6be8f18681ccc564fbbb3c
Phil Warren <P.Warren@pcs.hants.sch.uk>
2018-03-08 14:25:32 +0000

    Munch Page

---
+++ munch.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Munch</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-misc">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-misc">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Munch</h1>
+            <p class="text-capitalize">MUNCH ensures that no child, no matter what school or age need go hungry during the holidays.</p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-6 columns">
+            <p>As a school we were aware of the food poverty that some of our community experience and were concerned our students were going hungry. This was reinforced when our local community told us how the food bank was consistently running out of food. We set up a community group to tackle food poverty in our area and in April 2017 MUNCH was born.</p>
+            <p>We successfully contacted external agencies for funding and MUNCH is now in operation supplying nutritious 2 course meals and activities for children and their families attending any Leigh Park school during the school holidays.   Play equipment is provided, along with learning practitioners, who help our guests learn via play encouraging learning of vital maths and English skills.   Our school nurse is also on hand, on a drop in basis for any concerns parents may have.</p>
+            <p>Our vision for MUNCH is to develop a sustainable local social enterprise by establishing a community food shop and café.  This will provide volunteering and employment opportunities, as well as a casual meeting and eating venue to reduce social isolation – watch this space for more details.</p>
+            <p>We work in partnership with organisations who have excess food which otherwise would go into landfill .  This helps to fill our MUNCH fridge in the Dickinson Centre.  This is always fully stocked with emergency food for local families who are most in need.</p>
+            <p>We have expanded MUNCH to both Thursday nights and Sunday lunches.  2 course Sunday lunches start monthly from Sunday 25th March and Thursday nights meals start weekly from Thursday March 29th.</p>
+        </div>
+        <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-6 columns"><img src="images/munch/munch_logo.png" alt="MUNCH"></div>
+    </div>
+    <br><br><br>
+</section>
+
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-push-6 medium-6 columns">
+            <h4>MUNCH Testimonial (PO9 Pioneers, Barney Barron)
+            <p>I live and work in Leigh Park and have the privilege of partnering in some of the community projects delivered by Park Community School.  In particular the MUNCH project is an area the school have gone above and beyond to meet the needs of the community. </p>
+            <p>Food poverty is a serious and increasingly common problem in our community.</p>
+            <p>For many who cannot afford to feed their children leaving them at risk of hunger and malnutrition. MUNCH provide a healthy breakfast and cooked lunch.  This extends to Christmas day with hearty Christmas fare and entertainment and soon will be extending this provision to term time with a weekly evening meal and a Sunday lunch.</p>
+            <p>The MUNCH project also offers a real sense of community and support.  Often lone parents struggling to cope feeling quite isolated really benefit from coming to MUNCH and having the children entertained whilst having someone to talk to.  The value of the MUNCH project for our community is immeasurable providing a lifeline to many.</p>
+        </div>
+        <div class="text-center">
+                <a href="assets/munch_review.pdf" target="_blank" class="button success radius">Munch Review</a>
+            </div>
+        <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-6 medium-offset-0 medium-6 columns"><img src="images/munch/munch_fridge.jpg" alt="Munch Fridge></div>
+    </div>
+    <br><br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-6 columns">
+            <h4>MUNCH in numbers</h4>
+            <hr>
+                <ul class="bullet">
+                    <li>2,500 meals provided so far</li>
+                    <li>1,496 children attended</li>
+                    <li>34 schools involved</li>
+                    <li>60 Average daily attendance</li>
+                    <li>20+ organisations fund the project</li>
+                    <li>4 churches  volunteering</li>
+                    <li>120 guests for Christmas Lunch</li>
+                </ul>
+            <h4>Contact Information</h4>
+            <hr>
+            
+        </div>
+        <br><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
