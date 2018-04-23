c2e0930f4a2fc1b376579ef87771dfc90a7ecb68
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2018-04-23 08:43:37 +0100

    Parent Partnership Page Added, Top Nav included and Trips Updated

---
+++ parent-partnership.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Parent Partnership</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-parent">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-parent">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Parent Partnership</h1>
+            <p class="text-capitalize"></p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="columns">
+            
+            <h2>Our aims are expressed through the following principles:</h2>
+
+            <p>At Park we aim to be at each child's side every step of the way, guiding them as their character develops and as they make their own discoveries about what they love and may one day love to do. Through our main school site and three additional facilities, away from school, we provide pupils with a range of extra opportunities to raise their aspirations and ensure they are very well prepared for adult life. We are always looking for adults to help us find new ways to work with young people and the local community.</p>
+            
+            <br>
+            <h3>We have 5 ways of expressing our work:</h3>
+
+            <p><strong>Much more than just a school</strong> [We aim to perpetually innovate in order to provide aspiration, inspiration and new ways to work with young people, parents and our local community. We therefore offer an expansive educational experience: Apex Construction Skills Centre, Park Farm, PDP; Print and enterprise unit and Inspired Education, SCITT and Solent Teaching Alliance].</p>
+            <p><strong>Success for all through attainment, resilience and autonomy</strong> [We aim to ensure that all of our students are well prepared for the next step of long successful lives; demonstrating finish, polish, panache].</p>
+            <p><strong>We are guardians of the flame; kindling fires</strong> [“At Park we will be at your child's side every step of the way, guiding them as their character develops and as they make their own discoveries about what they love.” We believe education is a way to change lives: to kindle fires and awaken possibilities].</p>
+            <p><strong>Love to teach: joy, glee and delight</strong> [We believe all learners should be welcomed, inspired, challenged, and invested in as individuals, we also believe all staff should model passion for Park so students and their families experience joy, glee and delight].</p>
+            <p><strong>We believe that we make a difference and that qualifications alone are not enough we believe in empowering students to feel confident, be independent and make good choices</strong> [existentialism] <strong>within a caring and sharing community</strong>.</p>
+            
+            <br><br>
+            <h3>Our objectives include the creation of productive partnerships with all our parents</h3>
+
+            <ol>
+                <li>Our commitment to working effectively with our parents is fundamental to the Park ethos and is evidenced in our commitment to achieving the LPPA. <em>[KPI 1]</em></li>
+                <li>The continuous drive to improving our partnership with parents is reflected in improvement planning at all levels. <em>[KPI 2]</em></li>
+                <li>Park is a welcoming, communicative and friendly place for parents. <em>[KPI 3]</em></li>
+                <li>We promote the support and participation of all parents in their children’s learning. In addition, we promote opportunities for joint parent and child participation, as well as parent learning, wherever possible. <em>[KPI 4]</em></li>
+                <li>We seek to provide outstanding induction for all new parents. <em>[KPI 5]</em></li>
+                <li>We provide parents with helpful and user-friendly guidance and information to help them support their children’s learning. <em>[KPI 6]</em></li>
+                <li>We have clear policies to establish effective home–school links and improve children’s attendance, punctuality, progress and positive participation in school. <em>[KPI 7]</em></li>
+                <li>The school provides outstanding support for all parents as their children move through or leave the school. <em>[KPI 8]</em></li>
+            </ol>
+
+            <br><br>
+            <h3>We aspire to ensure:</h3>
+
+            <p><strong>Welfare and safeguarding</strong></p>
+
+            <ol>
+                <li>100% face to face contact with parents each academic year to secure support for all students’ progress.</li>
+                <li>All parents are contacted within 2 working days, wherever possible, regarding any concerns about their child. </li>
+                <li>Parents contacted via texts and phone calls if a student is absent.</li>
+                <li>All parents contacted about any incident their child may be involved in.</li>
+                <li>Parents are able to arrange a meeting at any time if they have concerns re their child’s welfare.</li>
+            </ol>
+
+            <p><strong>Academic progress</strong></p>
+            <ol>
+                <li>All parents will receive a data-based report on their child’s progress 4 times per year.</li>
+                <li>All parents will receive a clearly written full report on their child’s progress once per year. The report will explain simply and clearly what has been studied, how the student has made progress and what precisely s/he needs to do to improve.</li>
+                <li>All parents to be actively encouraged to attend a Parents Evening to discuss their child’s progress with subject teachers once per year. In the case of year 8 this will be part of the options process.</li> 
+                <li>All parents actively encouraged to attend Student Review Meetings to discuss their child’s progress and academic targets with mentors twice per year.</li>
+                <li>All parents in year 11 invited to attend additional parents’ evenings aimed at helping them to better support their child’s aspirations and examination success.</li>
+            </ol>
+
+            <p><strong>Interventions</strong></p>
+            <ol>
+                <li>Termly attendance clinics for targeted students to discuss attendance issues.</li>
+                <li>Early morning phone calls for those students who are attendance concerns.</li>
+                <li>Pastoral drop-in for parents to discuss any welfare concerns.</li>
+                <li>Revision sessions for Year 11 parents and students.</li>
+            </ol>
+
+            <p><strong>Community</strong></p>
+            <ol>
+                <li>We run a project called MUNCH which ensures that no child, no matter what school or age need go hungry during the holidays. This is being expanded to Thursday tea times and Sunday lunches.</li>
+                <li>Parents are given the opportunity to enrol their child for a week’s Summer School to aid transition.</li>
+                <li>Parents can hire our excellent facilities at affordable prices.</li>
+                <li>We offer a whole range of affordable children parties to enable our children to have a party with good quality equipment in a safe environment.</li>
+                <li>We have established our Student Recruitment Agency which creates paid work opportunities for our students from Year 7.</li>
+                <li>Range of events are provided for example, 6 weeks of Christmas magical festive programme, Family Picnic in the Park, fundraising events.</li>
+                <li>Courses run in the Dickinson Centre ranging from parenting skills to first aid.</li>
+                <li>Monthly pastoral informal support sessions.</li>
+                <li>Open 51 weeks a year open until 10pm – staff available to assist parents.</li>
+                <li>Funding applied for to the benefit of families and students.</li>
+
+            </ol>
+            <br><br>
+
+        </div>
+    </div><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
