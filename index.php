0913b215e3a3753056a758654323d72c7348e74a
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2018-04-24 13:36:57 +0100

    RaOP Vetted Images

--- index.php
+++ index.php
@@ -100,6 +100,8 @@
                     <div class="medium-offset-1 medium-10 large-offset-0 large-12">
                         <div class="homepage-slider">
                             <div><img src="images/events/race_at_our_place/2018/race_at_our_place_1.jpg" alt="Race at Our Place 2018"></div>
+                            <div><img src="images/events/race_at_our_place/2018/race_at_our_place_2.jpg" alt="Race at Our Place 2018"></div>
+                            <div><img src="images/events/race_at_our_place/2018/race_at_our_place_3.jpg" alt="Race at Our Place 2018"></div>
                         </div>
                         <p class="subheader text-center">Race at Our Place 2018</p>
                     </div>
