023f11425fd068cfd2f593bc77730fc41043a084
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2018-06-05 15:25:16 +0100

    Removal of expired events

--- events.php
+++ events.php
@@ -50,65 +50,7 @@
                   <div class="cost-wrap"></div>
                 </div>
               </div> -->
-<!--                  Event box -->    
-
-<!--                    Event Box  -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">Fri 25th May</p>
-                  <p class="event-time">18:30</p>
-                </div>
-                <h2 class="event-title">Leather &amp; Lace</h2>
-                <p class="event-subheader">ROCK ANTHEMS &amp; POWER BALLADS SHOW</p>
-                <p class="event-description">"Leather &amp; Lace" is a dynamic new show bringing all those 'Classic Rock Anthems' &amp; 'Power Ballads' you know and love to the stage.<br><br><a href="//www.tickettailor.com/events/leatherandlaceshow/159178">Buy Tickets Online</a></p>
-                <div class="link-wrap">
-                  <a href="assets/events/leather_and_lace.jpg" target="_blank" class="button success radius">Flyer</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="//www.google.com/maps/place/Park+Community+School/@50.8727051,-0.9867002,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">12.50</div>
-                </div>
-              </div>
-<!--                  Event box -->
-
-
-<!--                    Event Box  -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">Half Term</p>
-                  <p class="event-time">Various</p>
-                </div>
-                <h2 class="event-title">Half Term Holiday Fun!</h2>
-                <p class="event-subheader">Half Term Events</p>
-                <p class="event-description">Various events running throughout Half Term including: Dance, Assault Course and Bouncy Castle, Soft Play, Basket Ball, Football and More...</p>
-                <div class="link-wrap">
-                  <a href="assets/events/half_term_flyer.pdf" target="_blank" class="button success radius">View our Flyer</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Varies</div>
-                </div>
-              </div>
-<!--                  Event box -->
-
-<!--                    Event Box  -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">29th May - 1st June</p>
-                  <p class="event-time">12:00-13:00</p>
-                </div>
-                <h2 class="event-title">MUNCH</h2>
-                <p class="event-subheader">Free Meals</p>
-                <p class="event-description">Two course meal without charge for childing attending ANY school in Leigh Park. <br>Join us for Dinner on Thursdays - 17:30-18:30. <br><strong>Donations Welcome</strong></p>
-                <div class="link-wrap">
-                  <a href="assets/events/munch.pdf" target="_blank" class="button success radius">View our Flyer</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Free</div>
-                </div>
-              </div>
-<!--                  Event box -->
+<!--                  Event box --> 
 
 <!--                    Event Box  -->
               <div class="event-wrap">
