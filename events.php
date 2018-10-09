95f735c884021176ee030da686a5d266b3936420
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2018-10-09 15:52:08 +0100

    christmas munch

--- events.php
+++ events.php
@@ -53,6 +53,25 @@
 <!--                  Event box --> 
 
 <!--                    Event Box  -->
+              <div class="event-wrap feature-event sticky-event">
+                <div class="date-wrap">
+                  <p class="event-date">25th December</p>
+                  <p class="event-time">11:00-16:00</p>
+                </div>
+                <h2 class="event-title">Christmas Day</h2>
+                <p class="event-subheader">MUNCH</p>
+                <p class="event-description">Park Community's MUNCH team kindly invite you to join them for a traditional Christmas lunch at Park Community School. <br>The tables will be brimming with a traditional freshly prepared Christmas meal. There will be inflatables and games for the children and newspapers and chat for those who want company on the day.</p>
+                <div class="link-wrap">
+                  <a href="assets/events/christmas_munch.pdf" target="_blank" class="button success radius">Your Invite</a>
+                </div>
+                <div class="bottom-wrap">
+                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
+                  <div class="cost-wrap">Free</div>
+                </div>
+              </div>
+<!--                  Event box --> 
+
+<!--                    Event Box  -->
               <div class="event-wrap sticky-event">
                 <div class="date-wrap">
                   <p class="event-date">Thursday</p>
