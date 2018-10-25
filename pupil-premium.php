ed87038ec37a67088af655167e9f36291eb041e9
LivingDead1989 <livingdead1989@LivingDead1989s-Mac-mini.local>
2018-10-25 15:39:51 +0100

    PP Changed from ECa

--- pupil-premium.php
+++ pupil-premium.php
@@ -25,9 +25,62 @@
         <br>
         <div class="medium-7 columns">
           <p>Grant Allocation:</p>
-          <p>In 2017/2018 the school received £431,970  for Pupil Premium (PP) plus £2,100 for Children in Care.</p>
+          <p>At Park Community School we are committed to providing ALL students with a great education; securing strong academic outcomes alongside experiences of the curriculum and the world around us which broaden horizons and encourage a love of learning. We encourage students to discover what they love and therefore invest heavily in securing high quality teaching and enabling all students to access a broad and balanced curriculum, which includes a wealth of experiences outside of the classroom.</p>
+          
+          <p>Our Pupil premium Strategy is focussed on 3 broad areas:</p>
+          <ol>
+            <li>High quality teaching which develops independent thinking</li>
+            <li>Parental engagement and attendance</li>
+            <li>Cultural capital and student leadership</li>
+            <li>Behaviour for Learning</li>
+          </ol>
+
+          <p>Our Pupil Premium strategy is based on a comprehensive understanding of our students, their families and the community and is regularly reviewed using internal and external data and current research produced the Education Endowment Foundation. Park Community School participated in the ‘Improving Outcomes for Disadvantaged Learners’ project with Hampshire and Rosendale Research during 2017-18.</p>
+          <p>For 2018/2019 our allocated budget is:</p>
+          
+           <ul class="block-grid-large-2">
+            <li>
+              <p>For 2018/2019 our allocated budget is:</p>
+            </li>
+            <li>
+              <table>
+                <tr>
+                  <td>Pupil premium</td>
+                  <td>£413,270</td>
+                </tr>
+                <tr>
+                  <td>Service Children</td>
+                  <td>£2,400</td>
+                </tr>
+                <tr>
+                  <td>Post Looked After Children</td>
+                  <td>£0</td>
+                </tr>
+              </table>
+            </li>
+          </ul>
+
+          <p>Grant Allocation:</p>
+          <p>In 2017/2018 the school received £431,970 for Pupil Premium (PP) plus £2,100 for Children in Care.</p>
           <p>The school spent £475,095 providing additional support for PP students. The pupil premium is grant funding and is in addition to the school’s Delegated Budget. The school chooses to invest additional funding from its delegated budget to support pupil premium funding.</p>
           <p>The Pupil Premium is allocated by calculating the number of students who are on Free School Meals (FSM) or have had FSM in the last six years. Students who have been in care continuously for more than six months or have a parent in the Armed Services also lead to some additional funding for the school.</p>
+        </div>
+
+        <div class="small-10 small-centered medium-uncentered medium-5 columns">
+          <br><img src="images/pupil-premium/pupil-premium-01.jpg" alt="Park Community School students enjoy school life." />
+        </div>
+    </div>
+    <br><br><br>
+</section>
+
+<section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-8 medium-push-4 columns">
+          <p>In 2018 the gap in achievement between non PP and PP students in GCSEs was -0.12 for Progress 8 (PP-0.88, non PP -0.76)</p>
+          
           <ul class="accordion" data-accordion>
             <li class="accordion-navigation">
               <a href="#panel1a">Main barriers to educational achievement faced by eligible students could include:</a>
@@ -50,46 +103,6 @@
             </li>
           </ul>
           <br class="show-for-small-only">
-
-        </div>
-
-        <div class="small-10 small-centered medium-uncentered medium-5 columns">
-          <br><img src="images/pupil-premium/pupil-premium-01.jpg" alt="Park Community School students enjoy school life." />
-        </div>
-    </div>
-    <br><br><br>
-</section>
-
-<section class="grey-bg">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row">
-        <div class="medium-8 medium-push-4 columns">
-          <h3>How Pupil Premium is spent to address these barriers</h3>
-          <p>The Pupil Premium spent is used to raise attainment, autonomy and resilience, promote social skills, independent learning and positive behaviour, in order to increase student progress.</p>
-          <p>In 2016 the gap in achievement between not PP and PP students in their GCSE results was 0.02 for Progress 8 (PP -0.12, Not PP -0.14). The gap between Not PP and PP students achieving English and Maths A*-C was 6%. 42% of PP were entered for English Baccalaureate.</p>
-          <ul class="block-grid-large-2">
-            <li>
-              <p>For 2018/2019 our allocated budget is:</p>
-            </li>
-            <li>
-              <table>
-                <tr>
-                  <td>Pupil premium</td>
-                  <td>£413,270</td>
-                </tr>
-                <tr>
-                  <td>Service Children</td>
-                  <td>£2,400</td>
-                </tr>
-                <tr>
-                  <td>Post Looked After Children</td>
-                  <td>£0</td>
-                </tr>
-              </table>
-            </li>
-          </ul>
         </div>
         <div class="hide-for-small-only medium-uncentered medium-4 medium-pull-8 columns">
           <br>
@@ -113,29 +126,29 @@
                 <h4>Teaching &amp; Learning <br><small>addressing student low aspiration and gaps in learning</small></h4>
                 <hr>
                 <ul class="circle">
-                    <li>Emphasis on effective feedback and marking</li>
-                    <li>Summer School</li>
-                    <li>Residential visits</li>
-                    <li>Reduced contact time for teaching staff to allow for quality lesson planning</li>
-                    <li>Smaller class sizes in key subjects</li>
-                    <li>Writing portfolios</li>
-                    <li>Staff focus on success for all PP students</li>
-                    <li>1 : 1 Mentoring</li>
+                  <li>Emphasis on effective feedback and marking</li>
+                  <li>Focus on collaborative learning</li>
+                  <li>Explicit teaching of memory and revision</li>
+                  <li>Residential visits</li>
+                  <li>Staff focus on success for all PP students</li>
+                  <li>1 : 1 Mentoring</li>
                 </ul>
               </li>
+
               <li>
-                <h4>Curriculum <br><small>addressing student low aspiration and gaps in learning</small></h4>
+                <h4>Curriculum experiences at school farm and printing unit<br><small>addressing student low aspiration and gaps in learning</small></h4>
                 <hr>
                 <ul class="circle">
-                    <li>Numeracy programmes</li>
-                    <li>Study Clubs</li>
-                    <li>Literacy programmes</li>
-                    <li>Interventions to support learning</li>
-                    <li>Enrichment activities</li>
-                    <li>International visits/trips</li>
-                    <li>Social &amp; cultural visits/trips</li>
+                  <li>Numeracy programmes</li>
+                  <li>Period 6</li>
+                  <li>Literacy programmes</li>
+                  <li>Interventions to support learning</li>
+                  <li>Enrichment activities</li>
+                  <li>International visits/trips</li>
+                  <li>Social & cultural visits/trips</li>
                 </ul>
               </li>
+
               <li>
                 <h4>Inclusion (could be used) <br><small>addressing vulnerability arising from mental health and trauma within the families together with a lack of trust in, or significant dependence on, outside agencies</small></h4>
                 <hr>
@@ -143,10 +156,10 @@
                     <li>Home Liaison team; fortnightly referral meeting to follow up targeted PP students</li>
                     <li>Referral meeting attended by Key Stage leaders, Lead WSG behavior, Lead WSG pastoral, SENCO, Deputy Headteacher pastoral, Medical officer. Focus:</li>
                     <li>Nurture programmes</li>
-                    <li>Solace: counselling and support service</li>
                     <li>Individual programmes</li>
                     <li>Communitas: Youth Workers available during school day</li>
-                    <li>Medical &amp; Emotional support</li>
+                    <li>Medical & Emotional support</li>
+
                 </ul>
               </li>
               <li>
@@ -154,7 +167,8 @@
                 <hr>
                 <ul class="circle">
                     <li>Interventions to support families (those families identified on the Supporting Families programme; Attendance where there is family history of poor attendance)</li>
-                    <li>IAG (Information, Advice and Guidance) support &amp; intervention</li>
+                    <li>IAG (Information, Advice and Guidance) support & intervention</li>
+
                 </ul>
               </li>
               <li>
@@ -162,7 +176,7 @@
                 <hr>
                 <ul class="circle">
                     <li>Attendance groups</li>
-                    <li>1:1 Support</li>
+                    <li>1:1 Support and intervention</li>
                 </ul>
               </li>
           </ul>
@@ -206,113 +220,10 @@
     <div class="row">
       <div class="large-12 columns">
 
-          <h3>Outcomes: Key Assessment point 2 (KA2) December 2016 shows:</h3>
-
-          <p><strong>Overall Attitude To Learning (OATL) is Good in All Years</strong> <em>i.e. above average score of 2:</em></p>
-          <table>
-            <tr>
-              <td>Year 7</td>
-              <td>Year 8</td>
-              <td>Year 9</td>
-              <td>Year 10</td>
-            </tr>
-            <tr>
-              <td>7.41</td>
-              <td>5.53</td>
-              <td>7.35</td>
-              <td>6.84</td>
-            </tr>
-          </table>
-          <p><strong>Homework is Good in All Years</strong> <em>i.e. above average score of 2:</em></p>
-          <table>
-            <tr>
-              <td>Year 7</td>
-              <td>Year 8</td>
-              <td>Year 9</td>
-              <td>Year 10</td>
-            </tr>
-            <tr>
-              <td>4.39</td>
-              <td>2.88</td>
-              <td>3.72</td>
-              <td>3.6</td>
-            </tr>
-          </table>
-          <p><em>Homework clubs run by Communitas (youth workers) have been introduced to support this by providing additional support and a quiet space to work with computers</em></p>
-
-          <p><strong>P8 PP/non PP Gap</strong> <em>i.e. gaps are small</em></p>
-          <table>
-            <tr>
-              <td>Year 7</td>
-              <td>Year 8</td>
-              <td>Year 9</td>
-              <td>Year 10</td>
-            </tr>
-            <tr>
-              <td>-0.1</td>
-              <td>+0.1</td>
-              <td>-0.07</td>
-              <td>+0.15</td>
-            </tr>
-          </table>
-          <p><strong>Year 11 P8 PP/non PP Gap</strong></p>
-          <table>
-            <tr>
-              <td>English</td>
-              <td>Maths</td>
-              <td>Ebac</td>
-              <td>P8</td>
-            </tr>
-            <tr>
-              <td>-0.18</td>
-              <td>-0.47</td>
-              <td>-0.43</td>
-              <td>-0.38</td>
-            </tr>
-          </table>
-          <p><strong>Attendance:</strong> 128 PA who are PP and 46 who are non PP. Of the 112 PAs who have improved their attendance 80 were PP and 6 of those now have attendance of above 90%.</p>
+          
         </div>
       </div>
-    </div>
     <br><br>
-</section>
-
-<section class="grey-bg">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row">
-      <div class="medium-12 columns">
-        <h4>Catch-up Funding for Year 7 2017-2018</h4>
-        <p>In 2017/2018 we received £17,371 catch up funding which was used to provide:</p>
-        <p>One to One reading, spelling and literacy intervention for Year 7 students - with some students being in receipt of more than 1 round of intervention.</p>
-        <p>Additional classroom support for one class.</p>
-        <p>In total more than 41 students benefitted from this intervention. The impact of this support was measured through: student and parent feedback, Year 7 progress data, reading and spelling data.</p>
-        <p>All students receiving intervention demonstrated improvement in their reading with 24 making double ratio gains. Alongside this, 25 students also made progress in both their spelling and reading with 19 continuing to make further above chronological gains demonstrating the mastery of key skills enabling them to continue to make progress.</p>
-        <p>Parent and student feedback demonstrated positive increases regarding confidence and perception of ability alongside willingness to engage in additional learning outside of the classroom.</p>
-        <p>Any future funding will be used to help these students secure and close gaps in their literacy or numeracy through small group intervention, 1 to 1 intervention and in class support.</p>
-
-        <p>We are expecting to receive a similar amount in March 2019 for 2018/2019 catch-up funding.</p>
-      </div>
-    </div>
-    <br><br><br>
-</section>
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-      <div class="medium-push-5 medium-7 columns">
-        <p>If you believe your son/daughter is entitled to Free School Meals or you are uncertain as to whether they qualify for Free School Meals, Reception staff are happy to talk to you.</p>
-        <p>If you would like some advice and/or support on completing the Free School Meal application form, please come into Reception at any time to talk to staff, who are happy to help.</p>
-        <p>You can also find further information on eligibility and how to apply by visiting our <a href="/catering.php">Catering</a> page.</p>
-
-        <p><a href="/assets/pupilpremium/qa-pp-adopted-children.pdf" target="_blank">Adopted Children Questions and Answers</a></p>
-      </div>
-      <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-7 medium-offset-0 medium-5 columns"><img src="images/pupil-premium/pupil-premium-04.jpg" alt="Park Community School - Students enjoy the freedom to read real books"></div>
-    </div>
-    <br><br><br>
 </section>
 
 <!-- FOOTER -->
