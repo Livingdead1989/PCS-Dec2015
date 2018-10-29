eff40ab0f4cf64e4ec44d8cce3e739bcc0d62eee
LivingDead1989 <livingdead1989@LivingDead1989s-Mac-mini.local>
2018-10-29 09:32:02 +0000

    Munch Layout Changes - Pic Added

--- munch.php
+++ munch.php
@@ -36,9 +36,14 @@
 <section>
     <div class="row">
         <br>
-        <div class="medium-12 columns">
+        <div class="medium-8 columns">
             <p>As a school we were aware of the food poverty that some of our community experience and were concerned our students were going hungry. This was reinforced when our local community told us how the food bank was consistently running out of food. We set up a community group to tackle food poverty in our area and in April 2017 MUNCH was born.</p>
             <p>We successfully contacted external agencies for funding and MUNCH is now in operation supplying nutritious 2 course meals and activities for children and their families attending any Leigh Park school during the school holidays.   Play equipment is provided, along with learning practitioners, who help our guests learn via play encouraging learning of vital maths and English skills.   Our school nurse is also on hand, on a drop in basis for any concerns parents may have.</p>
+        </div>
+        <div class="medium-4 columns">
+            <img src="images/schoolmeals/schoolmeals-01.jpg">
+        </div>
+        <div class="medium-12 columns">
             <p>Our vision for MUNCH is to develop a sustainable local social enterprise by establishing a community food shop and café.  This will provide volunteering and employment opportunities, as well as a casual meeting and eating venue to reduce social isolation – watch this space for more details.</p>
             <p>We work in partnership with organisations who have excess food which otherwise would go into landfill .  This helps to fill our MUNCH fridge in the Dickinson Centre.  This is always fully stocked with emergency food for local families who are most in need.</p>
             <p>We have expanded MUNCH to both Thursday nights and Sunday lunches.  2 course Sunday lunches start monthly from Sunday 25th March and Thursday nights meals start weekly from Thursday March 29th.</p>
@@ -85,7 +90,6 @@
         </div>
         
         <div class="medium-6 columns text-center">
-            <br>
             <img src="images/munch/munch_asda_award2.jpg">
         </div>
 
