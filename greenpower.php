fa265eb39a4b301989912e1432a7857524bbf47b
LivingDead1989 <livingdead1989@LivingDead1989s-Mac-mini.local>
2018-11-06 13:33:21 +0000

    Greenpower Paragraph and Image Slider Added

--- greenpower.php
+++ greenpower.php
@@ -47,6 +47,9 @@
                 <li>Chase</li>
             </ul>
             <p>The full race results are hosted by <a href="//www.bbk-online.net/gpt/">BBK online</a> and can be find on their website</p>
+
+            <h3>How to join the team?</h3>
+            <p>Students can take part by having an exemplary behaviour record and have a keen interest in engineering. Places are limited - please see Mr Payne for details.</p>
         </div>
     </div>
     <br><br>
@@ -57,6 +60,18 @@
         <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
     <div class="row">
+        <div class="columns">
+            <p>Greenpower is a charity funded organisation that invites young students from the age of 11-16 to build an electric car (hence the name Greenpower) and race it at a variety of different venues around the country. Greenpower is an organisation that ‘’Promotes sustainable engineering to young people’’ Park Community School, Havant, Hampshire, believe that they are ‘’Much more than just a school’’, this is part of the reason why they have become part of the Greenpower family.</p>
+            <p>Park have been competing for 10 years in Greenpower with their original car called ‘Team Spirit’ being designed and built by its students. They have recently invested in 3 new kit cars manufactured by Greenpower and recruited new enthusiastic Park STEM engineers to build these new cars to try and become more competitive and challenge the top teams. The new Greenpower teams built the 3 new cars which are aptly named ‘Team Spirit 2’, ‘Keep Up’ and ‘Chase’.</p>
+            <p>The cars have been built by students to allow them to develop their STEM knowledge and to give an insight into possible future careers. They then compete at a national level in different areas of the country. Some of the races include Goodwood Motor circuit, Ford Dunton test track, Dunsfold (Top Gear test track) and Rockingham Motor Speedway. Over 260 cars competed in 2018 with 10,000 students taking part across the country. We finished 6th 29th and 39th out of 160 cars within our category of car.</p>
+
+            <div class="homepage-slider">
+                <div><img src="images/greenpower/greenpower1.jpg" alt="Park Community Schools Greenpower Team"></div>
+                <div><img src="images/greenpower/greenpower2.jpg" alt="Park Community Schools Greenpower Team"></div>
+                <div><img src="images/greenpower/greenpower3.jpg" alt="Park Community Schools Greenpower Team"></div>
+            </div>
+            <p class="subheader text-center">Park's Greenpower Team</p>
+        </div>
         
     </div>
     <br><br>
