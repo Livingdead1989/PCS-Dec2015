e6072d6b26fc9582056dbf2af3bdf2ed1c4b43ce
LivingDead1989 <livingdead1989@LivingDead1989s-Mac-mini.local>
2018-12-18 11:21:13 +0000

    Removed old english content replaced with two ext links

--- english.php
+++ english.php
@@ -45,110 +45,12 @@
             <p><strong>Reading</strong> – Pupils will have opportunities to read full texts for both pleasure and for gathering information. We will expose pupils to a range of English literature, seminal world literature and texts from Shakespeare, with the focus on developing an appreciation of texts, the ability to read critically and to engage with and access challenging texts.</p>
             <p><strong>Writing</strong> – Pupils will be given opportunities to develop their written skills in order to develop their written accuracy, fluency and use of writing for both pleasure and information. Pupils will be expected to write essays, stories, scripts, poetry, arguments, articles and letters using the writing process of planning, drafting, editing and re-drafting. Alongside this pupils will develop their ability to refine texts, enhance their vocabulary, as well as develop their understanding of spelling, punctuation and grammar.</p>
             <p><strong>Spoken communication</strong> – Pupils will use spoken communication to underpin the development of their reading and writing skills. They will develop and practise their speaking skills, using a range of communication skills effectively. This will involve debating in groups, presenting to others, interacting and developing ideas through talk and using role play to engage with the texts being studied.</p>
-
-            <ul class="accordion" data-accordion role="tablist">
-
-                <li class="accordion-navigation">
-                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">Keystage 3 - National Curriculum</a>
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Reading and writing of fiction texts.</li>
-                            <li>Reading and writing of non-fiction texts.</li>
-                            <li>Writing, reading and performing poetry and plays.</li>
-                        </ul>
-                        <p>Many of these units finish with a Rich Challenge.</p>
-                        <p>These challenges allow pupils to apply the skills that they have learnt to real audiences and purposes. They are also a celebration of the learning.</p>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Reading for pleasure and developing good reader strategies.</li>
-                            <li>Using PEE to describe and analyse a wide range of texts.</li>
-                            <li>Developing planning, drafting and editing skills.</li>
-                            <li>Writing accurately for a variety of audiences and purposes.</li>
-                            <li>Developing speaking to different audiences and using techniques to interest the listener.</li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Keystage 4 - GCSE</a>
-                    <div id="panel2d" class="content" role="tabpanel" aria-labelledby="panel2d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li><h5>Unit 1</h5>
-                            Two controlled assessments tasks. Reading, analysing and responding to non-fiction texts. Completing a piece of writing that could be used in the ‘real world’.
-                            </li>
-                            <li><h5>Unit 2</h5>
-                            Exam unit with 1 exam. Writing a piece of text that could be used for a real audience and a real purpose. Studying a Shakespeare play with a particular focus on character, performance techniques and key themes. Reading a novel from a different culture, focusing on character, the language the writer uses and the key themes.</li>
-                            <li><h5>Unit 3</h5>
-                            Two controlled assessment tasks. Developing a piece of creative writing that could be descriptive or a narrative. Studying a range of poetry and discussing the poets’ uses of language, poetic devices and themes.</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Using good reader strategies to support the independent understanding of texts.</li>
-                            <li>Using PEE to discuss and analyse texts, with a particular focus on language, alternative ideas and evaluation.</li>
-                            <li>Refining planning, drafting and editing skills in order to be able to create interesting and believable texts in tight time limits.</li>
-                            <li>Using a variety of writing tools, such as language, punctuation and sentence structure for deliberate effect on the reader.</li>
-                            <li>Being able to lead and shape discussions, present clear and detailed ideas and take on believable and realistic roles in performances.</li>
-                        </ul>
-                        
-                        <small>For more information please visit the <a href="http://www.edexcel.com/" target="_blank">EdExcel website</a></small>
-
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel3d" role="tab" id="panel3d-heading" aria-controls="panel3d">Keystage 4 - GCSE - Language</a>
-                    <div id="panel3d" class="content" role="tabpanel" aria-labelledby="panel3d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li><h5>Unit 1</h5>
-                            Two controlled assessments tasks. Reading, analysing and responding to non-fiction texts. Completing a piece of writing that could be used in the ‘real world’.
-                            </li>
-                            <li><h5>Unit 2</h5>
-                            Exam unit with 1 exam: Writing a piece of text that could be used for a real audience and a real purpose. Reading a novel from a different culture, focusing on character, the language the writer uses and the key themes.</li>
-                            <li><h5>Unit 3</h5>
-                            Two controlled assessment tasks. Investigating spoken language and the spoken word, analysing how language is adapted for different audiences and purposes. Writing a piece of text that is suitable for the spoken voice and is intended to be spoken, rather than read.</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Using good reader strategies to support the independent understanding of texts.</li>
-                            <li>Using PEE to discuss and analyse texts, with a particular focus on language, alternative ideas and evaluation.</li>
-                            <li>Refining planning, drafting and editing skills in order to be able to create interesting and believable texts in tight time limits.</li>
-                            <li>Using a variety of writing tools, such as language, punctuation and sentence structure for deliberate effect on the reader.</li>
-                            <li>Being able to lead and shape discussions, present clear and detailed ideas, and take on believable and realistic roles in performances.</li>
-                        </ul>
-                        <small>For more information please visit the <a href="http://www.edexcel.com/" target="_blank">EdExcel website</a></small>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel4d" role="tab" id="panel4d-heading" aria-controls="panel4d">Keystage 4 - GCSE - Literature</a>
-                    <div id="panel4d" class="content" role="tabpanel" aria-labelledby="panel4d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li><h5>Unit 1</h5>
-                            Exam unit: 1 exam. Studying a text written before 1914, focusing on the writer’s use of character, language, key themes and key events. Studying a novel from a different culture, focusing on how the novel reflects the context that it is written in.
-                            </li>
-                            <li><h5>Unit 2</h5>
-                            Exam unit: 1 exam. Studying an anthology of poetry linked by a common theme, before comparing 2 of the poems and how the poets explore similar ideas in them. Studying poetic devices and the poet’s use of language, in order to discuss a poem which has not been previously studied.</li>
-                            <li><h5>Unit 3</h5>
-                            Two controlled assessment tasks. Comparing a character, theme or relationship in one of Shakespeare’s plays with a modern version of the play. Studying a modern play and analysing the character, theme or staging used to present key ideas.</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Using the PEE structure flexibly and with confidence to discuss a text and the writer’s intended effect.</li>
-                            <li>Being able to select relevant, interesting and significant examples from the text to discuss.</li>
-                            <li>Expertly using a range of reading strategies to study and enjoy a wide range of texts, from a variety of writers.</li>
-                        </ul>
-                        <small>For more information please visit the <a href="http://www.edexcel.com/" target="_blank">EdExcel website</a></small>
-                    </div>
-                </li>
+            
+            <ul>
+                <li><a href="//www.aqa.org.uk/subjects/english/gcse/english-language-8700">GCSE English Language</a></li>
+                <li><a href="//www.aqa.org.uk/subjects/english/gcse/english-literature-8702">GCSE English Literature</a></li>
             </ul>
+            
             <br>
         </div>
 
