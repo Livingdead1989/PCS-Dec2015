6de131d63c4afac73cad319da0a7709ef49e3d91
LivingDead1989 <livingdead1989@LivingDead1989s-Mac-mini.local>
2018-12-19 15:43:01 +0000

    copy changes

--- statutory.php
+++ statutory.php
@@ -51,7 +51,7 @@
         <div class="hide-for-small medium-pull-0 medium-offset-0 medium-4 columns"><img src="images/statutory/statutory-01.jpg" alt="Park Community School - Our Staff are committed to providing the very best for our students"></div>
 
         <div class="columns">
-          <p>If, as a parent of a student attending this school, you require a paper copy of the information on this website, we shall provide this free of charge. Please enquire at <a href="/contact.php">the school reception</a>.</p>
+          <p>If as a parent of a student attending this school you require a paper copy of the information on this website, we shall provide this following a request in writing and for a charge. For clarification please enquire at <a href="/contact.php">the school reception</a>.</p>
         </div>
     </div>
 
