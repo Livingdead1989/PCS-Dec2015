70cd8c3d09077f7b850aa6b0fcf72c7fed190670
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-02-13 11:22:05 +0000

    Science letter and removed old archive letters

--- letters-home.php
+++ letters-home.php
@@ -30,7 +30,7 @@
 				  <ul class="circle">February 2019<hr>
 				  	<li><a href="/assets/letters/2019-02/student_review_day.pdf" target="_blank">Student Review Day (SRD)</a></li>
 				  	<li><a href="/assets/letters/2019-02/punctuality_and_uniform.pdf" target="_blank">Punctuality and Uniform</a></li>
-				  	
+				  	<li><a href="/assets/letters/2019-02/science_practicals.pdf" target="_blank">Science Required Practicals</a></li>
 				  </ul>
 				</li>
             	<li>           	
@@ -67,72 +67,6 @@
             	<li>           	
 				  <ul class="circle">March 2018<hr>
 				  	<li><a href="/assets/letters/2018-03/yr11_parentevening.pdf" target="_blank">Year 11 Parent's Evening</a></li>
-				  </ul>
-				</li>
-
-            	<li>           	
-				  <ul class="circle">December 2017<hr>
-				  	<li><a href="/assets/letters/2017-12/holiday_requests_term_time.pdf" target="_blank">Holiday Requests in Term Time</a></li>
-				  	<li><a href="/assets/letters/2017-12/ka2_reports_keydates_springterm.pdf" target="_blank">KA2 Reports and Key Dates for Spring Term</a></li>
-				  	<li><a href="/assets/letters/2017-12/yr10_workexperience_summer2018.pdf" target="_blank">Year 10 Work Experience - Summer 2018</a></li>
-				  </ul>
-				</li>
-
-				<li>           	
-				  <ul class="circle">October 2017<hr>
-				  	<li><a href="/assets/letters/2017-10/y7y11parentevening_9november.pdf" target="_blank">Year 7 &amp; Year 11 Parent Evening</a></li>
-				  	<li><a href="/assets/letters/2017-10/y10attendance_punctuality.pdf" target="_blank">Year 10 Attendance &amp; Punctuality</a></li>
-				  	<li><a href="/assets/letters/2017-10/sitesecurity_lockdowndrills.pdf" target="_blank">Site Security &amp; Lockdown Drills</a></li>
-				  	<li><a href="/assets/letters/2017-10/socialmedia_mobilephones_bullying.pdf" target="_blank">Social Media, Mobile Phones &amp; Bullying</a>
-						<ul class="">
-							<li><a href="/assets/letters/2017-10/jointstatement_mobilephones.pdf" target="_blank">Joint Statement - Mobile Phones</a></li>
-				  			<li><a href="/assets/letters/2017-10/jointstatement_relationalconflict_bullying.pdf" target="_blank">Joint Statement - Relational Conflict &amp; Bullying</a></li>
-						</ul>
-				  	</li>
-				  	
-				  </ul>
-				</li>
-
-				<li>           	
-				  <ul class="circle">September 2017<hr>
-				  	<li><a href="/assets/letters/2017-09/yr11_parents_evening.pdf" target="_blank">Year 11 Parents Evening</a></li>
-				  </ul>
-				</li>
-
-				<li>           	
-				      <ul class="circle">July 2017<hr>
-				      	<li><a href="/assets/letters/2017-07/crazy_hair_week-end_start_of_term-blazers.pdf" target="_blank">Crazy Hair Week, End &amp; Start of Term and Blazers</a></li>
-				      </ul>
-				</li>
-
-				<li>           	
-				  <ul class="circle">June 2017<hr>
-				  	<li><a href="/assets/letters/2017-06/key_events_to_end_of_academic_year.pdf" target="_blank">Key Events to end of Academic Year</a></li>
-				    <li><a href="/assets/letters/2017-06/discount_week_information.pdf" target="_blank">Skoolkit Discount Week Information</a></li>
-				    <li><a href="/assets/letters/2017-06/yr11-leavers-2017.pdf" target="_blank">Year 11 Leavers 2017</a></li>
-				  </ul>
-				</li>
-
-				<li>
-				  <ul class="circle">May 2017<hr>
-				      <li><a href="/assets/letters/2017-05/yr11_exams.pdf" target="_blank">Year 11 Exams</a></li>
-				      <li><a href="/assets/letters/2017-05/blazer.pdf" target="_blank">Blazers</a></li>
-				  </ul>
-				</li>
-
-				<li>
-				  <ul class="circle">April 2017<hr>
-				      <li><a href="/assets/letters/2017-04/raceatourplace-y11y10exams-uniform-absencenotification.pdf" target="_blank">Race at Our Place, Yr11 Yr10 exams, uniform and absence notification</a></li>
-				      <li><a href="/assets/letters/2017-04/ka4reports-insetday10july2017.pdf" target="_blank">KA4 Reports and Inset Day 10th July 2017</a></li>
-				  </ul>
-				</li>
-
-				<li>
-				  <ul class="circle">February 2017<hr>
-				      <li><a href="/assets/letters/2017-02/esafety.pdf" target="_blank">ESafety</a></li>
-				      <li><a href="/assets/letters/2017-02/statement-from-the-governing-bodies-and-headteachers-of-havant-federation-of-secondary-schools.pdf" target="_blank">Statement from the Governing Bodies and Headteachers of Havant Federation of Secondary Schools</a></li>
-				      <li><a href="/assets/letters/2017-02/ofsted-school-meal-price-increase.pdf" target="_blank">Ofsted, School Meal Price Increase</a></li>
-				      <li><a href="/assets/letters/2017-02/y11-mocks-provisional-exam-timetable-and-the-prom.pdf" target="_blank">Y11 mocks, provisional exam timetable and the Prom</a></li>
 				  </ul>
 				</li>
 
