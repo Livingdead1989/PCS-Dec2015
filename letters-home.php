b9aee33e0fedf73eec6609c5cc220c17e748f390
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-03-06 12:07:33 +0000

    New letter added yr10 revision evening

--- letters-home.php
+++ letters-home.php
@@ -27,6 +27,11 @@
         	<p>We feel that everyone should be kept informed. Below you will find all the letters we send home so you get the information needed, regardless of the animals eating it or it getting lost!</p>
             <ul class="small-block-grid-1">
             	<li>           	
+				  <ul class="circle">March 2019<hr>
+				  	<li><a href="/assets/letters/2019-03/yr10_info_revision_evening.pdf" target="_blank">Year 10 Information and Revision Evening</a></li>
+				  </ul>
+				</li>
+            	<li>           	
 				  <ul class="circle">February 2019<hr>
 				  	<li><a href="/assets/letters/2019-02/year_8_options_evening.pdf" target="_blank">Year 8 Options Evening</a></li>
 				  	<li><a href="/assets/letters/2019-02/student_review_day.pdf" target="_blank">Student Review Day (SRD)</a></li>
