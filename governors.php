49b7fa37ce992ab570b65db2fbb5fff5b7c74c43
Phil Warren <P.Warren@pcs.hants.sch.uk>
2019-03-19 12:14:38 +0000

    Governor B Harwood Add

--- governors.php
+++ governors.php
@@ -247,6 +247,28 @@
 			           </div>
 			</div>
 
+			<div class="medium-4 columns text-center  team-member-container end">
+                <a href="#" data-reveal-id="Governor-ejohnson">
+                  <img src="images/governors/person.jpg" alt="Governor Profile Image" class="team-member">
+                <p>Mr B Harwood</p>
+                Governor Details</a>
+                        <div id="Governor-bharwood" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+                          <h2 id="modalTitle">Mr B Harwood</h2>
+                          <p class="lead">Appointed: 11.05.2019 <br>Term Expires: 10.05.2023</p>
+                          <p>Category: Local Authority Govenor</p>
+                          <p>Committee Membership: </p>
+                            <ul class="circle">
+                                <li>Member of Performance Committee.</li>
+                            </ul>
+                          <p>Pecuniary Interests:</p>
+                            <ul class="circle">
+                              <li>TBA</li>
+                            </ul>
+
+                        <a class="close-reveal-modal" aria-label="Close">&#215;</a>    
+			           </div>
+			</div>
+
             <div class="medium-4 columns text-center  team-member-container end">
                 <a>
                   <img src="images/governors/person.jpg" alt="Governor Profile Image" class="team-member">
