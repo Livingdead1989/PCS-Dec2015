dea7c35ea180196dd7fe57cf3f4dc80272f38011
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-05-24 10:03:46 +0100

    Alterations to video iframe position

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                             </a>
 
                             <!-- VIMEO PROMO VIDEO -->
-                            <div style="position:relative;">
+                            <div style="padding:32% 0 0 0;position:relative;">
                             	<iframe src="https://player.vimeo.com/video/299177189?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                             </div>
 
