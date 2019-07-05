5946fa2714c5b8c3f8ef4873faaccc43d54b9d7f
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-07-05 15:31:34 +0100

    New subject Business Studies

---
+++ business-studies.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+        
+        <title>Park Community School | Business Studies</title>
+        <?php include("includes/head.html") ?>
+    </head>
+    <body class="page-learning">
+        <!-- NAVIGATION -->
+        <?php include("includes/topnav.html") ?>
+        <!-- HEADER SECTION -->
+        <header class="header-learning">
+            <div class="row">
+                <div class="medium-8 large-8 columns">
+                    <h1>Business Studies</h1>
+                    <p class="text-capitalize"></p>
+                </div>
+            </div>
+        </header>
+        <!-- Breadcrumb -->
+        <div class="row">
+            <ul class="breadcrumbs" role="menubar" aria-label="breadcrumbs">
+                <li role="menuitem"><a href="/curriculum.php">Curriculum</a></li>
+                <li role="menuitem" class="current"><a href="#">Business Studies</a></li>
+            </ul>
+        </div>
+        <!-- CONTENT -->
+        <section>
+            <div class="row">
+                <div class="large-12 columns">
+                    	<p>Whether you are planning to enter fashion or finance, or something in-between, here are six key reasons why you should study business and how it can open a world of possibilities for you.</p>
+						<ul>
+							<li>Develop communication skills – master the art of making a point</li>
+							<li>Marketing techniques – know your audience</li>
+							<li>Accounting and budgeting – don’t fall short</li>
+							<li>Entrepreneurship – be the next big thing in business</li>
+							<li>Investment and Finance – money makes the world go around</li>
+							<li>Teamwork – a soft skill desired by most employers</li>
+						</ul>
+
+						<p>This course draws upon the enterprise studies that all our students experience when they join Park, through their visits to PDP.</p>
+
+						<p>The aims and objectives of this qualification are to enable students to:</p>
+							<ul>
+								<li>know and understand business concepts, business terminology, business objectives, the integrated nature of business activity and the impact of business on individuals and wider society</li>
+								<li>apply knowledge and understanding to contemporary business issues and to different types and sizes of businesses in local, national and global contexts</li>
+								<li>develop as enterprising individuals with the ability to think commercially and creatively to demonstrate business acumen, and draw on evidence to make informed business decisions and solve business problems</li>
+								<li>develop as effective and independent students, and as critical and reflective thinkers with enquiring minds</li>
+								<li>use an enquiring, critical approach to make informed judgements</li>
+								<li>investigate and analyse real business opportunities and issues to construct well-argued, well-evidenced, balanced and structured arguments, demonstrating their depth and breadth of understanding of business</li>
+								<li>develop and apply quantitative skills relevant to business, including using and interpreting data</li>
+							</ul>
+
+
+						<p>Through studying this qualification students will:</p>
+							<ul>
+								<li>apply knowledge and understanding to different business contexts. These include businesses ranging from small enterprises to large multinationals and businesses operating in local, national and global contexts</li>
+								<li>develop an understanding of how these contexts impact on business behaviour</li>
+								<li>use business terminology to identify and explain business activity</li>
+								<li>apply business concepts to familiar and unfamiliar contexts</li>
+							</ul>
+
+
+						<h3>Progression</h3>
+						<p>A qualification in business will:</p>
+							<ul>
+								<li>enable students to understand more about the business world</li>
+								<li>motivate and challenge students, and prepare them to make informed decisions about further study and career pathways</li>
+							</ul>
+
+						<p>Students can progress from this qualification to a number of different academic and vocational qualifications at Level 3, including GCEs in Business, History, Geography, Economics and Psychology and BTEC Nationals in Business.</p>
+
+						<p>The knowledge and skills gained from GCSE Business support students’ entry into employment or other training in specific aspects of business, such as apprenticeships and vocational qualifications which focus on more specialised business areas.</p>
+						<p>GCSE Business provides a strong foundation for employment, with students progressing, with further training, to a wide range of careers training such as banking, sales, product management and general management.</p>
+
+						<br>
+						<div class="text-center">
+							<a role="button" target="_blank" href="assets/curriculum/business-studies/roadmap.pdf" class="button">Road Map</a>
+						</div>
+						<br>
+
+						<br><br>
+                </div>
+            </div>
+
+            <?php include("includes/backtotop.html") ?>
+        </section>
+        
+        <!-- FOOTER -->
+        <?php include("includes/footer.html") ?>
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
