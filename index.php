89ef10a311b17b525dee7c3b105b1637e84c177a
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-07-11 12:34:39 +0100

    connect4summer - changed linking

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 <section class="page-notification">
   <div class="row">
     <div class="large-12 columns">
-        <p><a href="/events.php" style="color: #474747">Learn about Connect4Summer...</a></p> 
+        <p><a href="/connect4summer.php" style="color: #474747">Learn about Connect4Summer...</a></p> 
     </div>
   </div>
 </section>
