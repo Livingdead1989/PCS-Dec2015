c861a61f1d2aa5df33fd7f985518100e90c68e7b
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-07-11 12:26:44 +0100

    updated layout, added breaks and anchor links for mailto

--- connect4summer.php
+++ connect4summer.php
@@ -23,6 +23,7 @@
 <section>
 	<div class="row">
 		<div class="columns">
+			<br>
 			<h2>What is Connect4Summer?</h2>
 			<p>Connect4Summer is a programme for children in 23 venues across Hampshire which provides an exciting programme that will include a range of sports activities, healthy cooking classes for children and their parents or carers, as well as additional support for our most vulnerable families who are in need.</p>
 			<p>This scheme has been made possible by a joint funding bid by Park Community School and Romsey Community School.</p>
@@ -32,7 +33,7 @@
 			<p>29th July - 23rd August 2019 in The Dickinson Centre at Park Community School.</p>
 
 			<p><strong>Booking Childcare</strong></p>
-			<p>Contact kerryainsley@sky.com to book places, each child can have 10 places which are subject to availablity.</p>
+			<p>Contact <a href="mailto:kerryainsley@sky.com">kerryainsley@sky.com</a> to book places, each child can have 10 places which are subject to availablity.</p>
 
 			<h2>What Activities are offered?</h2>
 			<p>Our programme is different every day, activities include:</p>
@@ -49,6 +50,7 @@
 				<li>and much more...</li>
 			</ul>
 			<p>Booking required, please come into school for a booking form.</p>
+			<br><br>
 		</div>
 	</div>
 </section>
@@ -61,6 +63,7 @@
 		<div class="columns">
 			<h2>Summer MUNCH at Park</h2>
 			<p>Our holiday <a href="/munch.php">MUNCH</a> meals continue to provide a free 2 course meal for children attending any school in Leigh Park, parents and carers are also welcome, whose curent circumstances might mean that receiving a free meal is necessary. - <strong>Excludes Bank Holidays</strong></p>
+			<br><br>
 		</div>
 	</div>
 </section>
@@ -76,20 +79,21 @@
 			<p>On 8 May 2019, Children and Families Minister, Nadhim Zahawi, announced that 11 coordinators will receive a share of £9.1m funding to provide 50,000 disadvantaged children with free meals and activities over the upcoming summer holidays.</p>
 			<p>The Havant and Romsey schools are being given a grant to co-ordinate a programme called ‘Connect4Summer’ for children in 23 venues across Hampshire, including Portsmouth and Southampton.</p>
 			<p>Connect4Summer will be providing an exciting programme that will include a range of sports activities, healthy cooking classes for children and their parents or carers, as well as additional support for our most vulnerable families who are in need.</p>
-			<p>Headteacher of Park Community School, Christopher Anders, said: “We are very excited and delighted to be involved in this large project supporting families with holiday child care and family fun days.”</p>
-			<p>Headteacher Jonathan de Sausmarez of Romsey Community School said “I am really pleased that the need for vulnerable families to have support and care over the summer holidays has been recognised. We want to make a real difference and we are very proud to support this much needed project.”</p>
-			<p>Both Community Managers of each school, Susan Parish (Park Community School) and Jan Lefley (Romsey Community School) reported that they wanted to provide a programme that has a future legacy, that engages families and provides them with opportunities after the summer to continue with a variety of activities. Susan said “It is very important to us that we continue to address holiday hunger and this programme enables us to reach large numbers of children and their families. We will also be able to offer children good quality, exciting activities and new opportunities whilst ensuring they are safe throughout the holidays.”</p>
+			<p>Headteacher of Park Community School, Christopher Anders, said: <em>“We are very excited and delighted to be involved in this large project supporting families with holiday child care and family fun days.”</em></p>
+			<p>Headteacher Jonathan de Sausmarez of Romsey Community School said <em>“I am really pleased that the need for vulnerable families to have support and care over the summer holidays has been recognised. We want to make a real difference and we are very proud to support this much needed project.”</em></p>
+			<p>Both Community Managers of each school, Susan Parish (Park Community School) and Jan Lefley (Romsey Community School) reported that they wanted to provide a programme that has a future legacy, that engages families and provides them with opportunities after the summer to continue with a variety of activities. Susan said <em>“It is very important to us that we continue to address holiday hunger and this programme enables us to reach large numbers of children and their families. We will also be able to offer children good quality, exciting activities and new opportunities whilst ensuring they are safe throughout the holidays.”</em></p>
 			<br>
 			<p>For further details please contact:</p>
 			<ul>Susan Parish, Park Community School
-				<li>Email: S.Parish@pcs.hants.sch.uk</li>
+				<li>Email: <a href="mailto:S.Parish@pcs.hants.sch.uk">S.Parish@pcs.hants.sch.uk</a></li>
 				<li>Tel: 02392489800</li>
 			</ul>
 
 			<ul>Jan Lefley, Romsey Community School
-				<li>Email: JLefley@romsey.hants.sch.uk</li>
+				<li>Email: <a href="mailto:JLefley@romsey.hants.sch.uk">JLefley@romsey.hants.sch.uk</a></li>
 				<li>Tel: 01794522106</li>
 			</ul>
+			<br><br>
 		</div>
 	</div>
 </section>
