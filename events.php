efb50872fc9fe92518e3b54105c470ad16bdfedc
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-07-18 11:43:21 +0100

    Hermitage event removed feature and sticky tags

--- events.php
+++ events.php
@@ -53,7 +53,7 @@
 <!--                  Event box --> 
 
 <!--                    Event Box  -->
-              <div class="event-wrap feature-event sticky-event">
+              <div class="event-wrap">
                 <div class="date-wrap">
                   <p class="event-date">27th July</p>
                   <p class="event-time">11:00-15:00</p>
