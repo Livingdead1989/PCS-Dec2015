285a656c08b9ea9658c98dd0f98b27d8f968d8a3
Phil Warren <P.Warren@pcs.hants.sch.uk>
2019-11-15 08:13:05 +0000

    Senior Citizens Event

--- events.php
+++ events.php
@@ -123,8 +123,27 @@
                   <a href="/assets/events/christmas_light.pdf" target="_blank" class="button success radius">View our flyer...</a>
                 </div>
                 <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div
+                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
                   <div class="cost-wrap">Free</div>
+                </div>
+              </div>
+<!--                  Event box --> 
+
+<!--                    Event Box  -->
+              <div class="event-wrap">
+                <div class="date-wrap">
+                  <p class="event-date">8th December</p>
+                  <p class="event-time">14:00 - 16:00</p>
+                </div>
+                <h2 class="event-title">Senior Citizens Christmas Party</h2>
+                <p class="event-subheader"></p>
+                <p class="event-description">Please join us for High Tea and a festive sing along.</p>
+                <div class="link-wrap">
+                  <a href="/assets/events/senior_citizen_tea.pdf" target="_blank" class="button success radius">View our flyer...</a>
+                </div>
+                <div class="bottom-wrap">
+                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
+                  <div class="cost-wrap">£2.50</div>
                 </div>
               </div>
 <!--                  Event box --> 
