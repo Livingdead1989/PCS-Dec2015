e220b0760862f6467bca3e79a2f5beef76d0bdec
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-11-15 09:59:37 +0000

    new converted to inserts and removed old events

--- news.php
+++ news.php
@@ -27,6 +27,7 @@
 </div>
 
 <!-- CONTENT -->
+<!-- PARK POST -->
 <section>
     <div class="row">
         <br>
@@ -64,227 +65,52 @@
         </div>
     </div><br>
 </section>
+<!-- /PARK POST -->
 
 <section class="grey-bg">
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
     </div>
-    <div class="row">
-
-        <div id="primary-link" class="medium-12 columns">
-            <h3>Primary Link (Completed)</h3>
-            <p>Primary Link magazine is all about the schools in our local area.</p>
-            <p>Primary Link is available in downloadable PDF format please visit our <a href="/park-archive.php">Archive</a> page.</p>
-        </div>
-    </div>
-    <br><br>
-</section>
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-        <div class="medium-6 columns">
-            <h4>BBC News Day</h4>
-            <p>BBC News School Report gives 11-16 year-old students in the UK the chance to make their own news reports for a real audience.</p>
-            <p>View our submissions and follow along with the day.</p>
-            <div class="text-center">
-              <a class="button success" href="/bbcnewsday.php">Submissions</a>
-              <br><br>
-              <img src="images/news/bbcnewsday.jpg" style="height: 200px; width: 200px;" alt="BBC School News Day">
-              <br><br>
-            </div>
-        </div>
-        <div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-6 columns">
-            <a class="twitter-timeline" data-height="600" data-dnt="true" href="https://twitter.com/BBCSchoolReport">Tweets by BBCSchoolReport</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-    </div>
-    <br><br>
-</section>
-
-<section class="grey-bg">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row" id="BloodHoundEvent2017">
-        <div class="medium-12 columns">
-            <h4>Race for the Line - BLOODHOUND Project</h4>
-            <p>The BLOODHOUND project is aimed to inspire students in science with a hands on experience making and using rocket cars with the aid of a <a href="//microbit.org/" target="_blank">BBCMicroBit</a> to record data. for more information about the BLOODHOUND project visit their website <a href="//www.bloodhoundssc.com/" target="_blank">BLOODHOUNDSSC</a>.</p>
-            <div class="flex-video widescreen border-radius-5">
-              <iframe src="https://www.youtube.com/embed/p8kYfo2e9Js" frameborder="0" allowfullscreen></iframe>
-            </div>
-
-            <ul class="medium-block-grid-4 small-block-grid-2 text-center">
-              <p>Follow the BLOODHOUND Project</p>
-                <li><i class="fa fa-facebook-official" aria-hidden="true"></i> <a href="//www.facebook.com/BLOODHOUNDSSC" target="_blank">Facebook</a></li>
-                <li><i class="fa fa-twitter-square" aria-hidden="true"></i> <a href="//twitter.com/BLOODHOUND_SSC" target="_blank">Twitter</a></li>
-                <li><i class="fa fa-instagram" aria-hidden="true"></i> <a href="//www.youtube.com/user/1050mph" target="_blank">YouTube</a></li>
-                <li><i class="fa fa-flickr" aria-hidden="true"></i> <a href="//www.flickr.com/photos/stefanmarjoram/sets/72157627819481179/" target="_blank">Flickr</a></li>
-            </ul>
-        </div>
-    </div>
-    <br><br><br>
-</section>
-
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row" id="laca">
-        <div class="medium-8 columns">
-            <h4>LACA Award</h4>
-            <p><strong>Park Community School named as LACA Secondary School Catering Team of the Year.</strong></p>
-            <p>Thursday 12th July 2018 Park Community School was named Secondary School Catering Team of the Year at the Lead Association for Catering in Education Awards (LACA) held at the Hilton Birmingham Metropole.</p>
-            <p>Every year the LACA Awards for Excellence acknowledge individuals and teams of people who continue to make a real difference in education catering. Good management, strong performance and high achievement is recognised.</p>
-            <p>Park Community School are proud to be ‘Much More Than Just a School’ and their catering team encompass this through providing affordable, quality food throughout the year including Christmas Day. Catering from nursery through to retirement, their passion shines through in providing free meals for their community through the MUNCH programme in the school holidays.</p>
-        </div>
-        <div class="medium-4 columns">
-            <img src="images/news/laca/catering_team.jpg" alt="PCS Catering Team">
-            <p class="text-center"><small>PCS Catering Department</small></p>
-        </div>
-        
-        <div class="medium-8 columns">
-            <br>
-            <p>The school are very proud of the achievements of the catering team which also include:</p>
-            <ul class="circle">
-                <li>Soil Association Food for Life – <strong>Gold Award</strong> <em>(one of three secondary schools in England to achieve this)</em>.</li>
-                <li>Eat Out, Eat Well – <strong>Gold Award</strong> <em>(Hampshire County Council)</em>.</li>
-                <li>Fair Trade Awards – Accredited Active and Aware school</li>
-                <li>Reached the finals of Essential Cuisines - <strong>Second in Britain’s Best Brigade 2017</strong>.</li>
-                <li>Ecucatering Excellence Awards 2016 - <strong>Winner Newcomer of the Year Award Steven Cross</strong>.</li>
-            </ul>
-            
-            <br><br>
-            <p><strong>Headteacher, Christopher Anders said</strong></p>
-            <blockquote>“From the moment we decided to employ our own catering team the school has never looked back. We provide affordable, good quality nutritious food for our students, staff, visitors and members of our community. We are delighted to receive this award that recognises the hard work of our catering team over the last four years”.</blockquote>
-        </div>
-        <div class="medium-4 columns">
-            <img src="images/news/laca/head_headchef.jpg" alt="PCS Headteacher and Head of Catering">
-            <p class="text-center"><small>Headteacher and Head of Catering</small></p>
-        </div>
-    </div>
+    <!-- persimmon building futures award - 8/10/2019 -->
+    <?php include("includes/news/persimmon-building-futures-award.html") ?>
+    <!-- /persimmon building futures award - 8/10/2019 -->
     <br><br>
     <?php include("includes/backtotop.html") ?>
 </section>
 
-
-<section class="grey-bg">
+<section>
     <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
-    <div class="row" id="communityimpact">
-        <div class="medium-8 columns">
-            <h4>HCC Community Impact Award</h4>
-            <p>We are delighted to announce that Park Community School were last night awarded Hampshire County Council’s ‘Community Impact Award’.</p> 
-            <p>We have been successful in winning this award in respect of <a href="/munch.php">our MUNCH project</a>, which aims to reduce food poverty for the benefit of local children, young people and families in Leigh Park.</p>
-            <p>We started MUNCH, which runs every day through the school holidays, Thursday tea times and the last Sunday of the month to ensure local families always have something to eat.</p>
-        </div>
-        <div class="medium-4 columns">
-            <img src="images/news/communityimpact/community_impact_award_2.jpg" alt="Community and Site Manager Collecting the Award">
-            <p class="text-center"><small>Community &amp; Site Managers Collecting the Award.</small></p>
-        </div>
-    </div>
-    <br><br><br>
+    <!-- social good award - 8/10/2019 -->
+    <?php include("includes/news/social-good-award.html") ?>
+    <!-- /social good award - 8/10/2019 -->
+    <br><br>
+    <?php include("includes/backtotop.html") ?>
 </section>
-
 
 <section>
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
-    <div class="row" id="remembrance">
-        <div class="columns">
-            <h4>Remembrance Commemorations</h4>
-            <p>As part of our Remembrance commemorations this year, with the help of our local community we have created a knitted poppy wave to honour those who have lost their lives through war.  Our display is available to view from 4:00pm daily until the 16th November.</p>
-        </div>
-
-        <div class="row">
-            <div class="columns small-8 small-push-2">
-                <div class="homepage-slider">
-                    <div><img src="images/news/remembrance/remembrance1.jpg" alt="Rememberance Commemorations at Park"></div>
-                    <div><img src="images/news/remembrance/remembrance2.jpg" alt="Rememberance Commemorations at Park"></div>
-                    <div><img src="images/news/remembrance/remembrance3.jpg" alt="Rememberance Commemorations at Park"></div>
-                    <div><img src="images/news/remembrance/remembrance4.jpg" alt="Rememberance Commemorations at Park"></div>
-                    <div><img src="images/news/remembrance/remembrance5.jpg" alt="Rememberance Commemorations at Park"></div>
-                </div>
-            </div>
-        </div>
-
-    </div>
+    <!-- children and young people award - 21/11/2018 -->
+    <?php include("includes/news/children-and-young-people-award.html") ?>
+    <!-- /children and young people award - 21/11/2018 -->
     <br><br>
     <?php include("includes/backtotop.html") ?>
 </section>
-
-
 
 <section class="grey-bg">
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
     </div>
-    <div class="row" id="children_and_young_people_award">
-        <div class="medium-8 columns">
-            <h4>Children and Young People Now Family Support Award 2018</h4>
-            <p>We are proud to announce that on 21 November 2018, Park Community School was named as the 2018 winner of the Family Support Award at the Children and Young People Now Awards, for our MUNCH community food project.</p>
-            <p>The CYPNOW awards are firmly established as the ‘gold standard’ in services for children, young people and families across the UK.  The award provides recognition and pride for teams and individuals who strive to improve young lives day in, day out and whose hard work and perseverance otherwise goes unnoticed and unsung.</p>
-            <p>MUNCH addresses the problem of holiday hunger and offers free meals to children who normally receive free school meals during term time, providing a safe environment for children to come and have a meal during the school holidays.  Everyone is welcome from all local primary and secondary schools.  MUNCH has now expanded to include Thursday tea times and Sunday lunches.</p> 
-            <p>In the judge’s opinion we have done the most in our category, to support whole families to help their children to lead healthy, happy and fulfilling lives safe from harm.</p>
-        </div>
-        <div class="medium-4 columns">
-            <img src="images/news/children_and_young_people_award/children_and_young_people_award.jpg" alt="Collecting the Children and Young People Award">
-            <p class="text-center"><small>Business &amp; Facilities Managers Collecting the Award.</small></p>
-        </div>
-    </div>
-    <br><br><br>
-</section>
-
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-
-        <div class="columns">
-            <h4>Park Wins Social Good Award</h4>
-                <p>We’re delighted to announce our success in winning this year’s Social Good Award awarded by EDUcaterer at the Royal Garden Hotel in London on Tuesday 8 October 2019. Park Community School has been recognised for MUNCH, which ensures that no child, no matter what school or age need go hungry in the school holidays.</p>
-                <p>Winning an EDUcatering Excellence Award is a great achievement and one that is highly recognised across catering and education.</p>
-                <p>For more information about, or to donate to MUNCH please email MUNCH@pcs.hants.sch.uk</p>
-                <img src="images/news/catering/educatering.png" alt="Headteacher and Head Chef accepting award"> 
-        </div>
-
-    </div>
+    <!-- hcc community impact award -->
+    <?php include("includes/news/hcc-community-impact-award.html") ?>
+    <!-- /hcc community impact award -->
     <br><br>
     <?php include("includes/backtotop.html") ?>
 </section>
 
-
-
-<section class="grey-bg">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row" id="persimmon_building_futures_award">
-    	<h4>Persimmon Building Futures Awards</h4>
-    		<p>On Tuesday 8 October, Susan Parish, Business and Community Manager attended the Persimmon Building Futures Awards in York. Building Futures was launched to share £1 million to causes supporting under 18s across England. We were shortlisted for a prize towards funding for an activity track at Park Community School. As one of the finalists we were awarded a £5,000 donation on the night. Susan said:</p>
-    		<p>“It was a fantastic night and we were made to feel so special and were surrounded by charities who carry out amazing things. Our thanks to Persimmon for providing these donations which are going to make such a huge difference to so many. Pleased to say, and so well deserved, locally Portsmouth Downs Syndrome Association were the big winners scooping the £100k first prize on the night”.</p>
-			<img src="/images/news/persimmon_building_futures_award/persimmon_building_futures_award.jpg" alt="Accepting the award for Persimmon Building Furtures">       
-    </div>
-    <br><br><br>
-</section>
-
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-
-    </div>
-    <br><br>
-    <?php include("includes/backtotop.html") ?>
-</section>
 
 <!-- FOOTER -->
 <?php include("includes/footer.html") ?>
