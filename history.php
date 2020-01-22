f59c2b80b8bb5def8578153cf628f8e1ecd3a9a6
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2020-01-22 11:22:23 +0000

    History content change

--- history.php
+++ history.php
@@ -32,26 +32,22 @@
         <div class="large-12 columns">
         <br>
 
-        <h3>Purpose</h3>
-        <p>It is our belief that all students should study the past to develop a sense of their own historical identity. By studying the human mind-set and experience throughout the ages, it allows them to make sense of the world around them. It develops critical and enquiring minds that are able to understand different perspectives and challenge the thinking of others.</p>
+        <h3>Curriculum Intention</h3>
+            <p>It is our belief that all students should study the past to develop a sense of their own historical identity. By studying the human mind-set and experience throughout the ages, it allows them to make sense of the world around them. It develops critical and enquiring minds that are able to understand different perspectives and challenge the thinking of others.</p> 
+            <p>Park historians work like detectives, piecing together a range of evidence to help them reconstruct the past. While they enquire, they will question, hypothesise, weigh and evaluate the evidence to help them come to a well-considered conclusion.  They will know that, as historians, there are different judgements that we can draw from the past, even if we are using the same evidence.</p> 
+            <p>Historian mastery will see a pupil with a solid chronological framework of history, so that they are able to hypothesise by drawing on their existing knowledge. It is our aim to make them confident in evaluating and critically reading sources, considering their use in the context of their audience and purpose. They recognise how and why historians form certain interpretations about the past. In addition, they are able to articulate and write their own historical thinking with clarity and precision.</p> 
 
-        <h3>Aims</h3>
-        <p>Park historians work like detectives, piecing together a range of evidence to help them reconstruct the past. While they enquire, they will question, hypothesise, weigh and evaluate the evidence to help them come to a well-considered conclusion.  They will know that, as historians, there are different judgements that we can draw from the past, even if we are using the same evidence.</p>
-        <p>Historian mastery will see a pupil with a solid chronological framework of history, so that they are able to hypothesise by drawing on their existing knowledge. It is our aim to make them confident in evaluating and critically reading sources, considering their use in the context of their audience and purpose. They recognise how and why historians form certain interpretations about the past. In addition, they are able to articulate and write their own historical thinking with clarity and precision.</p>
-
-        <h3>Areas of Study</h3>
-        <p>The KS3 curriculum follows a two-year course set within a chronological framework. Big enquiry questions drive the learning and are underpinned by the second-order historical concepts to allow our students to think and work like historians.</p>
-        <p>Our year 7 curriculum begins with an investigation into students’ own history through a local study of the development of Leigh Park, as well as the opportunity for students to conduct their own research into their own families or local points of interest. We then journey through pre-20th century history, exploring the Medieval, Early Modern and Industrial periods. Some of the enquiry questions we use to explore the history of these periods include ‘Does Mary I deserve her nickname of ‘Bloody Mary’?’ and ‘How should we remember the British Empire?’.</p>
-        <p>Year 8 continues in a chronological approach and largely focuses on the study of the 20th century. We study a range of historical figures and events, including the Suffragette movement and civil rights in the USA. Enquiry questions that drive the learning include challenging questions such as ‘Was it really the assassination of one man that caused the First World War?’ and ‘Was the Holocaust inevitable?’.</p>
-        <p>Our Key Stage 4 curriculum begins in year 9. As well as selecting topics that are interesting and engaging, we believe these histories are important in understanding significant changes which have helped to shape the world around us.</p>
-
-        <p>The following units are studied under the Edexcel GCSE board: </p>
-        <ul>
-            <li>Paper 1: Crime and Punishment, 1000-present day and Whitechapel, 1870-1900</li>
-            <li>Paper 2: Early Elizabethan England, 1558-1588 and Superpower Relations, 1941-1991</li>
-            <li>Paper 3: Weimar and Nazi Germany, 1918-1939</li>
-        </ul>
-
+        <h3>Curriculum Implementation</h3>
+            <p>The KS3 curriculum follows a two-year course set within a chronological framework. Big enquiry questions drive the learning and are underpinned by the second-order historical concepts to allow our students to think and work like historians.</p> 
+            <p>Our year 7 curriculum begins with an investigation into students’ own history through a local study of the development of Leigh Park, as well as the opportunity for students to conduct their own research into their own families or local points of interest. We then journey through pre-20th century history, exploring the Medieval, Early Modern and Industrial periods. Some of the enquiry questions we use to explore the history of these periods include ‘Does Mary I deserve her nickname of ‘Bloody Mary’?’ and ‘How should we remember the British Empire?’</p> 
+            <p>Years 8 and 9 continue in a chronological approach and largely focuses on the study of the 20th century. We study a range of historical figures and events, including the Suffragette movement and civil rights in the USA. Enquiry questions that drive the learning include challenging questions such as ‘Was it really the assassination of one man that caused the First World War?’ and ‘Was the Holocaust inevitable?’</p> 
+            <p>Our Key Stage 4 curriculum begins in year 10. As well as selecting topics that are interesting and engaging, we believe these histories are important in understanding significant changes which have helped to shape the world around us.</p> 
+            <p>The following units are studied under the Edexcel GCSE board: </p>
+            <ul>
+                <li>Paper 1: Crime and Punishment, 1000-present day and Whitechapel, 1870-1900</li>
+                <li>Paper 2: Early Elizabethan England, 1558-1588 and Superpower Relations, 1941-1991</li>
+                <li>Paper 3: Weimar and Nazi Germany, 1918-1939</li>
+            </ul>
         <br>
         </div>
     </div>
