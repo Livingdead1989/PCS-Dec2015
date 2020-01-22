b19d891f9d0f6303753f55c4c3fda0adf9805ad1
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2020-01-22 10:59:15 +0000

    Geo content change

--- geography.php
+++ geography.php
@@ -31,33 +31,127 @@
     <div class="row">
         <div class="large-12 columns">
         <br>
-            <h3>Purpose</h3>
+            <h3>Purpose of Study & Intention</h3>
+
+            <blockquote>Geography is the subject which holds the key to our future <cite>Michael Palin</cite></blockquote>
             
-            <p>Geography is primarily concerned with promoting an understanding of the nature of the Earth’s surface and more particularly the character of places, the complex nature of people’s relationships and interactions with their environment, and the importance in human affairs of the location and spatial organisation of human activities. This understanding includes the development of critical awareness of problems and conflicts in the geographical environment which relates to differing attitudes and situations. In this social context geography can also be a valuable medium for developing an understanding and sensitivity to multicultural communities and societies.</p>
+            <p>Geography is arguably one of the most diverse, interesting and important subjects the national curriculum offers.</p>
+
+            <p>Geography is primarily concerned with promoting an understanding of the nature of the Earth’s surface and more particularly the character of places, the complex nature of people’s relationships and interactions with their environment, and the importance in human affairs of the location and spatial organisation of human activities. This understanding includes the development of critical awareness of problems and conflicts in the geographical environment which relates to differing attitudes and situations. In this social context geography can also be a valuable medium for developing an understanding of the sensitivity to multicultural communities and societies.</p>
 
             <h3>Aims</h3>
-            <p>Through the variety of teaching and learning strategies involved in Geography, pupils will form an understanding of the different physical, human and environmental processes that are an integral part of our world.</p>
+            <p>Through the variety of teaching and learning strategies involved within geography, students will form an understanding of the different physical, human and environmental processes that are an integral part of our world.</p>
 
-            <ul class="circle">
-                <li>Geography seeks to explore the relationship between the Earth and its people through the study of place, space and environment. Students will ask the questions where, what, who, how, and why?</li>
-                <li>The study of 'place' is aimed to allow students to describe and understand not only the location of physical and human features of the Earth, but also the processes, systems and interrelationships that create or influence those features.</li>
-                <li>The study of 'space' teaches students to explore the relationships between places and patterns of activity arising from the use people make of the physical setting where they live and work.</li>
-                <li>The study of the 'environment' embraces both its physical and human dimensions. Thus it addresses the resources, sometimes scarce and fragile that the Earth provides and on which all human life depends; the impact on those resources of human activities; and the wider social, economic, political and cultural consequences of the interrelationship between the two.</li>
-            </ul>
-
-            <p>The overall implications of sustainability both of land, resources, and human actions will promote the development of knowledge, skills, understanding and values in pupils so they make responsible decisions, both individually and collectively, at both local and global scales, about the environment.</p>
+            <p>Geography seeks to explore the relationship between the Earth and its people through the study of PLACE, SPACE and ENVIRONMENT. Students will ask the questions WHERE, WHAT, WHO, HOW and WHY.</p>
+            <p>The study of PLACE is aimed to allow students to describe and understand not only the location of physical and human features of the Earth, but also the processes, systems and interrelationships that create or influence those features.</p>
+            <p>The study of SPACE teaches students to explore the relationships between places and patterns of activity arising from the use people make of the physical setting where they live and work.</p>
+            <p>The study of the ENVIRONMENT embraces both its physical and human dimensions. Thus it addresses the resources, sometimes scarce and fragile that the Earth provides and on which all human life depends; the impact on those resources of human activities; and the wider social, economic, political and cultural consequences of the interrelationship between the two.</p>
             
-            <h3>Areas of Study</h3>
-            <p>The focus of Geography in years 7 and 8 is to study the interdependence of the physical and human world. </p>
-
-            <p>In Year 7 students concentrate on the key skills required for successful study of Geography throughout their school career, starting with map skills. The Earth’s population will be 9 billion by 2050. As the stress of 7 billion people takes its toll on the Earth’s resources, we study how different people are affected by changes in population and the power of the earth itself. How many more people can the Earth take? Extreme weather is an increasing problem in the 21st century and therefore crucial to understand The outcome of this unit is to be able to produce and deliver a detailed weather forecast from synoptic charts and data. The year culminates in a rich challenge to demonstrate knowledge, understanding and key skills.</p>
-
-            <p>In Year 8 students start looking at more in-depth topics to examine the human and physical world. The year begins with an in-depth study of the natural world and the risks that humans have to endure on a daily basis. Tectonics looks at the natural hazards earthquakes, volcanoes, and tsunamis, whilst the study of rivers and coasts allows students to explore the diverse natural environments we have in the UK. The focus is on the impacts of these events with close links to Science. Media plays an important role in this unit which builds in ‘Geography in the News’. Healthy World focuses on the idea of climate change and our findings about global population, making detailed links between units from both Year 7 and 8. How does climate affect the lives of people around us? Are some people in the world affected more than others? How can we protect our futures in a sustainable way?  Understanding the pressure on global resources is paramount therefore students study the Tropical Rainforest and begin looking at the concept of sustainability. In our ever-changing world the alarming inequalities, particularly with women’s rights is evident, and the pupils look into which countries don’t meet the Millennium Development Goals, and make comparisons between more and less economically developed countries.</p>
-
-            <p>GCSE Geography teaching starts in Year 9 following main themes: Tectonic hazards, extreme weather, climate change, ecosystems, tropical rainforests, hot deserts, coastal landscapes, river landscapes, the urban world, urban change in the UK, urban sustainability, the development gap, emerging economies, changing economy of the UK, resource management, energy management, and geographical skills. These topics are all in-depth studies to broaden the geographical understanding of all pupils and prepare them for the world of work. All pupils will have the opportunity to complete field work to put theory into practice.</p>
+            <p>The overall implications of sustainability both of land, resources, human actions etc. will promote the development of knowledge, skills, understanding and values in students so they make responsible decisions, both individually and collectively, at both local and global scales, about the environment. </p>
             
 
-            
+            <h3>Curriculum Implementation</h3>
+
+            <table>
+              <tr>
+                <th></th>
+                <th>Autumn 1</th>
+                <th>Autumn 2</th>
+                <th>Spring 1<br></th>
+                <th>Spring 2</th>
+                <th>Summer 1</th>
+                <th>Summer 2</th>
+              </tr>
+              <tr>
+                <td>YEAR 7</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Geographical Skills</td>
+                <td>Weather and Climate</td>
+                <td>Climate Change</td>
+                <td>Brazil</td>
+                <td>Population and Urbanisation</td>
+                <td>Rivers</td>
+              </tr>
+              <tr>
+                <td>YEAR 8</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Africa</td>
+                <td>Crime</td>
+                <td colspan="2">Natural Hazards</td>
+                <td>Global Fashion</td>
+                <td>Coasts</td>
+              </tr>
+              <tr>
+                <td><br>Year 7 and 8 Assessment Model</td>
+                <td colspan="6">Key Stage 3 will be tested with multiple choice tests every term in addition to extended writing and exam style questions within lessons.</td>
+              </tr>
+              <tr>
+                <td>YEAR 9</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">The challenge of natural hazards</td>
+                <td colspan="3">The living world</td>
+              </tr>
+              <tr>
+                <td>YEAR 10</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="2">Rivers and Coasts</td>
+                <td colspan="2">Urban issues and challenges</td>
+                <td colspan="2">The changing economic world</td>
+              </tr>
+              <tr>
+                <td>YEAR 11</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="2">Resource management</td>
+                <td><br>Paper 1 and 2 completion<br>Unit 3 Preperation<br></td>
+                <td>Unit 3 Preperation and Fieldwork Lee-On-The-Solent</td>
+                <td colspan="2">Revision</td>
+              </tr>
+              <tr>
+                <td>GCSE Assessment Model</td>
+                <td colspan="6">ALL GCSE units to be tested with exam questions and key term tests.</td>
+              </tr>
+            </table>
+
             <br>
         </div>
 
