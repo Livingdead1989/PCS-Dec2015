7bbc0b502316e76103f47db3b1d9a20f7116057f
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-02-05 07:50:12 +0000

    Letter Update

--- letters-home.php
+++ letters-home.php
@@ -27,6 +27,11 @@
         	<p>We feel that everyone should be kept informed. Below you will find all the letters we send home so you get the information needed, regardless of the animals eating it or it getting lost!</p>
             <ul class="small-block-grid-1">
             	<li>           	
+				  <ul class="circle">February 2020<hr>
+				  	<li><a href="/assets/letters/2020-02/srd-gcse-course-updates-key-dates.pdf" target="_blank">SRD, GCSE Course Updates and Key Dates</a></li>
+			  	  </ul>
+				</li>
+            	<li>           	
 				  <ul class="circle">January 2020<hr>
 				  	<li><a href="/assets/letters/2020-01/assemblies-attendance-homework-srd-keydates.pdf" target="_blank">Assemblies, Attendance, Homework, SRD and Key Dates</a></li>
 			  	  </ul>
