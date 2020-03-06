2c0e4d5becde6befda8ea802e7e267f79772b819
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-03-06 13:12:18 +0000

    Media Page update

--- media.php
+++ media.php
@@ -31,55 +31,44 @@
     <div class="row">
         <div class="large-12 columns">
         <br>
-            <h3>Purpose</h3>
-            <p>Media at Park looks at developing pupils' skills in exploring media theory and practice. It gives an opportunity to learn about real media products and industries. Four key concepts are studied at GCSE level. They are Media Language, Institutions, Audience and Representation.</p>
+            <h3>Intent</h3>
+            <p>In Media we equip pupils to understand existing media texts, carefully plan their own media texts, create media texts in a professional manner, then evaluate them effectively. Media is all about working in a professional way, using technical terminology, skills and equipment in order to create fully realised products for specific audiences. A further fundamental in media is cultural capital. We live in a media led society and this course encourages pupils to understand how media works, how it impacts them and how it is constantly evolving. They will consume a wide range of media texts designed for a variety of audiences, opening their eyes to  the media world outside their immediate sphere of knowledge.
+            </p>
 
-            <h3>Aims</h3>
+            <h3>Implementation</h3>
+            <p>Our curriculum begins in year 9 and aims to give pupils an introduction to the world of pre-production skills before they begin working on coursework units. Lessons encourage an ethos of independence and provide pupils with opportunities to work both individually and within groups. Further opportunities to develop media skills are built in to ensure they can make informed choices about which path they take.  Coursework teaching is structured to cover the 4 assessment areas required:
+</p>
             <ul class="circle">
-                <li>To analyse and respond to a range of media texts.</li>
-                <li>To plan and present projects which show flair and creativity.</li>
-                <li>To create pre-production work which uses appropriate forms and conventions.</li>
-                <li>To be able to plan and research independently and effectively.</li>
-            </ul>
-            
-            <h3>Areas of Study</h3>
-            <p>The first area of study looks at the method of print with students comparing two magazine covers. This includes analysis of target audience, evaluation of purpose and designing a magazine front cover using all the conventions of a magazine design. Students will analyse and evaluate the effectiveness of their work.</p>
-
-            <p>The second area focuses on cross media through Advertising and Marketing. Students will study a range of adverts across at least two media platforms, based on the same product. They will then devise a storyboard or a mock up of their own advertising method for the same product and analyse the effectiveness of the method within their work.</p>
-
-            <p>Assignment three focuses on Moving Image and involves planning and creating a trailer for a film or a music video. This involves extensive planning using audience research, storyboarding, scripting and marketing methods. The final product is created and evaluated through the chosen medium.</p>
-
-            <p>Finally students will complete an external exam focusing on a media brief set by the AQA exam board. Students will have planning time for this four weeks before the exam.</p>
-
-            <ul class="accordion" data-accordion role="tablist">
-
-                <li class="accordion-navigation">
-                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Year 7-11 - BTEC</a>
-                    <div id="panel2d" class="content" role="tabpanel" aria-labelledby="panel2d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Digital publishing</li>
-                            <li>Planning &amp; pitching a product</li>
-                            <li>Film</li>
-                            <li>Audiences</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Independence</li>
-                            <li>Technical skills
-                                <ul>
-                                    <li>Cameras</li>
-                                    <li>Filming</li>
-                                </ul>
-                            </li>
-                            <li>Groupwork</li>
-                        </ul>
-                        <small>For more information please visit the <a href="http://www.edexcel.com/" target="_blank">EdExcel website</a></small>
-
-                    </div>
+                <li>LO1 - Pupils will learn how to understand the purpose and content/uses/properties of existing media texts. They will research existing examples and deconstruct to ensure they show clear understanding of how, why and for whom they have been made.
+                </li>
+                <li>LO2 - Pupils will learn how to plan their own media texts. They will begin by deconstructing and understanding a product brief. They will then learn about a wide range of potential planning documents, before showing their understanding by choosing and using the correct documents for the type of text they are going to create.
+                </li>
+                <li>LO3 - Pupils will learn how to create a media text. They will adopt professional working practices where possible and use technical equipment and software to create a product for a specific audience which responds clearly to a brief.
+                </li>
+                <li>LO4 - Pupils will learn how to review their own and other pupil’s work. They will be able to explain how well they have met the product brief, how successful their text is and how they would adapt their work in future if they were to do something similar again.
                 </li>
             </ul>
+            <p>Due to the nature of the course we are able to give pupils a degree of ownership over how the course is organised for them. While the first 2 units are prescribed by the exam board, subsequent units are chosen by classes after the list of unit choices is presented to them. There is potential for multiple units to be taking place within one class depending on the level of independence and capability of the pupils within the group.
+            </p>
+
+             
+            <h3>Impact</h3>
+            
+            <ul class="circle">
+                <li>Pupils have a appreciation of media and how powerful it is in shaping modern society.</li>
+                <li>Pupils will have a critical appreciation both of the work of others and the work they are able to create themselves.</li>
+                <li>Pupils leave us with a broader cultural capital, a deeper understanding and appreciation of the world around them and the ability to see the creative potential of a range of situations.</li>
+                <li>Pupils are able to confidently use subject specific vocabulary when describing, analysing and evaluating the media texts of others and themselves.  </li>
+                <li>Pupils are able to work independently, organise themselves, use equipment confidently and problem solve how to get the results they want to achieve.</li>
+            </ul>
+
+             <div class="text-center">
+                        <a role="button" target="_blank" href="assets/curriculum/media/year-9-creative-imedia.pdf" class="button">Year 9</a>
+                        <a role="button" target="_blank" href="assets/curriculum/media/year-10-creative-imedia.pdf" class="button">Year 10</a>
+                        <a role="button" target="_blank" href="assets/curriculum/media/year-11-creative-imedia.pdf" class="button">Year 11</a>
+                        <a role="button" target="_blank" href="assets/curriculum/media/creative-imedia-assessment-policy.pdf" class="button">Assesment Policy</a>
+                    </div>
+
             <br>
         </div>
 
