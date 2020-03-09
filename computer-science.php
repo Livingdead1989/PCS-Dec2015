eee94e0820f4c2fe1b7e1b7e8cf4334222b665c0
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-03-09 09:25:01 +0000

    Computer Science Page Update

--- computer-science.php
+++ computer-science.php
@@ -28,10 +28,13 @@
         <section>
             <div class="row">
                 <div class="large-12 columns">
-                    <p>Computing sits at the cornerstone of the modern world, affecting the way we communicate and work as it encompasses Digital Literacy, IT and Computer Science. With this in mind our curriculum offers a pathway for our students to explore the use of applications and the creation of software to solve complex real-world problems through the use of computational thinking, which consists of abstraction, decomposition and pattern recognition.</p>
+                	<h3>Intent</h3>
+                    <p>Computing sits at the cornerstone of the modern world, affecting the way we communicate and work as it encompasses Digital Literacy, IT and Computer Science. With this in mind our curriculum offers a pathway for our students to explore the use of applications and the creation of software to solve complex real-world problems through the use of algorithmic thinking, which consists of abstraction, decomposition and pattern recognition. Our focus is to develop confident independent learners who can apply thinking and reasoning skills developed in computing and other iStem subjects across the curriculum.</p>
+                    <p>We aim to develop competent students who are able to express themselves using a variety of applications and problem-solving techniques which allows them to tackle challenging problems related to business and the communication of data. To achieve this, students must master a number of applications including the ability to decompose a problem into smaller more manageable tasks through the use of abstraction to clarify the important details contained in a specification brief. In order to master these skills students are exposed to familiar and unfamiliar contexts which require the application of critical thinking skills to simplify computable problems. Due to the crossover between Maths and Computer Science students will be encouraged to make use of mathematical formulae when they develop software solutions as well as the use of algebra to develop and test programming concepts.</p>
 
-					<h3>Year 7</h3>
-					<p>In year 7 students will develop their skills and knowledge of a range of software which will allow them to be creative and prepare them for further study.</p>
+					<h3>Implementation</h3>
+					<h4>Year 7</h4>
+					<p>n year 7 students will develop their skills and knowledge of a range of software which will allow them to be creative and prepare them for further study.</p>
 					<p>The curriculum includes opportunities to:</p>
 						<ul>
 							<li>Explore e-safety and gain an understanding of measure to protect personal data</li>
@@ -47,7 +50,7 @@
 					</div>
 					<br>
 
-					<h3>Year 8</h3>
+					<h4>Year 8</h4>
 					<p>Through Year 8 students will deepen their knowledge and proficiency of computing by:</p>
 						<ul>
 							<li>Learning to challenge and investigate what is shared online</li>
@@ -62,6 +65,21 @@
 					</div>
 					<br>
 
+					<h4>Year 9</h4>
+					<p>The areas of study for this year builds on prior learning in years 7 & 8 and constructs the foundations for further study at GCSE through:</p>
+						<ul>
+							<li>Enhancing problem solving strategies through challenging programming projects</li>
+							<li>Investigating the history of computing and the impact of technology on society</li>
+							<li>Exploring how computer networks allow for communication and collaboration</li>
+							<li>Deepen the skills and knowledge required to communicate via HTML and JavaScript, which many businesses state is a skill they want in employees</li>
+							<li>Games design through project based learning and application of critical thinking whilst assessing the needs of users</li>
+						</ul>
+					<br>
+					<div class="text-center">
+						<a role="button" target="_blank" href="assets/curriculum/computer-science/yr9-roadmap.pdf" class="button">Year 9 Curriculum Road Map</a>
+					</div>
+					<br>
+
 					<h3>GCSE Computer Science</h3>
 					<p>Computer Science is a highly creative subject that calls on learners to be inventive. To help us develop this engaging, modern qualification, we talked to companies like Microsoft, Google and Cisco; organisations like Computing at School (CAS) and also teachers and other academics.</p>
 
@@ -69,9 +87,9 @@
 
 					<p>Understanding of computer systems will include:</p>
 						<ul>
-							<li>Studying how processors work.</li>
+							<li>Studying how processors work especially the Von Neumann architecture</li>
 							<li>Investigating computer memory and storage.</li>
-							<li>Exploring modern network layouts and how they function</li>
+							<li>Exploring modern network layouts and how they function to provide platforms for communication and collaboration</li>
 							<li>Building skills in the increasingly important realm of cyber security</li>
 							<li>Investigating how types of software are used within computer systems</li>
 							<li>Considering how computing can affect ethical, legal, cultural and environmental issues</li>
@@ -133,6 +151,30 @@
 							<a role="button" target="_blank" href="assets/curriculum/computer-science/gcse-roadmap.pdf" class="button">GCSE Curriculum Road Map</a>
 						</div>
 						<br>
+
+					<h3>Parents can help their children by:</h3>
+						<ul>
+							<li>Encouraging your son/daughter to discuss the different ways of arriving an answer. Reasoning and conversation lie at the heart of developing problem-solving skills, so talking about school work will help him/her develop as a good computer scientist.</li>
+							<li>Additional challenge can be added by asking ‘What if…?’, and then change the problem in some way.</li>
+							<li>Allowing your son/daughter to ‘teach’ you.</li>
+							<li>Involving him/her in real-life problem-solving, such as algorithms used for decision making for example: traffic light sequences, level crossings, heating controls and alarm systems</li>
+							<li>Challenging him/her to draw algorithms as flow diagrams of write pseudocode to solve problems</li>
+							<li>If your son/daughter is doing well, praise him/her for the effort s/he has put in ('well done, you worked so hard!'), rather than applauding her 'talent'. This will help him/her to learn that s/he can always develop his/her abilities as long as s/he works hard.</li>
+							<li>When s/he gets stuck, ask him/her to explain what s/he has done so far and what exactly s/he is finding hard. Try and help him/her to work out where s/he has gone wrong.</li>
+							<li>Trying to ask open-ended questions to keep a conversation going and explain that, with maths, there is often more than one way to solve problems</li>
+
+					<h3>Revision</h3>	
+
+					<div class="row">
+        				<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    				</div>
+    				<div class="row">
+        				<div class="medium-push-4 medium-8 columns">
+        					<h3>For online resources try the following:</h3>
+            				<p>OCR Mooc – which is free to access once an account has been created<a target="_blank" href="https://cambridgegcsecomputing.org">here</a></p>
+            			</div>
+        				<div class="small-10 small-centered medium-uncentered medium-pull-8 medium-4 columns"><img src="assets/curriculum/computer-science/cs-revision.jpg" alt="Revision Books"></div>
+    					</div>	
 
 						<br><br>
                 </div>
