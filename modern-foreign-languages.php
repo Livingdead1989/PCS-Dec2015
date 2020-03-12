65fc23a76abe0b92a027707540ecf7a79783bc5c
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-03-12 11:37:41 +0000

    MFL Update

--- modern-foreign-languages.php
+++ modern-foreign-languages.php
@@ -34,19 +34,19 @@
             <h3>Purpose</h3>
             <p>Why should you learn a foreign language?  Well -  <em>60% of British employers look for candidates who can speak a foreign language as well as English</em>.</p>
             <p>Whether working on the shop floor dealing with foreign customers, in the port with travellers from Europe, in the transport industry with goods and people from abroad, in the tourist industry travelling or needing to check facilities overseas for your clients, in the building industry sourcing materials or seeking contracts from around the world, in the legal system or in banking, the likelihood is that someone who can make themselves understood and can understand a second language will be more useful than someone who can only speak English.</p>
-            <p>Sometimes it is the skills involved in learning the language that matter rather than the language itself.  We teach Spanish because many of our families holiday in Spain.  Your child will probably not have a choice in Year 7 which language he/she learns but will be given the opportunity to choose whether to switch to the other language or continue with the one they started with. Whichever language is being studied he/she is still learning to listen carefully, develop the confidence to have a go even if not correct, communicate with others, notice patterns, understand consequences, value the need for accuracy, and appreciate cultural differences. Learning a foreign language also reinforces basic literacy skills in English. These are all skills which employers actively look for. </p>
-            <p>Learning a foreign language gives you keys to new opportunities.</p>
+            <p>We teach Spanish because many of our families holiday in Spain and your child will not have a choice in which language he/she learns. Learning a language will teach your child long-life skills – they will learn to listen carefully to not only the words but the intonation, develop the confidence to have a go even if they are unsure, communicate with others and understand others’ cultures and views. Students will be trained to notice patterns in the Spanish language and in English which will lead them to value the need for accuracy, and appreciate cultural differences.</p>
+            <p>Learning a foreign language also reinforces basic literacy skills in English. These are all skills which employers actively look for.</p>
 
-            <h3>Aims</h3>
-            <p>Foreign languages and culture - to be accessible to all pupils at an appropriate level. </p>
+            <h3>Intent</h3>
             <ul class="cirle">
-                <li>Enable opportunities for all pupils at home and abroad.</li>
-                <li>Encourage positive attitudes to learning foreign languages.</li>
-                <li>Develop the ability to use language effectively for communication.</li>
-                <li>Offer insights into the culture and civilisation of other countries combating xenophobia and broadening horizons.</li>
-                <li>Develop pupils’ understanding of themselves, their own language and culture.</li>
-                <li>Promote learning skills useful in other areas – analysis, logic, memory.</li>
-                <li>Support literacy objectives in English.</li>
+                <li>To provide all students with an understanding of the Spanish language and culture.</li>
+                <li>To enable opportunities for all pupils at home and abroad.</li>
+                <li>To encourage positive attitudes to learning foreign languages and open our students’ minds to another way of thinking, speaking and living.​</li>
+                <li>To develop the ability to use language effectively for communication.</li>
+                <li>To offer insights into the culture and civilisation of other countries combating xenophobia and broadening horizons.​</li>
+                <li>To develop pupils’ understanding of themselves, their own language and culture.​</li>
+                <li>To promote learning skills pupils will be able to use in other areas of their studies – analysis, logic, memory, reading skills…</li>
+                <li>To support literacy objectives in English.</li>
                 <li>Develop pupils who: 
                     <ul>
                         <li>become more confident, showing a greater willingness to persevere.</li>
@@ -56,191 +56,28 @@
                 </li>
             </ul>
             
-            <h3>Areas of Study</h3>
-            <p></p>
-
-            <ul class="accordion" data-accordion role="tablist">
-
-                <li class="accordion-navigation">
-                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">Year 7 - National Curriculum - Spanish</a>
-                    <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Introductions and basic personal information and classroom phrases.</li>
-                            <li>Identity – nationality, family, pets, descriptions.</li>
-                            <li>School – subjects, opinions, telling the time, timetable, school day, uniform.</li>
-                            <li>Sport and leisure – likes/dislikes, frequency, weather.</li>
-                            <li>Where you live – house, town, directions.</li>
-                            <li>Trip – preparation and follow-up.</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Communicating</li>
-                            <li>Listening and speaking.</li>
-                            <li>Reading and writing.</li>
-                            <li>Intercultural understanding.</li>
-                            <li>Knowledge about language.</li>
-                            <li>Language learning strategies.</li>
-                            <li>E.g. Appropriate use of dictionaries and other resources.</li>
-                            <li>Phoneme/grapheme links.</li>
-                            <li>Presenting information.</li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Year 8 - National Curriculum - Spanish</a>
-                    <div id="panel2d" class="content" role="tabpanel" aria-labelledby="panel2d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>Consolidation of basics.</li>
-                            <li>World War 1 (joint project with History).</li>
-                            <li>Life in other countries.</li>
-                            <li>Healthy lifestyle – food, fitness etc.</li>
-                            <li>I’m a celebrity – pupil choice.</li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Listening and speaking.</li>
-                            <li>Reading and writing.</li>
-                            <li>Intercultural Understanding.</li>
-                            <li>Knowledge about language.</li>
-                            <li>Language learning strategies.</li>
-                            <li>e.g. Appropriate use of dictionaries and other resources.</li>
-                            <li>General study skills :
-                                <ul>
-                                    <li>Perseverance</li>
-                                    <li>Team working</li>
-                                    <li>Presentation</li>
-                                    <li>Creativity</li>
-                                    <li>Evaluating and editing</li>
-                                </ul>
-                                
-
-                            </li>
-                        </ul>
-
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel3d" role="tab" id="panel3d-heading" aria-controls="panel3d">Year 9 - GCSE - Spanish</a>
-                    <div id="panel3d" class="content" role="tabpanel" aria-labelledby="panel3d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-
-                        <h5>Personal and social life</h5>
-                        <ul class="circle">
-                                    <li>Self</li>
-                                    <li>Family</li>
-                                    <li>Friends</li>
-                                    <li>Home life</li>
-                                    <li>Shopping</li>
-                                    <li>Meals</li>
-                                    <li>Healthy living</li>
-                                    <li>Illness and accident</li>
-                                    <li>Free time</li>
-                                    <li>Fashion</li>
-                                    <li>Relationships</li>
-                                    <li>Future plans</li>
-                        </ul>
-                        <br>
-                        <h5>Local community</h5>
-                                <ul class="circle">
-                                    <li>Home town</li>
-                                    <li>School</li>
-                                    <li>Education</li>
-                                    <li>Local environment</li>
-                                    <li>Pollution</li>
-                                    <li>Recycling</li>
-                                    <li>Local facilities</li>
-                                    <li>Comparisons with other towns and regions</li>
-                                    <li>Weather and seasons</li>
-                                </ul>
-                        <br>
-                            <h5>The world of work</h5>
-                                <ul class="circle">
-                                    <li>Work experience, part-time jobs</li>
-                                    <li>Future careers</li>
-                                    <li>Technology (sending messages, accessing information)</li>
-                                </ul>
-                        <br>
-                            <h5>The wider world</h5>
-                                <ul class="circle">
-                                    <li>Travel and holidays</li>
-                                    <li>Media</li>
-                                    <li>Social issues (e.g. life of young people today, homelessness, crime, drugs, healthy living, religion, politics)</li>
-                                    <li>Life in the countries and communities where the language is spoken</li>
-                                </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Listening and Speaking</li>
-                            <li>Reading and Writing</li>
-                            <li>Knowledge about language</li>
-                            <li>Language learning strategies</li>
-                            <li>Presentations</li>
-                            <li>Exam technique</li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel4d" role="tab" id="panel4d-heading" aria-controls="panel4d">Year 10 - GCSE - Spanish</a>
-                    <div id="panel4d" class="content" role="tabpanel" aria-labelledby="panel4d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>In the world of work
-                                <ul>
-                                    <li>Personal ID</li>
-                                    <li>Making reservations for travel</li>
-                                    <li>Accommodation and leisure</li>
-                                    <li>Applying for a job</li>
-                                    <li>Adverts</li>
-                                    <li>CV and interview</li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Understanding, speaking, reading and writing</li>
-                            <li>Knowledge about language</li>
-                            <li>Organisation skills</li>
-                            <li>Self-evaluation</li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel5d" role="tab" id="panel5d-heading" aria-controls="panel5d">Year 11 - GCSE - Spanish</a>
-                    <div id="panel5d" class="content" role="tabpanel" aria-labelledby="panel5d-heading">
-                        <p class="text-capitalize">Main topics students study:</p>
-                        <ul class="circle">
-                            <li>In the world of work
-                                <ul>
-                                    <li>Personal ID</li>
-                                    <li>Making reservations for travel</li>
-                                    <li>Accommodation and leisure</li>
-                                    <li>Applying for a job</li>
-                                    <li>Adverts</li>
-                                    <li>CV and interview</li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <br>
-                        <p class="text-capitalize">Main skills students develop:</p>
-                        <ul class="circle">
-                            <li>Understanding, speaking, reading and writing</li>
-                            <li>Knowledge about language</li>
-                            <li>Organisation skills</li>
-                            <li>Self-evaluation</li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+            <h3>Implementation</h3>
+            
             <br>
+            <div class="text-center">
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year7-spanish.pdf" class="button">Year 7 Overview</a>  
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year8-spanish.pdf" class="button">Year 8 Overview</a> 
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year9-spanish.pdf" class="button">Year 9 Overview</a>  
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year10-spanish.pdf" class="button">Year 10 Overview</a>                      
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year11-spanish.pdf" class="button">Year 11 Overview</a>                      
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year7-homework-project1.pdf" class="button">Year 7 Project 1</a> 
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year7-homework-project2.pdf" class="button">Year 7 Project 2</a> 
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year8-homework-project1.pdf" class="button">Year 8 Project 1</a>                      
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year8-homework-project2.pdf" class="button">Year 8 Project 2</a>                      
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/year9-homework-project.pdf" class="button">Year 9 Project</a>                      
+            </div>
+            <br>
+            <div class="text-center">
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/aqa-gcse-resources.pdf" class="button">AQA GCSE Resources</a>
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/competencies.pdf" class="button">Competencies</a>
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/curriculum-map.pdf" class="button">Curriculum Map</a>
+                        <a role="button" target="_blank" href="assets/curriculum/mfl/language-lesson.pdf" class="button">Language Lesson</a> 
+            </div>
         </div>
 
     </div>
