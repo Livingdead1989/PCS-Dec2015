abf88ab44f84271a898a4b07ed5882957965edc6
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-03-16 13:14:34 +0000

    Dance Update

--- dance.php
+++ dance.php
@@ -31,22 +31,19 @@
     <div class="row">
         <div class="large-12 columns">
         <br>
-            <h3>Purpose</h3>
-            <p>Dance at Park helps students develop technical and expressive skills as well as a knowledge and understanding of Dance through performance, choreography and critical appreciation. It allows students to actively and physically demonstrate their abilities through exciting and engaging schemes of work which cover different styles and cultures of Dance that prepare students for studying RSL Dance.</p>
-
             <h3>Intent</h3>
-            <p>Dance uses movement symbolically as a fundamental form of human expression. It is both empowering and powerful as a form of non-verbal communication. The value of Dance is the development of creative, imaginative, physical, emotional and intellectual capacities. Dance is both physical and expressive. Dance also helps students with essential life skills required after their secondary education and into the world of work or further education. These include skills such as cooperation, communication, leadership and problem solving skills alongside increased confidence in taking on new challenges.</p>
+            <p>Within the dance department we aim to ensure that every child has the opportunity to explore their creativity, expressive skills and evaluative skills. Students are encouraged to work within small groups as a way of building their problem solving skills, confidence, communication, empathy, self discipline and resilience.</p>
+            <p>Dance at Park helps students develop technical and expressive skills as well as a knowledge and understanding of Dance through performance, choreography and appreciation. It allows students to actively and physically demonstrate their abilities through exciting and engaging schemes of work which cover different styles and cultures that prepare students for studying RSL Dance.</p>
                         
-            <h3>Areas of Study</h3>
-            <p>The KS3 curriculum enables students to build up a good repertoire of basic skills that students need to master before they go into KS4. Students learn a range of different choreographic devices and performance skills that build their self-confidence and team work skills that are fundamental to their learning. Students have the opportunity to learn different styles of Dance and are encouraged to self assess and evaluate the work of others. Examples of units are Thriller, Boy Blue, Site Sensitive and Shadows which include looking at a wide range of dance styles.</p>
-
+            <h3>Implementation</h3>
+            <p>The KS3 curriculum enables students to build up a good repertoire of basic skills that students need to master before they go into KS4. Students learn a range of different choreographic devices and performance skills that build their self-confidence and team work skills that are fundamental to their learning. Students have the opportunity to learn different styles of Dance and are encouraged to self assess and evaluate the work of others. Examples of units are Thriller, Bollywood, Hip hop, Site Sensitive and Shadows which include looking at a wide range of dance styles.</p>
             <p>Once in KS4 students then begin studying for the RSL Level 2 Certificate in Creative and Performing Arts (Dance) which consists of two units; Ensemble and Live Performance.</p>
 
             <p><strong>Performance</strong> Students look at physical fitness and effectiveness as a performer through safe practice, technique classes, rehearsal and performance. They are encouraged to work together to create and perform pieces which challenge students and enable them to achieve their full potential.</p>
 
             <p><strong>Choreography</strong> Students explore ideas based around a variety of stimuli through creating, improvising, developing and selecting movements appropriate to a theme or stimulus. Throughout the course they create a group piece of choreography as well as a solo, both of which are based on stimuli from professional dance works.</p>
 
-            <p><strong>Critical Appreciation</strong> Students develop their critical skills through describing, analysing, interpreting and evaluating professional dance works. They will look at movement, set design, costume, lighting design and accompaniment. This culminates in a written examination which focuses on pulling together the practical knowledge they have acquired during the course.</p>
+            <p><strong>Analysis</strong> Students develop their evaluative  skills through describing and analysing their practical work. They will keep a weekly journal where they will evaluate strengths, areas for improvement and set personal targets. </p>
 
             <br>
         </div>
