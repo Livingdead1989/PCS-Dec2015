1de017fb840476901681c4e42314923d9fb5d320
Phil Warren <P.Warren@pcs.hants.sch.uk>
2020-03-16 13:10:21 +0000

    Drama Update

--- drama.php
+++ drama.php
@@ -31,11 +31,6 @@
     <div class="row">
         <div class="large-12 columns">
 
-			<h3>Purpose</h3>
-			<p>Studying drama requires emotional maturity, and gives students a deep understanding of themselves. Because it involves using not only their voices and bodies, but also emotions and creativity, it is able to engender a deep sense of self-esteem. It is also a subject that requires a great deal of peer trust, and so it plays an important role in teaching communication, listening and empathy skills. Studying Drama is demanding, and teaches students that success only comes from hard work.</p>
-
-			<p>The skills that are learnt by studying Drama are invaluable in later life. For example, being able to speak and present confidently in front of people, and the skill of planning that is required in any production, are useful in many careers. Drama can be a good basis for a career not only in the theatre, but in advertising, the film industry and even in events planning, among many others.</p>
-			
 			<h3>Intent</h3>
 			<p>Studying drama requires emotional maturity, and gives students a deep understanding of themselves. Because it involves using not only their voices and bodies, but also emotions and creativity, it is able to engender a deep sense of self-esteem. It is also a subject that requires a great deal of peer trust, and so it plays an important role in teaching communication, listening and empathy skills. Studying Drama is demanding, and teaches students that success only comes from hard work.</p>
 
@@ -59,11 +54,23 @@
 				<li>The development of inter-personal skills (e.g. by helping students to be aware of their roles as members of groups, and through taking into account other pupil’s thoughts and ideas).</li>
 				<li>The development of functional skills, which will enable them to operate confidently, effectively and independently in life and work.</li>
 			</ul>
-			<h3>Areas of Study</h3>
-			<h3>Key Stage 3</h3>
+			<h3>Implementation</h3>
 			<p>In Year 7 students are introduced to the basic conventions of theatre and storytelling, exploring skill of characterisation, using physical and vocal skills.  Students will also learn the basics of staging plays they have devised themselves. Students will also begin to explore physical theatre through a horror scheme of work. This unit also includes how to create atmosphere and tension and how to use the lighting desk to create this feeling. In these lessons, students will develop group work and problem-solving abilities alongside dramatic techniques. The emphasis is on mastering the skills required to perform in character, with confidence to a small audience of their peers.</p>
 			<p>Once in Year 8, all students explore ways that movement can be used within performance to communicate meaning to an audience. Students learn about genres in theatre and start off with Comedy including Melodrama, Slapstick Comedy and Physical theatre. This scheme aims to develop student’s self confidence in moving with control in a range of scenes. This topic also covers the basics of technical directing for stage, learning lines and recreating characters an important part of study at Key Stage 4 whilst looking at script work.</p>
-			<p>Once in KS4 students then begin studying for the RSL Level 2 Certificate in Creative and Performing Arts (Acting) which consists of two units; Live Text and Live Performance.</p>
+			<p>Once in KS4 students then begin studying for the RSL Level 2 Certificate in Creative and Performing Arts (Acting) which consists of two units; Performing Text and Live Performance.</p>
+
+			<h3>Extra Curricular</h3>
+			<p>Drama Club. A weekly club for students from Year 7 - 10 offering the following;</p>
+						<ul>
+							<li>Annual Drama Club Shows</li>
+							<li>Trips</li>
+							<li>Workshops</li>
+							<li>Script Writing</li>
+							<li>Directing Skills</li>
+							<li>Performance and workshops with local primary schools</li>
+						</ul>
+
+			<p>In year 9 students begin to develop the skills autonomously in readiness for RSL Acting. These topics look at different genres in Drama, devising from a set stimulus and learning lines to recreate a well known script. Students will also take part in performance within school as well as to parents to help prepare them for the performance aspect that is essential when studying RSL Acting.</p>
 		<br><br>
         </div>
     </div>
