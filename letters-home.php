41226b78d7597d44470f43333d561b84d73b179a
P Warren <P.Warren@pcs.hants.sch.uk>
2020-06-19 08:19:39 +0100

    Letter Update

--- letters-home.php
+++ letters-home.php
@@ -28,6 +28,7 @@
             <ul class="small-block-grid-1">
             	<li>
             		<ul class="circle">June 2020<hr>
+            			<li><a href="/assets/letters/2020-06/yr9-face-to-face.pdf" target="_blank">16th June - Year 9 Face-to-Face</a></li>
             			<li><a href="/assets/letters/2020-06/yr6_parent_08-06.pdf" target="_blank">Year 6 Transition</a></li>
             			<li><a href="/assets/letters/2020-06/yr10-face-to-face.pdf" target="_blank">10th June - Year 10 Face-to-Face</a></li>
             			<li><a href="/assets/letters/2020-06/newsletter-05-06.pdf" target="_blank">Newsletter 5th June</a></li>
