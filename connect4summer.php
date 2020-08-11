5e28ca7417d438b3ddff7a98f7a1ce5125cca52c
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2020-08-11 12:26:43 +0100

    SPA Changes - C4S

--- connect4summer.php
+++ connect4summer.php
@@ -140,7 +140,7 @@
 
 			</div>
 
-	<hr>
+	<br><hr>
 
 	<div class="row">
 		<div class="columns">
@@ -153,7 +153,7 @@
 		</div>
 	</div>
 
-	<hr>
+	<br><hr>
 
 	<div class="row">
 		<div class="columns">
