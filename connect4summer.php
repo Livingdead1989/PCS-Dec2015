c202088fd066499644abc6d38411b16c86bc714f
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2020-08-11 12:24:11 +0100

    SPA Changes - Connect4Summer

--- connect4summer.php
+++ connect4summer.php
@@ -23,6 +23,43 @@
 <section>
 	<div class="row">
 		<div class="columns">
+			<h2>What is Connect4Summer?</h2>
+			<p>In October 2019 the Department for Education published its invitation to bid for the local coordination of a free holiday provision fund which included activities and healthy food for disadvantaged children during 2020 summer holidays.</p>
+
+			<p>During the summer of 2019 Romsey Community School and Park Community School successfully bid and provided a programme which was very successful. Therefore, we bid for the Holiday Activities Fund in summer 2020 and were delighted to hear we had won the bid once again.</p>
+
+			<div class="row">
+				<div class="small-push-2 small-8 medium-push-2 medium-8 large-push-3 large-6 columns">
+					<img src="images/connect4summer/connect4summer_logo.png" alt="Connect4Summer Logo">
+				</div>
+			</div>
+			<br>
+
+			<p>The project for 2020 is based on the pilot year and has been expanded to include more venues in Hampshire, Portsmouth, Southampton and the Isle of Wight.</p>
+
+			<p>The programme is run in 50 venues and includes a range of sports activities, healthy cooking classes for children and their parents or carers, as well as additional support for our most vulnerable families who are in need.</p>
+
+			<p>The 20-day project runs from 29 July - 25 August, 10am-3pm.</p> 
+
+			<div class="small-offset-2 small-8 text-center">
+				<div class="flex-video widescreen border-radius-5">
+					<iframe src="https://player.vimeo.com/video/446392942" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+				</div>
+			</div>
+
+			<br><br>
+			
+		</div>
+	</div>
+</section>			
+
+<section class="grey-bg">
+    <div class="row">
+    	<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+	<div class="row">
+		<div class="columns">
+
 			<br>
 				<h2>Connect4Summer 2020</h2>
 				<p>Connect4Summer is back at Park for 2020! Join us from 29th July-25th August for a summer of fun.</p>
@@ -40,31 +77,26 @@
                 <a class="large button radius" target="_blank" href="https://connect4.org.uk/">Connect4Summer</a>
             </div>
 
-            <div class="columns small-12 medium-12 large-6 text-center">
+            <div class="small-offset-2 small-8 text-center">
 				<div class="flex-video widescreen border-radius-5">
 					<iframe src="https://player.vimeo.com/video/434748011" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 				</div>
 			</div>
-
-            <div class="columns small-12 medium-12 large-6 text-center">
-				<div class="flex-video widescreen border-radius-5">
-					<iframe src="https://player.vimeo.com/video/446392942" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-				</div>
-			</div>
-
 		
 			<br><br>
+			
 		</div>
 	</div>
-</section>			
+</section>
 
-<section class="grey-bg">
-    <div class="row">
-    	<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+<section>
+	<div class="row">
+    	<div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
 	<div class="row">
 		<div class="columns">
-			<h2>Connect4Summer 2019 - Review</h2>
+			<div class="row">
+				<h2>Connect4Summer 2019 - Review</h2>
 				<p>We are really proud of what we have achieved, 17 organisations have provided free daily child care and 21 organisations have provided free family fun days for 20 days and the feedback from parents has been tremendous....</p>
 
 				<div class="small-offset-2 small-8 text-center">
@@ -78,18 +110,18 @@
                 </div>
 
                 <br><br>
+				
 		</div>
 	</div>
 </section>
 
-<section>
-	<div class="row">
-    	<div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+<section class="grey-bg">
+    <div class="row">
+    	<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
     </div>
 	<div class="row">
 		<div class="columns">
-			<div class="row">
-				<h2>Connect4Summer in the BBC</h2>
+			<h2>Connect4Summer in the News</h2>
 
 
 				<div class="columns small-12 medium-12 large-6 text-center">
@@ -108,53 +140,23 @@
 
 			</div>
 
-			<div class="row">
-				<div class="columns">
-					<h2>Connect4Summer on the Radio</h2>
-					<p>BBC Radio Solent's Julian Glegg talking with Susan Parish about the Connect4Summer scheme.</p>
-					<div class="text-center">
-						<audio src="assets/connect4summer/radio.mp3" controls>
-						</audio>
-					</div>
-				</div>
-			</div>
-	
-			
-			<h2>What is Connect4Summer?</h2>
-			<p>Connect4Summer is a programme for children in 23 venues across Hampshire which provides an exciting programme that will include a range of sports activities, healthy cooking classes for children and their parents or carers, as well as additional support for our most vulnerable families who are in need.</p>
+	<hr>
 
-			<div class="row">
-				<div class="small-push-2 small-8 medium-push-2 medium-8 large-push-3 large-6 columns">
-					<img src="images/connect4summer/connect4summer_logo.png" alt="Connect4Summer Logo">
-				</div>
-			</div>
-			<br>
-
-			<p>This scheme has been made possible by a joint funding bid by Park Community School and Romsey Community School.</p>
-			<br><br>
-		</div>
-	</div>
-</section>
-
-<section class="grey-bg">
-    <div class="row">
-    	<div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
 	<div class="row">
 		<div class="columns">
-			<h2>Summer MUNCH at Park</h2>
-			<p>Our holiday <a href="/munch.php">MUNCH</a> meals continue to provide a free 2 course meal for children attending any school in Leigh Park, parents and carers are also welcome, whose curent circumstances might mean that receiving a free meal is necessary. - <strong>Excludes Bank Holidays</strong></p>
-			<br><br>
+			<h2>Connect4Summer on the Radio</h2>
+			<p>BBC Radio Solent's Julian Glegg talking with Susan Parish about the Connect4Summer scheme.</p>
+			<div class="text-center">
+				<audio src="assets/connect4summer/radio.mp3" controls>
+				</audio>
+			</div>
 		</div>
 	</div>
-</section>
 
-<section>
+	<hr>
+
 	<div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-	<div class="row">
-		<div class="column">
+		<div class="columns">
 			<h2>Press Release - Minister announces funding awarded to Park Community and Romsey Community Schools</h2>
 			<p>News that a joint funding bid by Park Community School in Havant and Romsey Community School in Havant has been successful has been warmly welcomed.</p>
 			<p>On 8 May 2019, Children and Families Minister, Nadhim Zahawi, announced that 11 coordinators will receive a share of £9.1m funding to provide 50,000 disadvantaged children with free meals and activities over the upcoming summer holidays.</p>
@@ -174,6 +176,22 @@
 				<li>Email: <a href="mailto:JLefley@romsey.hants.sch.uk">JLefley@romsey.hants.sch.uk</a></li>
 				<li>Tel: 01794522106</li>
 			</ul>
+			<br><br>
+		</div>
+	</div>
+			<br><br>	
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+	<div class="row">
+		<div class="column">
+			<h2>Summer MUNCH at Park</h2>
+			<p>Our holiday <a href="/munch.php">MUNCH</a> meals continue to provide a free 2 course meal for children attending any school in Leigh Park, parents and carers are also welcome, whose curent circumstances might mean that receiving a free meal is necessary. - <strong>Excludes Bank Holidays</strong></p>
 			<br><br>
 		</div>
 	</div>
