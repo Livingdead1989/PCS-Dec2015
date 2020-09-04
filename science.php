7b0217ae93b2c4bd6daff82e1649dfda1d82fb9f
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2020-09-04 14:20:41 +0100

    science update

--- science.php
+++ science.php
@@ -30,214 +30,124 @@
 <section>
     <div class="row">
         <div class="large-12 columns">
-    
-        <div class="text-right"><a href="#" data-reveal-id="revision_resources" class="button round alert">Useful Resources</a></div>
-
         <br>
             <h3>Intent</h3>
-                    <p>Science has something to offer every student, suiting all abilities and all aspirations. Our aim is to develop worldly citizens through boosting cultural capital.</p>
-                    <p>During KS3 in science, we promote awe and wonder. We want our students to be curious about the world around them and be confident to ask questions and investigate a range of possibilities.</p>
-                    <p>Our curriculum is designed to build upon the learning in KS2 and enable students to develop the necessary skills when working as a scientist. Vocabulary is very important in science and we develop the amount of science specific words learnt by students and ensure these are practised both orally and in written work.</p>
-                    <p>Once at KS4, we build upon the key skills and knowledge learnt in KS3 and continue to master these and apply to a range of contexts. </p>
+            <p>Science has something to offer every student, suiting all abilities and all aspirations. Our aim is to develop worldly citizens through boosting cultural capital.</p>
+            <p>During KS3 in science, we promote awe and wonder. We want our students to be curious about the world around them and be confident to ask questions and investigate a range of possibilities.</p>
+            <p>Our curriculum is designed to build upon the learning in KS2 and enable students to develop the necessary skills when working as a scientist. Vocabulary is very important in science and we develop the amount of science specific words learnt by students and ensure these are practised both orally and in written work.</p>
+            <p>Once at KS4, we build upon the key skills and knowledge learnt in KS3 and continue to master these and apply to a range of contexts.</p>         
 
-                    <h3>Implementation</h3>
-                    <p>We actively promote literacy and cultural capital through the reading of articles to allow students to understand the world around them.</p>
-                    <p>In KS3, we currently complete fortnightly awe and wonder specific lessons-based space; these are designed to allow exploration of students own ideas and questions.</p>
-                    <p>We have a centralised SOW designed to ensure NC covered with appropriate levels of challenge. Includes working scientifically skills to be covered and assessed.</p>
-                    <p>For KS4, we have a centralised SOW designed to ensure AQA specification is covered from grades 1-9.</p>
-                    <p>Clear focus on expanding scientific vocabulary to ensure fluency in both written and verbal answers.</p>
-                    <p>Fully embedded maths skills that are required, with clear links with maths methods and vocabulary used.</p>
-
+            <h3>Implementation</h3>
+            <p>We actively promote literacy and cultural capital through the reading of articles to allow students to understand the world around them.</p>
+            <p>In KS3, we currently complete fortnightly awe and wonder specific lessons-based space; these are designed to allow exploration of students own ideas and questions.</p>
+            <p>We have a centralised SOW designed to ensure NC covered with appropriate levels of challenge. Includes working scientifically skills to be covered and assessed.</p>
+            <p>For KS4, we have a centralised SOW designed to ensure AQA specification is covered from grades 1-9.</p> 
+            <p>We improve and master the working scientifically skills built upon from KS3, while covering the required practical elements of the course.</p>
+            <p>Clear focus on expanding scientific vocabulary to ensure fluency in both written and verbal answers.</p>
+            <p>Fully embedded maths skills that are required, with clear links with maths methods and vocabulary used.</p>         
                      
             <br>
 
-                    <h3>Impact</h3>
-                    <p>Our focus by the end of KS3, is for all students to have acquired the necessary scientific skills and knowledge to ensure they are ‘GCSE ready’.</p>
-                    <p>Students leave KS3 with high aspirations having had a positive experience of science. They are able to achieve age related expectations (ARE) or above and are fully prepared for their GCSE.</p>
-                    <p>Once in KS4, our aim is to increase the number of students going onto study a science subject at higher education.</p>
+            <h3>Impact</h3>
+            <p>Our focus by the end of KS3, is for all students to have acquired the necessary scientific skills and knowledge to ensure they are ‘GCSE ready’.</p> 
+            <p>Students leave KS3 with high aspirations having had a positive experience of science. They are able to achieve age related expectations (ARE) or above and are fully prepared for their GCSE.</p> 
+            <p>Once in KS4, our aim is to increase the number of students going onto study a science subject at higher education.</p>                   
 
             <br>
-                    <div class="text-center">
-                        <a role="button" target="_blank" href="assets/curriculum/science/ks4-trilogy.pdf" class="button">Key Stage 4 Trilogy</a>
-                    </div>
-                    
-                    <h3>Subject Content</h3>
+            <div class="text-center">
+                <a role="button" target="_blank" href="assets/curriculum/science/curriculum_map.pdf" class="button">Curriculum Map</a>
+            </div>
+            
+            <h3>Subject Content</h3>
 
             <ul class="accordion" data-accordion role="tablist">
                 <li class="accordion-navigation">
-                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">Biology</a>
+                    <a href="#panel1d" role="tab" id="panel1d-heading" aria-controls="panel1d">Key Stage 3</a>
                     <div id="panel1d" class="content" role="tabpanel" aria-labelledby="panel1d-heading">
-                        <ul>
-                            <li>Cell biology</li>
-                            <li>Organisation</li>
-                            <li>Infection and response</li>
-                            <li>Bioenergetics</li>
-                            <li>Homeostasis and response</li>
-                            <li>Inheritance, variation and evolution</li>
-                            <li>Ecology</li>
-                            <li>Key ideas</li>
-                        </ul>
+                        <p><a href="assets/curriculum/science/ks3_big_picture.pdf">KS3 Big Picture</a></p>
+                        
+                        <h4>Year 7</h4>
+                            <ul>
+                                <li>Identity</li>
+                                <li>Organisms</li>
+                                <li>Forces 1</li>
+                                <li>Reactions 1</li>
+                                <li>Particles</li>
+                                <li>Electricity</li>
+                            </ul>
+                        <hr>
+
+                        <h4>Year 8</h4>
+                            <ul>
+                                <li>Body Systems</li>
+                                <li>Sound</li>
+                                <li>Light</li>
+                                <li>Magnets</li>
+                                <li>Speed</li>
+                                <li>Reactions 2</li>
+                                <li>Pressure</li>
+                            </ul>
+                        <hr>
+
+                        <h4>Year 9</h4>
+                            <ul>
+                                <li>Photosynthesis &amp; Respiration</li>
+                                <li>Earth</li>
+                                <li>Energy</li>
+                                <li>Static</li>
+                                <li>Forces 2</li>
+                                <li>Reactions 3</li>
+                            </ul>
                     </div>
                 </li>
 
                 <li class="accordion-navigation">
-                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Chemistry</a>
+                    <a href="#panel2d" role="tab" id="panel2d-heading" aria-controls="panel2d">Key Stage 4</a>
                     <div id="panel2d" class="content" role="tabpanel" aria-labelledby="panel2d-heading">
-                        <ul>
-                            <li>Atomic structure and the periodic table</li>
-                            <li>Bonding, structure, and the properties of matter</li>
-                            <li>Quantitative chemistry</li>
-                            <li>Chemical changes</li>
-                            <li>Energy changes</li>
-                            <li>The rate and extent of chemical change</li>
-                            <li>Organic chemistry</li>
-                            <li>Chemical analysis</li>
-                            <li>Chemistry of the atmosphere</li>
-                            <li>Using resources</li>
-                            <li>Key ideas</li>
-                        </ul>
-                    </div>
-                </li>
+                        <h4>AQA Trilogy</h4>
+                        <p>We believe that science has something to offer every student. That’s why we have a suite of science qualifications for Key Stage 4 – to suit students of all abilities and all aspirations.</p>
 
-                <li class="accordion-navigation">
-                    <a href="#panel3d" role="tab" id="panel3d-heading" aria-controls="panel3d">Physics</a>
-                    <div id="panel3d" class="content" role="tabpanel" aria-labelledby="panel3d-heading">
+                        <p><strong>Big Pictures</strong></p>
                         <ul>
-                            <li>Energy</li>
-                            <li>Electricity</li>
-                            <li>Particle model of matter</li>
-                            <li>Atomic structure</li>
-                            <li>Forces</li>
-                            <li>Waves</li>
-                            <li>Magnetism and electromagnetism</li>
-                            <li>Key ideas</li>
+                            <li><a href="assets/curriculum/science/ks4_bio_big_picture.pdf">KS4 Biology Big picture</a></li>
+                            <li><a href="assets/curriculum/science/ks4_chem_big_picture.pdf">KS4 Chemistry Big picture</a></li>
+                            <li><a href="assets/curriculum/science/ks4_phy_big_picture.pdf">KS4 Physics Big picture</a></li>
                         </ul>
+
+                        <h4>Year 10</h4>
+                            <ul>
+                                <li>B5 Homeostasis and response</li>
+                                <li>B6 Inheritance, Variation and Evolution</li>
+                                <li>B7 Ecology</li>
+                                <li>C1 Atomic structure and periodic table</li>
+                                <li>C2 Structure and Bonding</li>
+                                <li>C3 Quantitative chemistry</li>
+                                <li>C4 Chemical changes</li>
+                                <li>C5 Energy changes</li>
+                            </ul>
+                        <hr>
+                        
+                        <h4>Year 11</h4>
+                            <ul>
+                                <li>B5 Homeostasis and response</li>
+                                <li>B6 Inheritance, Variation and Evolution</li>
+                                <li>B7 Ecology</li>
+                                <li>P5 Forces</li>
+                                <li>P6 Waves</li>
+                                <li>P7 Magnets</li>
+                                <li>C10 Using resources</li>
+                            </ul>
                     </div>
                 </li>
             </ul>
 
-            <h3>How and What is Assessed</h3>
-
-            <ul class="accordion" data-accordion role="tablist">
-                <li class="accordion-navigation">
-                    <a href="#panel1p" role="tab" id="panel1p-heading" aria-controls="panel1p">Biology Paper 1</a>
-                    <div id="panel1p" class="content" role="tabpanel" aria-labelledby="panel1p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Biology Topics 1-4: Cell Biology; Organisation; Infection and Response; and Bioenergetics.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel2p" role="tab" id="panel2p-heading" aria-controls="panel2p">Biology Paper 2</a>
-                    <div id="panel2p" class="content" role="tabpanel" aria-labelledby="panel2p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Biology Topics 5-7: Homeostasis and Response; Inheritance, Variation and Evolution; and Ecology.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-
-                <li class="accordion-navigation">
-                    <a href="#panel3p" role="tab" id="panel3p-heading" aria-controls="panel3p">Chemistry Paper 1</a>
-                    <div id="panel3p" class="content" role="tabpanel" aria-labelledby="panel3p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Chemistry Topics 8-12: Atomic Structure and the Periodic Table; Bonding, Structure, and the Properties of Matter; Quantitative Chemistry; Chemical Changes; and Energy changes.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-                 <li class="accordion-navigation">
-                    <a href="#panel4p" role="tab" id="panel4p-heading" aria-controls="panel4p">Chemistry Paper 1</a>
-                    <div id="panel4p" class="content" role="tabpanel" aria-labelledby="panel4p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Chemistry Topics 13-17: The Rate and Extent of Chemical change; Organic Chemistry; Chemical Analysis of the Atmosphere; and using resources.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-                <li class="accordion-navigation">
-                    <a href="#panel5p" role="tab" id="panel5p-heading" aria-controls="panel5p">Physics Paper 1</a>
-                    <div id="panel5p" class="content" role="tabpanel" aria-labelledby="panel5p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Physics Topics 18-21: Energy; Electricity; Particle Model of Matter; and Atomic Structure.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-                <li class="accordion-navigation">
-                    <a href="#panel6p" role="tab" id="panel6p-heading" aria-controls="panel6p">Physics Paper 2</a>
-                    <div id="panel6p" class="content" role="tabpanel" aria-labelledby="panel6p-heading">
-                        <h4>What's Assessed</h4>
-                        <p>Physics Topics 22-24: Forces; Waves; and Magnetism and Electromagnetism.</p>
-                        <h4>How It's Assessed</h4>
-                        <ul class="circle">
-                            <li>Written Exam: 1 hour 15 minutes</li>
-                            <li>Foundation and Higher Tier</li>
-                            <li>70 Marks</li>
-                            <li>16.7% of GCSE</li>
-                        </ul>
-                        <h4>Questions</h4>
-                        <p>Multiple Choice, Structured, Closed Short Answer and Open Response.</p>
-                    </div>
-                </li>
-            </ul>
+            
 
             <br>
         </div>
 <br><br>
     </div>
 </section>
-
-<!-- Modal -->
-<div id="revision_resources" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
-    <h2>Resources</h2>
-    <p><a href="https://filestore.aqa.org.uk/resources/science/specifications/AQA-8464-SP-2016.PDF" target="_blank">GCSE Trilogy Specification <small>(WEB)</small></a></p>
-    <p><a href="https://filestore.aqa.org.uk/resources/science/AQA-8464-8465-PRACTICALS-HB.PDF" target="_blank">GCSE Combined science: Required Practicals <small>(WEB)</small></a></p>
-    <p><a href="https://www.bbc.co.uk/bitesize/examspecs/z8r997h" target="_blank">BBC bitesize - Revision <small>(WEB)</small></a></p>
-    <p><a href="https://www.youtube.com/channel/UCqbOeHaAUXw9Il7sBVG3_bw" target="_blank">GCSE Science lesson - Revision <small>(WEB)</small></a></p>
-  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
-</div>
-
-
-        
-<!-- /Modal -->
 
 <!-- FOOTER -->
 <?php include("includes/footer.html") ?>
