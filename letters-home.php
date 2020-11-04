94fdad88c66c8d4d5c370b84f2e9b4b835cb8b46
P Warren <P.Warren@pcs.hants.sch.uk>
2020-11-04 17:10:59 +0000

    Letter Update

--- letters-home.php
+++ letters-home.php
@@ -26,6 +26,11 @@
         <div class="medium-12 columns">
         	<p>We feel that everyone should be kept informed. Below you will find all the letters we send home so you get the information needed, regardless of the animals eating it or it getting lost!</p>
             <ul class="small-block-grid-1">
+                <li>
+                    <ul class="circle">November 2020<hr>
+                        <li><a href="/assets/letters/2020-11/general-letter-04.pdf" target="_blank">General Letter 4th November</a></li>
+                    </ul>
+                <li>
             	<li>
             		<ul class="circle">October 2020<hr>
                         <li><a href="/assets/letters/2020-10/general-letter-22.pdf" target="_blank">General Letter 22nd October</a></li>
