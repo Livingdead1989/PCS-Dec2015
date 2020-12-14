88caa83f4f950899ebac2c230a67a4456c151e65
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2020-12-14 13:15:17 +0000

    order change

--- munch.php
+++ munch.php
@@ -105,30 +105,102 @@
         <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
     <div class="row">
-
         <div class="columns">
-            <p>At Park on Tuesday we were delighted to welcome Katy Trapani the Community Champion from Asda Havant who kindly presented Susan Parish, Business and Community Manager at Park Community School a  cheque to help support the MUNCH project that is running throughout the holidays. Susan said “I am delighted to accept this on behalf of Park Community School for the MUNCH project, it makes such a difference and ensures that no child or parent will go hungry in Leigh Park”.</p>
-            <p>Katy also kindly brought along crayons, games and drawing pads for the children to enjoy when they come to MUNCH. Thank you so much to the ASDA Foundation.</p>
+            <h4>Hampshire Education - Community Impact 2020</h4>
+            <div class="row">
+                <div class="columns small-8 small-push-2">
+                    <img src="images/munch/hampshire_education_awards_2020.jpg">
+                </div>
+            </div>
+            <br>
+            <p>Virtually Hampshire Education Awards took place on Monday. Proud to receive the Community Impact Award. This recognised Connect4Summer 2019 which provided free family holiday activities, childcare and meals. It was a fantastic team effort working in partnership with Romsey Community School and Department for Education Holiday Activities Fund Team.</p>
         </div>
-
-        <div class="medium-6 columns text-center">
-            <img src="images/munch/munch_asda_award.jpg">
-        </div>
-
-        <div class="medium-6 columns text-center">
-            <img src="images/munch/munch_asda_award2.jpg">
-        </div>
-
-        <div class="columns">
-        <br>
-            <p>If anyone is hungry throughout the holidays please do <a href="/contact.php">contact Park Community School</a> for further details.</p>
-        </div>
-
+        <br><br>
     </div>
     <br><br><br>
 </section>
 
 <section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="columns">
+            <h4>Coronavirus Hero Award</h4>
+            <div class="row">
+                <div class="columns small-8 small-push-2">
+                    <img src="images/munch/coronavirus_hero_award.jpg">
+                </div>
+            </div>
+            <br>
+            <p>At Park Community School our team have received recognition of our efforts during the lock downs by providing our Munch Meals from <a href="//uk.linkedin.com/in/alanmakhavant">Alan Mak MP</a>.</p>
+        </div>
+        <br><br>
+    </div>
+    
+    <br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <div class="medium-6 columns">
+            <h4>Order a MUNCH Money Box</h4>
+            <p>If you’re a local business or looking for an easy way to help us then please order one of our MUNCH Money boxes to fill up and raise money to feed local children who qualify for free school meals during the school holidays.  When you have filled it up, please return it to us at school, or please contact us and we’ll collect it. We will post you a receipt and replace it on request.</p>
+            <p><a href="mailto:munch@pcs.hants.sch.uk">Order your MUNCH money box today!</a></p>
+        </div>
+        <div class="medium-6 columns text-center">
+            <img src="images/munch/munch_money_box.jpg">
+        </div>
+        <br><br>
+    </div>
+    <br>
+</section>
+
+<section class="grey-bg" id="chrunch">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
+    </div>
+    <div class="row">
+        <div class="columns">
+            <h4>BBC Inside Out South</h4>
+            <p>Park Community School's MUNCH project has been featured in BBC's Inside Out South</p>
+            <p>"We meet the Portsmouth head teacher who opens his school throughout the holidays to feed children who might otherwise be missing meals."</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="columns small-8 small-push-2">
+            <div class="flex-video widescreen border-radius-5">
+                <iframe src="https://player.vimeo.com/video/299177101" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <br><br>
+</section>
+
+<section>
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+        <div class="columns">
+            <h4>CHRUNCH 2018 <small>Christmas in a box</small></h4>
+            <p>The MUNCH team at Park Community School working in partnership with the FatFace Foundation, have created CHRUNCH to try and reduce food poverty at Christmas in the Havant area. PO9 Foodbank will be assisting with distribution from the Dickinson Centre at Park Community School.</p>
+
+            <p><strong>This years CHRUNCH has now finished</strong>, the support for this project has been fantatic we wish to thank everyone that made this possible.</p>
+            <p>If you wish to support future projects please contact our <a href="/contact.php">school reception</a>.</p>
+        </div>
+        <div class="columns text-center">
+            <a href="assets/munch/chrunch_review_2018.pdf" target="_blank" class="button success radius">CHRUNCH Review</a>
+        </div>
+    </div>
+    
+    <br><br>
+</section>
+
+<section class="grey-bg" id="chrunch">
     <div class="row">
         <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
     </div>
@@ -166,84 +238,6 @@
             <a href="assets/munch/munch_review_2019.pdf" target="_blank" class="button success radius">Munch Review</a>
         </div>
     </div>
-    <br><br>
-</section>
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-        <div class="columns">
-            <h4>BBC Inside Out South</h4>
-            <p>Park Community School's MUNCH project has been featured in BBC's Inside Out South</p>
-            <p>"We meet the Portsmouth head teacher who opens his school throughout the holidays to feed children who might otherwise be missing meals."</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="columns small-8 small-push-2">
-            <div class="flex-video widescreen border-radius-5">
-                <iframe src="https://player.vimeo.com/video/299177101" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            </div>
-        </div>
-    </div>
-    <br>
-</section>
-
-<section class="grey-bg" id="chrunch">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row">
-        <div class="columns">
-            <h4>CHRUNCH 2018 <small>Christmas in a box</small></h4>
-            <p>The MUNCH team at Park Community School working in partnership with the FatFace Foundation, have created CHRUNCH to try and reduce food poverty at Christmas in the Havant area. PO9 Foodbank will be assisting with distribution from the Dickinson Centre at Park Community School.</p>
-
-            <p><strong>This years CHRUNCH has now finished</strong>, the support for this project has been fantatic we wish to thank everyone that made this possible.</p>
-            <p>If you wish to support future projects please contact our <a href="/contact.php">school reception</a>.</p>
-        </div>
-        <div class="columns text-center">
-            <a href="assets/munch/chrunch_review_2018.pdf" target="_blank" class="button success radius">CHRUNCH Review</a>
-        </div>
-    </div>
-    <br><br>
-</section>
-
-<section>
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
-    </div>
-    <div class="row">
-        <div class="medium-6 columns">
-            <h4>Order a MUNCH Money Box</h4>
-            <p>If you’re a local business or looking for an easy way to help us then please order one of our MUNCH Money boxes to fill up and raise money to feed local children who qualify for free school meals during the school holidays.  When you have filled it up, please return it to us at school, or please contact us and we’ll collect it. We will post you a receipt and replace it on request.</p>
-            <p><a href="mailto:munch@pcs.hants.sch.uk">Order your MUNCH money box today!</a></p>
-        </div>
-        <div class="medium-6 columns text-center">
-            <img src="images/munch/munch_money_box.jpg">
-        </div>
-        <br><br>
-    </div>
-    <br>
-</section>
-
-<section class="grey-bg" id="chrunch">
-    <div class="row">
-        <div class="small-centered large-1 column text-center hr-logo white-bg"></div>
-    </div>
-    <div class="row">
-        <div class="columns">
-            <h4>Coronavirus Hero Award</h4>
-            <div class="row">
-                <div class="columns small-8 small-push-2">
-                    <img src="images/munch/coronavirus_hero_award.jpg">
-                </div>
-            </div>
-            <br>
-            <p>At Park Community School our team have received recognition of our efforts during the lock downs by providing our Munch Meals from <a href="//uk.linkedin.com/in/alanmakhavant">Alan Mak MP</a>.</p>
-        </div>
-        <br><br>
-    </div>
     
     <br><br>
 </section>
@@ -253,18 +247,27 @@
         <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
     </div>
     <div class="row">
+
         <div class="columns">
-            <h4>Hampshire Education - Community Impact 2020</h4>
-            <div class="row">
-                <div class="columns small-8 small-push-2">
-                    <img src="images/munch/hampshire_education_awards_2020.jpg">
-                </div>
-            </div>
-            <br>
-            <p>Virtually Hampshire Education Awards took place on Monday. Proud to receive the Community Impact Award. This recognised Connect4Summer 2019 which provided free family holiday activities, childcare and meals. It was a fantastic team effort working in partnership with Romsey Community School and Department for Education Holiday Activities Fund Team.</p>
+            <p>At Park on Tuesday we were delighted to welcome Katy Trapani the Community Champion from Asda Havant who kindly presented Susan Parish, Business and Community Manager at Park Community School a  cheque to help support the MUNCH project that is running throughout the holidays. Susan said “I am delighted to accept this on behalf of Park Community School for the MUNCH project, it makes such a difference and ensures that no child or parent will go hungry in Leigh Park”.</p>
+            <p>Katy also kindly brought along crayons, games and drawing pads for the children to enjoy when they come to MUNCH. Thank you so much to the ASDA Foundation.</p>
+        </div>
+
+        <div class="medium-6 columns text-center">
+            <img src="images/munch/munch_asda_award.jpg">
+        </div>
+
+        <div class="medium-6 columns text-center">
+            <img src="images/munch/munch_asda_award2.jpg">
+        </div>
+
+        <div class="columns">
+        <br>
+            <p>If anyone is hungry throughout the holidays please do <a href="/contact.php">contact Park Community School</a> for further details.</p>
         </div>
         <br><br>
     </div>
+    
     <br>
 </section>
 
