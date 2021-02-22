e483033450d1b4d37318915fe8caef88e826646b
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-02-22 14:13:19 +0000

    Letter Update

--- letters-home.php
+++ letters-home.php
@@ -27,11 +27,12 @@
         	<ul class="small-block-grid-1">
                 <li>
                     <ul class="circle">February 2021<hr>
+                        <li><a href="/assets/letters/2021-02/22-general-letter-to-parents.pdf" target="_blank">General Letter to Parents 22nd Feb</a></li>
                         <li><a href="/assets/letters/2021-02/newsletter-12-02.pdf" target="_blank">Newsletter 12th February</a></li>
                         <li><a href="/assets/letters/2021-02/memorial-woodland-naming-competition.pdf" target="_blank">Memorial Woodland Naming Competition</a></li>
                         <li><a href="/assets/letters/2021-02/parent-survey-remote-learning-tips-and-keydates.pdf" target="_blank">Parent Survey, Remote Learning, Tips and Key Dates</a></li>
                         <li><a href="/assets/letters/2021-02/keyworker-school-key-dates.pdf" target="_blank">Key Worker School Key Dates</a></li>
-						<li><a href="/assets/letters/2021-02/8-general-letter-to-parents.pdf" target="_blank">General Letter to Parents</a></li>
+						<li><a href="/assets/letters/2021-02/8-general-letter-to-parents.pdf" target="_blank">General Letter to Parents 8th Feb</a></li>
                         
                     </ul>
                 <li>
