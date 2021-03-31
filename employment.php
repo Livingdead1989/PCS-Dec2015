ab9923be2c61ede292cee25b8a76f8c1bac7a842
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-03-31 07:38:12 +0100

    Receptionist Advert

--- employment.php
+++ employment.php
@@ -58,6 +58,16 @@
         </span>
       </li>
 
+      <li>
+        <h4>Receptionist</h4>  
+        <p><em>Closing Date: 20th April 2021</em></p>
+
+        <span class="subheader">
+        <a href="assets/recruitment/receptionist.pdf" target="_blank">Position Details</a>
+        </span>
+      </li>
+
+        
       <!--<br><span class="subheader">Please check back later.</span>-->
       
           
