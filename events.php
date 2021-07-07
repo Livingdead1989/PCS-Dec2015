08ccc3ff795f776effbe252644c44e3e4891f60d
LivingDead1989 <Steven.Smith1989@Hotmail.co.uk>
2021-07-07 12:20:44 +0100

    -removed expired events

--- events.php
+++ events.php
@@ -67,8 +67,25 @@
                 <div class="cost-wrap">Free</div>
               </div>
             </div>
+
+            <div class="event-wrap">
+                <div class="date-wrap">
+                  <p class="event-date">Every Day</p>
+                  <p class="event-time">07:45-08:15</p>
+                </div>
+                <h2 class="event-title">The Big Healthy Breakfast</h2>
+                <p class="event-subheader">Every day starting in January 2019</p>
+                <p class="event-description">Free breakfast for EVERY student. <br> Choose from: Hot bagels, Cereals or Porridge (other options available but will be charged)</p>
+                <div class="link-wrap">
+                  <a href="/assets/events/big_breakfast.pdf" target="_blank" class="button success radius">View our Flyer</a>
+                </div>
+                <div class="bottom-wrap">
+                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
+                  <div class="cost-wrap">Free</div>
+                </div>
+              </div>
          
-<!--                    Event Box  -->
+
               <!-- <div class="event-wrap feature-event sticky-event">
                 <div class="date-wrap">
                   <p class="event-date"></p>
@@ -85,84 +102,7 @@
                   <div class="cost-wrap"></div>
                 </div>
               </div> -->
-<!--                  Event box --> 
 
-<!--                    Event Box  -->
-               <div class="event-wrap feature-event sticky-event">
-                <div class="date-wrap">
-                  <p class="event-date">Thursday</p>
-                  <p class="event-time">18:00-20:00</p>
-                </div>
-                <h2 class="event-title">Cook and Create</h2>
-                <p class="event-subheader">Every Thursday for 6 Weeks</p>
-                <p class="event-description">The course at Park Community School is totally free and for anyone needing a little cooking inspiration.</p>
-                <div class="link-wrap">
-                  <a href="/assets/events/cook-and-create.pdf" target="_blank" class="button success radius">Read More...</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Free</div>
-                </div>
-              </div>
-
-
-
-<!--                  Event Box -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">Thursday</p>
-                  <p class="event-time">17:30-18:30</p>
-                </div>
-                <h2 class="event-title">MUNCH Tea Time</h2>
-                <p class="event-subheader">Every Thursday</p>
-                <p class="event-description">Join us for dinner on Thursday. <br>This has all been made possible by generous funding from various organisations.</p>
-                <div class="link-wrap">
-                  <a href="/munch.php" target="_blank" class="button success radius">Munch Page</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Free</div>
-                </div>
-              </div>
-<!--                    Event box -->
-
-<!--                    Event Box  -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">Every Day</p>
-                  <p class="event-time">07:45-08:15</p>
-                </div>
-                <h2 class="event-title">The Big Healthy Breakfast</h2>
-                <p class="event-subheader">Every day starting in January 2019</p>
-                <p class="event-description">Free breakfast for EVERY student. <br> Choose from: Hot bagels, Cereals or Porridge (other options available but will be charged)</p>
-                <div class="link-wrap">
-                  <a href="/assets/events/big_breakfast.pdf" target="_blank" class="button success radius">View our Flyer</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Free</div>
-                </div>
-              </div>
-<!--                  Event box --> 
-
-<!--                    Event Box  -->
-              <div class="event-wrap">
-                <div class="date-wrap">
-                  <p class="event-date">Thursday</p>
-                  <p class="event-time">16:00+</p>
-                </div>
-                <h2 class="event-title">Free Counselling Service</h2>
-                <p class="event-subheader">16+ and adults</p>
-                <p class="event-description">Free service by Katrina Martine - Qualified Counsellor for more information visit <a href="//www.therapywithkat.com/">www.therapywithkat.com</a></p>
-                <div class="link-wrap">
-                  <a href="/assets/events/counselling_service.pdf" target="_blank" class="button success radius">Read More...</a>
-                </div>
-                <div class="bottom-wrap">
-                  <div class="location-wrap"><a href="https://www.google.com/maps/place/Park+Community+School/@50.8727033,-0.986906,14z/data=!4m5!3m4!1s0x487444917f5df9f1:0x1e05d50144e0a88a!8m2!3d50.87005!4d-1.001233" target="_blank">Park Community School</a></div>
-                  <div class="cost-wrap">Free</div>
-                </div>
-              </div>
-<!--                  Event box -->
 
             </div>
               
